dbb4301318cf2999b37d03e306ae33769efd7f45
mattclarke <matt.clarke@stfc.ac.uk>
2015-06-16 11:17:05 +0100

    Removed JavadocPackage check and made the Eclipse version less strict

--- base/uk.ac.stfc.isis.ibex.ui.configserver/src/uk/ac/stfc/isis/ibex/ui/configserver/dialogs/EditConfigDialog.java
+++ base/uk.ac.stfc.isis.ibex.ui.configserver/src/uk/ac/stfc/isis/ibex/ui/configserver/dialogs/EditConfigDialog.java
@@ -22,30 +22,26 @@ import uk.ac.stfc.isis.ibex.configserver.configuration.Configuration;
 import uk.ac.stfc.isis.ibex.configserver.editing.EditableConfiguration;
 import uk.ac.stfc.isis.ibex.ui.configserver.editing.ConfigEditorPanel;
 
-public class EditConfigDialog extends TitleAreaDialog implements MessageDisplayer {
-	
+public class EditConfigDialog extends TitleAreaDialog implements
+		MessageDisplayer {
+
 	private static final Point INITIAL_SIZE = new Point(650, 750);
 	private final String title;
 	private final String subTitle;
 
 	private Map<String, String> errorMessages = new HashMap<String, String>();
-	
+
 	private EditableConfiguration config;
-	
+
 	private ConfigEditorPanel editor;
 	private boolean doSaveAs = false;
 	private boolean doAsComponent = false;
 	private boolean isComponent;
 	private boolean isBlank;
 	private Button saveAsBtn;
-	
-	public EditConfigDialog(
-			Shell parentShell, 
-			String title, 
-			String subTitle, 
-			EditableConfiguration config, 
-			boolean isComponent,
-			boolean isBlank) {
+
+	public EditConfigDialog(Shell parentShell, String title, String subTitle,
+			EditableConfiguration config, boolean isComponent, boolean isBlank) {
 		super(parentShell);
 		setShellStyle(getShellStyle() | SWT.DIALOG_TRIM | SWT.RESIZE);
 		this.title = title;
@@ -54,97 +50,99 @@ public class EditConfigDialog extends TitleAreaDialog implements MessageDisplaye
 		this.isComponent = isComponent;
 		this.isBlank = isBlank;
 	}
-	
+
 	@Override
-	protected Control createDialogArea(Composite parent) {		
+	protected Control createDialogArea(Composite parent) {
 		editor = new ConfigEditorPanel(parent, SWT.NONE, this, isComponent);
-		editor.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));		
+		editor.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		setTitle(subTitle);
 		editor.setConfigToEdit(config);
-		
+
 		return editor;
 	}
 
-	
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
-		if (isBlank == false){ 
+		if (isBlank == false) {
 			createButton(parent, IDialogConstants.OK_ID, "Save", true);
 		}
-		saveAsBtn = createButton(parent, IDialogConstants.CLIENT_ID + 1, "Save as ...", false);
-		
-		saveAsBtn.addSelectionListener(new SelectionAdapter(){
+		saveAsBtn = createButton(parent, IDialogConstants.CLIENT_ID + 1,
+				"Save as ...", false);
+
+		saveAsBtn.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				Collection<String> configNames = Configurations.getInstance().server().configNames();
-				Collection<String> componentNames = Configurations.getInstance().server().componentNames();
-				boolean hasComponents = !config.getEditableComponents().getSelected().isEmpty();
-				SaveConfigDialog dlg = new SaveConfigDialog(null, config.getName(), config.getDescription(), configNames, componentNames, !isComponent, hasComponents);
+				Collection<String> configNames = Configurations.getInstance()
+						.server().configNames();
+				Collection<String> componentNames = Configurations
+						.getInstance().server().componentNames();
+				boolean hasComponents = !config.getEditableComponents()
+						.getSelected().isEmpty();
+				SaveConfigDialog dlg = new SaveConfigDialog(null, config
+						.getName(), config.getDescription(), configNames,
+						componentNames, !isComponent, hasComponents);
 				if (dlg.open() == Window.OK) {
-					if (dlg.getNewName() != config.getName()){
+					if (dlg.getNewName() != config.getName()) {
 						config.setName(dlg.getNewName());
 					}
-					
+
 					config.setDescription(dlg.getNewDescription());
 					doAsComponent = dlg.willBeComponent();
-					
+
 					okPressed();
 				}
 			}
 		});
-		
+
 		createButton(parent, IDialogConstants.CANCEL_ID, "Cancel", false);
-	}	
-	
+	}
+
 	@Override
 	protected void configureShell(Shell shell) {
 		super.configureShell(shell);
 		shell.setText(title);
 	}
-	
+
 	@Override
 	protected Point getInitialSize() {
 		return INITIAL_SIZE;
 	}
-	
+
 	@Override
 	public void setErrorMessage(String source, String error) {
 		errorMessages.put(source, error);
 		showErrorMessage();
 	}
-	
+
 	// Show the current error messages
 	private void showErrorMessage() {
 		StringBuilder sb = new StringBuilder();
-		for ( String key : errorMessages.keySet() ) {
-			if (errorMessages.get(key)!=null) {
+		for (String key : errorMessages.keySet()) {
+			if (errorMessages.get(key) != null) {
 				sb.append(errorMessages.get(key));
 				sb.append("  ");
 			}
 		}
-		
-		if (sb.length()>0) {
+
+		if (sb.length() > 0) {
 			setErrorMessage(sb.toString());
 			// Don't allow save until errors are cleared
 			setOKEnabled(false);
 			saveAsBtn.setEnabled(false);
-		}
-		else {
+		} else {
 			setErrorMessage(null);
 			setOKEnabled(true);
 			saveAsBtn.setEnabled(true);
 		}
 	}
 
-	private void setOKEnabled(boolean value)
-	{
+	private void setOKEnabled(boolean value) {
 		Button okButton = getButton(IDialogConstants.OK_ID);
-		if (okButton != null)
-		{
+		if (okButton != null) {
 			okButton.setEnabled(value);
 		}
 	}
-	
+
 	public Configuration getConfig() {
 		return config.asConfiguration();
 	}
@@ -152,11 +150,11 @@ public class EditConfigDialog extends TitleAreaDialog implements MessageDisplaye
 	public Configuration getComponent() {
 		return config.asComponent();
 	}
-	
+
 	public boolean doSaveAs() {
 		return doSaveAs;
 	}
-	
+
 	public boolean doAsComponent() {
 		return doAsComponent;
 	}
