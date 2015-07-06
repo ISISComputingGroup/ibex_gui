eb765fe2518117598ddc746c1e3425ba5a6168f4
Lottie Greenwood <lottie.greenwood@stfc.ac.uk>
2015-07-06 11:37:13 +0100

    Added license message

---
+++ licenseAppend.py
@@ -0,0 +1,41 @@
+import os
+
+licenseText = """
+/*
+* This file is part of the ISIS IBEX application.
+* Copyright (C) 2012-2015 Science & Technology Facilities Council.
+* All rights reserved.
+*
+* This program is distributed in the hope that it will be useful.
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v1.0 which accompanies this distribution.
+* EXCEPT AS EXPRESSLY SET FORTH IN THE ECLIPSE PUBLIC LICENSE V1.0, THE PROGRAM 
+* AND ACCOMPANYING MATERIALS ARE PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES 
+* OR CONDITIONS OF ANY KIND.  See the Eclipse Public License v1.0 for more details.
+*
+* You should have received a copy of the Eclipse Public License v1.0
+* along with this program; if not, you can obtain a copy from
+* https://www.eclipse.org/org/documents/epl-v10.php or 
+* http://opensource.org/licenses/eclipse-1.0.php
+*/
+"""
+
+for dirname, dirnames, filenames in os.walk('.'):
+    # print path to all subdirectories first.
+    for subdirname in dirnames:
+        print(os.path.join(dirname, subdirname))
+
+    # print path to all filenames.
+    for filename in filenames:
+		print(os.path.join(dirname, filename))
+		a = os.path.join(dirname, filename)
+		if '.java' in filename:
+			with open(a, "r+") as targetFile:
+				content = targetFile.read()
+				targetFile.seek(0,0)
+				targetFile.write(licenseText+ "\n" + content)
+
+    # editing the 'dirnames' list will stop os.walk() from recursing into there.
+    if '.git' in dirnames:
+        # don't go into any .git directories.
+        dirnames.remove('.git')
