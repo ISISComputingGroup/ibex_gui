dc346f0e29b3f63837e335183d6d1e176485f078
mattclarke <matt.clarke@stfc.ac.uk>
2015-07-14 14:03:04 +0100

    Time channels file hidden
    Nobody uses TCB files anymore so I have deleted the controls.
    I have also fixed a problem with the Time Unit not updating.
    The P, D, S, W file selecter did not work if set to read-only, so I
    have switched it back. This should be investigated in the future.

--- base/uk.ac.stfc.isis.ibex.ui.dae/src/uk/ac/stfc/isis/ibex/ui/dae/experimentsetup/periods/PeriodsPanel.java
+++ base/uk.ac.stfc.isis.ibex.ui.dae/src/uk/ac/stfc/isis/ibex/ui/dae/experimentsetup/periods/PeriodsPanel.java
@@ -81,7 +81,7 @@ public class PeriodsPanel extends Composite {
 		lblPeriodFile.setSize(58, 15);
 		lblPeriodFile.setText("Period File:");
 		
-		periodFile = new Combo(composite, SWT.DROP_DOWN | SWT.READ_ONLY);
+		periodFile = new Combo(composite, SWT.DROP_DOWN);
 		GridData gd_periodFile = new GridData(SWT.LEFT, SWT.FILL, false, false, 4, 1);
 		gd_periodFile.widthHint = 500;
 		periodFile.setLayoutData(gd_periodFile);
