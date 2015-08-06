ff85a5794abe80e965220588b83a6545b439545f
mattclarke <matt.clarke@stfc.ac.uk>
2015-08-06 15:26:26 +0100

    The back end is mostly working...

---
+++ base/uk.ac.stfc.isis.ibex.runcontrol/src/uk/ac/stfc/isis/ibex/runcontrol/RunControlActivator.java
@@ -0,0 +1,75 @@
+
+/*
+* This file is part of the ISIS IBEX application.
+* Copyright (C) 2012-2015 Science & Technology Facilities Council.
+* All rights reserved.
+*
+* This program is distributed in the hope that it will be useful.
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v1.0 which accompanies this distribution.
+* EXCEPT AS EXPRESSLY SET FORTH IN THE ECLIPSE PUBLIC LICENSE V1.0, THE PROGRAM 
+* AND ACCOMPANYING MATERIALS ARE PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES 
+* OR CONDITIONS OF ANY KIND.  See the Eclipse Public License v1.0 for more details.
+*
+* You should have received a copy of the Eclipse Public License v1.0
+* along with this program; if not, you can obtain a copy from
+* https://www.eclipse.org/org/documents/epl-v10.php or 
+* http://opensource.org/licenses/eclipse-1.0.php
+*/
+
+package uk.ac.stfc.isis.ibex.runcontrol;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import uk.ac.stfc.isis.ibex.epics.pv.Closer;
+import uk.ac.stfc.isis.ibex.instrument.Instrument;
+
+public class RunControlActivator extends Closer implements BundleActivator {
+
+	private static BundleContext context;
+	private static RunControlActivator instance;
+	
+	private final RunControlVariables variables;
+	private final RunControlServer server;
+	
+	public RunControlActivator() {
+		instance = this;
+		variables = registerForClose(new RunControlVariables(Instrument.getInstance().channels()));
+		server = registerForClose(new RunControlServer(variables));
+	}
+
+	static BundleContext getContext() {
+		return context;
+	}
+	
+	public static RunControlActivator getInstance() {
+		return instance;
+	}
+	
+	public RunControlVariables getVariables() {
+		return variables;
+	}
+	
+	public RunControlServer getServer() {
+		return server;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		RunControlActivator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		RunControlActivator.context = null;
+		close();
+	}
+
+}
