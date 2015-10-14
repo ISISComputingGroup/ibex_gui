8726bc04ddc20b63872450e664d158054be25ef0
KathrynBaker <kathryn.baker@stfc.ac.uk>
2015-10-14 10:17:01 +0100

    Additional class to hold validators

---
+++ base/uk.ac.stfc.isis.ibex.validators/src/uk/ac/stfc/isis/ibex/validators/ValidatorsActivator.java
@@ -0,0 +1,30 @@
+package uk.ac.stfc.isis.ibex.validators;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class ValidatorsActivator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		ValidatorsActivator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		ValidatorsActivator.context = null;
+	}
+
+}
