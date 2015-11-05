a9c61b323772987d197cc8566ad5f1fa192a616c
Ian Bush <ian.bush@stfc.ac.uk>
2015-11-05 12:31:48 +0000

    Slighlty messy way of restoring the window state

--- base/uk.ac.stfc.isis.ibex.product/src/uk/ac/stfc/isis/ibex/product/ApplicationWorkbenchWindowAdvisor.java
+++ base/uk.ac.stfc.isis.ibex.product/src/uk/ac/stfc/isis/ibex/product/ApplicationWorkbenchWindowAdvisor.java
@@ -19,6 +19,17 @@
 
 package uk.ac.stfc.isis.ibex.product;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
@@ -26,6 +37,9 @@ import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
+
+    private final static int MIN_WINDOW_HEIGHT = 800;
+    private final static int MIN_WINDOW_WIDTH = 1100;
 	
 	public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
 		super(configurer);
@@ -49,6 +63,99 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
     public void postWindowCreate() {
     	super.postWindowCreate();
         final Shell shell = getWindowConfigurer().getWindow().getShell();
-        shell.setMinimumSize(1100, 800);   
+        
+        WindowLayout windowLayout = getPreviousWindowSettings();
+
+        shell.setMinimumSize(MIN_WINDOW_WIDTH, MIN_WINDOW_HEIGHT);
+        shell.setLocation(windowLayout.windowX, windowLayout.windowY);
+        shell.setSize(windowLayout.windowWidth, windowLayout.windowHeight);
+        shell.setMaximized(windowLayout.windowMaximised);
+    }
+
+    /**
+     * Attempts to get previous window settings, otherwise returning sensible
+     * defaults.
+     * 
+     * @return An object with parameters for width, heigh, x position, y
+     *         poistion and maximised flag
+     */
+    private WindowLayout getPreviousWindowSettings() {
+        int windowHeight = MIN_WINDOW_HEIGHT;
+        int windowWidth = MIN_WINDOW_WIDTH;
+        int windowX = 0;
+        int windowY = 0;
+        boolean windowMaximised = true;
+
+        IPath workspaceRoot = ResourcesPlugin.getWorkspace().getRoot().getLocation();
+
+        workspaceRoot = workspaceRoot.append(new Path(".metadata"));
+        workspaceRoot = workspaceRoot.append(new Path(".plugins"));
+        workspaceRoot = workspaceRoot.append(new Path("org.eclipse.ui.workbench"));
+
+        // This pattern picks out from <window height="800" maximized="true"
+        // width="1100" x="0" y="0">
+        Pattern patternMaximized = Pattern.compile(
+                "<window height=\"(\\d+)\" maximized=\"([a-z]+)\" width=\"(\\d+)\" x=\"(-?\\d+)\" y=\"(-?\\d+)\">");
+
+        // This pattern picks out from <window height="800" width="1100" x="0"
+        // y="0">
+        Pattern patternNotMaximized = Pattern
+                .compile("<window height=\"(\\d+)\" width=\"(\\d+)\" x=\"(-?\\d+)\" y=\"(-?\\d+)\">");
+
+        try {
+            File file = new File(workspaceRoot + "/workbench.xml");
+            FileReader reader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            
+            boolean foundMatch = false;
+
+            while (bufferedReader.ready() && !foundMatch) {
+                String line = bufferedReader.readLine();
+
+                Matcher matchMaximised = patternMaximized.matcher(line);
+                Matcher matchNotMaximised = patternNotMaximized.matcher(line);
+
+                if (matchMaximised.matches()) {
+                    windowHeight = Integer.parseInt(matchMaximised.group(1));
+                    windowMaximised = Boolean.parseBoolean(matchMaximised.group(2));
+                    windowWidth = Integer.parseInt(matchMaximised.group(3));
+                    windowX = Integer.parseInt(matchMaximised.group(4));
+                    windowY = Integer.parseInt(matchMaximised.group(5));
+                    
+                    foundMatch = true;
+                } else if (matchNotMaximised.matches()) {
+                    windowHeight = Integer.parseInt(matchNotMaximised.group(1));
+                    windowMaximised = false;
+                    windowWidth = Integer.parseInt(matchNotMaximised.group(2));
+                    windowX = Integer.parseInt(matchNotMaximised.group(3));
+                    windowY = Integer.parseInt(matchNotMaximised.group(4));
+                    
+                    foundMatch = true;
+                }
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new WindowLayout(windowHeight, windowWidth, windowX, windowY, windowMaximised);
+    }
+
+    private class WindowLayout {
+        public int windowHeight;
+        public int windowWidth;
+        public int windowX;
+        public int windowY;
+        public boolean windowMaximised;
+
+        public WindowLayout(int height, int width, int x, int y, boolean maximised) {
+            windowHeight = height;
+            windowWidth = width;
+            windowX = x;
+            windowY = y;
+            windowMaximised = maximised;
+        }
     }
 }
