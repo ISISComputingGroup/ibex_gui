dfdc41962410847cb46c29c0efa9213a152425c5
mattclarke <matt.clarke@stfc.ac.uk>
2016-01-04 11:21:18 +0000

    Formatting

--- base/uk.ac.stfc.isis.ibex.ui.scripting/src/uk/ac/stfc/isis/ibex/ui/scripting/GeniePythonConsoleFactory.java
+++ base/uk.ac.stfc.isis.ibex.ui.scripting/src/uk/ac/stfc/isis/ibex/ui/scripting/GeniePythonConsoleFactory.java
@@ -38,8 +38,7 @@ public class GeniePythonConsoleFactory extends PydevConsoleFactory {
 	
 	private final NullProgressMonitor monitor = new NullProgressMonitor();
 	
-	private final static String GENIE_INITIALISATION = 
- "# Configuring genie_python, please wait\n"
+    private final static String GENIE_INITIALISATION = "# Configuring genie_python, please wait\n"
 			+ "import sys;sys.executable=''\n" 
 			+ "from genie_python.genie_startup import * \n" 
 			+ "load_script(None, globals()) \n"
