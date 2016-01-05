cb46a288e3fe553da9bcdb315f3418e56b66a948
Ian Bush <ian.bush@stfc.ac.uk>
2016-01-05 16:17:22 +0000

    Fixed experiment details tests, so they now run on maven and broken tests pass

--- base/uk.ac.stfc.isis.ibex.experimentdetails.tests/src/uk/ac/stfc/isis/ibex/experimentdetails/tests/database/SqlStatementTest.java
+++ base/uk.ac.stfc.isis.ibex.experimentdetails.tests/src/uk/ac/stfc/isis/ibex/experimentdetails/tests/database/SqlStatementTest.java
@@ -1,6 +1,25 @@
+
+/*
+ * This file is part of the ISIS IBEX application. Copyright (C) 2012-2015
+ * Science & Technology Facilities Council. All rights reserved.
+ *
+ * This program is distributed in the hope that it will be useful. This program
+ * and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution. EXCEPT AS
+ * EXPRESSLY SET FORTH IN THE ECLIPSE PUBLIC LICENSE V1.0, THE PROGRAM AND
+ * ACCOMPANYING MATERIALS ARE PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND. See the Eclipse Public License v1.0 for more
+ * details.
+ *
+ * You should have received a copy of the Eclipse Public License v1.0 along with
+ * this program; if not, you can obtain a copy from
+ * https://www.eclipse.org/org/documents/epl-v10.php or
+ * http://opensource.org/licenses/eclipse-1.0.php
+ */
+
 package uk.ac.stfc.isis.ibex.experimentdetails.tests.database;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,7 +48,7 @@ public class SqlStatementTest {
 		String result = sql.getSelectStatement();
 		
 		//Assert
-		assertEquals("SELECT user.name FROM  WHERE ", result);
+        assertEquals("SELECT user.name AS \"user.name\" FROM  WHERE ", result);
 	}
 	
 	@Test
@@ -44,7 +63,7 @@ public class SqlStatementTest {
 		String result = sql.getSelectStatement();
 		
 		//Assert
-		assertEquals("SELECT user.name, role.name FROM  WHERE ", result);
+        assertEquals("SELECT user.name AS \"user.name\", role.name AS \"role.name\" FROM  WHERE ", result);
 	}
 	
 	@Test
@@ -203,7 +222,9 @@ public class SqlStatementTest {
 		String result = sql.getSelectStatement();
 		
 		//Assert
-		assertEquals("SELECT user.userID, role.name FROM exp_data.experiment, exp_data.role WHERE user.userID = experimentteams.userID AND experiment.experimentID LIKE 1234", result);
+        assertEquals(
+                "SELECT user.userID AS \"user.userID\", role.name AS \"role.name\" FROM exp_data.experiment, exp_data.role WHERE user.userID = experimentteams.userID AND experiment.experimentID LIKE 1234",
+                result);
 	}
 	
 	
