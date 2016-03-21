fa1acad6f56d8582a01f9a6cb26b1adcaf184c4d
IsabellaRey <isabella.rey@stfc.ac.uk>
2016-03-21 16:56:17 +0000

    Removed hard-coded target names from source. They are now taken directly from the opi_info.xml file

--- base/uk.ac.stfc.isis.ibex.ui.synoptic.editor/src/uk/ac/stfc/isis/ibex/ui/synoptic/editor/target/DefaultTargetForComponent.java
+++ base/uk.ac.stfc.isis.ibex.ui.synoptic.editor/src/uk/ac/stfc/isis/ibex/ui/synoptic/editor/target/DefaultTargetForComponent.java
@@ -21,10 +21,13 @@ package uk.ac.stfc.isis.ibex.ui.synoptic.editor.target;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Maps;
+
 import uk.ac.stfc.isis.ibex.opis.Opi;
+import uk.ac.stfc.isis.ibex.opis.desc.OpiDescription;
 import uk.ac.stfc.isis.ibex.synoptic.model.ComponentType;
 import uk.ac.stfc.isis.ibex.synoptic.model.desc.TargetDescription;
 import uk.ac.stfc.isis.ibex.synoptic.model.desc.TargetType;
@@ -38,82 +41,13 @@ public final class DefaultTargetForComponent {
     private DefaultTargetForComponent() {
     }
 
-    public static Collection<TargetDescription> defaultTarget(ComponentType compType) {
-        Map<String, TargetType> targetNameList = new HashMap<>();
-
-        switch (compType) {
-            case UNKNOWN:
-            case MOVINGBEAMSTOP:
-                // no sensible default(s) for the above at present
-                break;
-            case DAE:
-                targetNameList.put("DAE", TargetType.COMPONENT);
-                break;
-            case GONIOMETER:
-                targetNameList.put("Goniometer", TargetType.COMPONENT);
-                break;
-            case JAWS:
-                targetNameList.put("Slit 1", TargetType.OPI);
-                break;
-            case CHOPPER:
-                targetNameList.put("Mk3 Chopper", TargetType.OPI);
-                break;
-            case MONITOR:
-                targetNameList.put("Double Monitor", TargetType.OPI);
-                targetNameList.put("Monitor", TargetType.OPI);
-                break;
-            case JULABO:
-                targetNameList.put("Julabo FL300", TargetType.OPI);
-                targetNameList.put("Julabo FP50", TargetType.OPI);
-                break;
-            case SAMPLESTACK:
-                targetNameList.put("Sample stage", TargetType.OPI);
-                break;
-            case CAEN:
-                targetNameList.put("Caen HV", TargetType.OPI);
-                break;
-            case KEPCO:
-                targetNameList.put("Kepco", TargetType.OPI);
-                break;
-            case BEAMSTOP:
-                targetNameList.put("Beam-stop", TargetType.OPI);
-                break;
-            case MOVINGMONITOR:
-                targetNameList.put("In Out Monitor", TargetType.OPI);
-                break;
-            case ROTATINGBENCH:
-                targetNameList.put("Rotating Bench", TargetType.OPI);
-                break;
-            case SAMPLECHANGER:
-                targetNameList.put("Sample changer", TargetType.OPI);
-                break;
-            case ANALYSER:
-                targetNameList.put("Analyser", TargetType.OPI);
-                break;
-            case POLARISER:
-                targetNameList.put("Polariser", TargetType.OPI);
-                break;
-            case EUROTHERM:
-                targetNameList.put("Eurotherm", TargetType.OPI);
-                break;
-            case PINHOLESELECTOR:
-                targetNameList.put("Pinhole Selector", TargetType.OPI);
-                break;
-            case SINGLESTAGE:
-                targetNameList.put("Single Stage", TargetType.OPI);
-                break;
-            default:
-                // no sensible default(s) at present
-                break;
-
-        }
-        
+    public static Collection<TargetDescription> defaultTarget(final ComponentType compType) {
         Collection<TargetDescription> targetDescriptions = new ArrayList<>();
 
-        for (String targetName : targetNameList.keySet()) {
-            if (targetNameInOpiList(targetName) && targetNameList.get(targetName) == TargetType.OPI) {
-                targetDescriptions.add(new TargetDescription(targetName, targetNameList.get(targetName)));
-            }
+        Map<String, OpiDescription> opis = Opi.getDefault().descriptionsProvider().getDescriptions().getOpis();
+        Collection<String> matchingTargetNames = Maps.filterValues(opis, typeMatches(compType)).keySet();
+        for (String targetName : matchingTargetNames) {
+            targetDescriptions.add(new TargetDescription(targetName, TargetType.OPI));
         }
         
         if (targetDescriptions.size() == 0) {
@@ -122,10 +56,13 @@ public final class DefaultTargetForComponent {
 
         return targetDescriptions;
     }
-
-    private static Boolean targetNameInOpiList(String targetName) {
-        Collection<String> availableOPIs = Opi.getDefault().descriptionsProvider().getOpiList();
-        return availableOPIs.contains(targetName);
+    
+    private static Predicate<OpiDescription> typeMatches(final ComponentType compType) {
+        return new Predicate<OpiDescription>() {
+            @Override
+            public boolean apply(OpiDescription opiDescription) {
+                return opiDescription.getType().equals(compType.toString());
+            }
+        };
     }
-
 }
