6579f38a55b50a08e2c6c6fc7702e6232abdc7ae
IsabellaRey <isabella.rey@stfc.ac.uk>
2016-03-30 14:48:04 +0100

    Ticket 1093: added search on instrument name

--- base/uk.ac.stfc.isis.ibex.ui.mainmenu/src/uk/ac/stfc/isis/ibex/ui/mainmenu/instrument/InstrumentSelectionPanel.java
+++ base/uk.ac.stfc.isis.ibex.ui.mainmenu/src/uk/ac/stfc/isis/ibex/ui/mainmenu/instrument/InstrumentSelectionPanel.java
@@ -42,8 +42,8 @@ import uk.ac.stfc.isis.ibex.instrument.InstrumentInfo;
 
 public class InstrumentSelectionPanel extends Composite {
 
-    private final Text txtInstrument;
-    private final InstrumentTable instrumentTable;
+    private Text txtInstrument;
+    private InstrumentTable instrumentTable;
     private Collection<InstrumentInfo> instruments;
 
     public InstrumentSelectionPanel(Composite parent, int style, Collection<InstrumentInfo> instruments) {
@@ -57,48 +57,10 @@ public class InstrumentSelectionPanel extends Composite {
         grpInstrument.setText("Instrument Selector");
         grpInstrument.setLayout(new GridLayout(3, false));
 
-        Label lblInstrument = new Label(grpInstrument, SWT.NONE);
-        lblInstrument.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-        lblInstrument.setText("Instrument:");
-
-        txtInstrument = new Text(grpInstrument, SWT.BORDER);
-        GridData gdInstrument = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-        gdInstrument.widthHint = 130;
-        txtInstrument.setLayoutData(gdInstrument);
-        txtInstrument.addModifyListener(new ModifyListener() {
-            @Override
-            public void modifyText(ModifyEvent arg0) {
-                instrumentTable.setSearch(txtInstrument.getText());
-            }
-        });
-
-        final Button btnClear = new Button(grpInstrument, SWT.NONE);
-        btnClear.setText("Clear");
-        Listener clearListener = new Listener() {
-            @Override
-            public void handleEvent(Event event) {
-                if (event.widget == btnClear) {
-                    txtInstrument.setText("");
-                }
-            }
-        };
-        btnClear.addListener(SWT.Selection, clearListener);
-
-        instrumentTable = new InstrumentTable(grpInstrument, SWT.NONE,
-                SWT.V_SCROLL | SWT.NO_SCROLL | SWT.FULL_SELECTION, this.instruments);
-        GridData gdInstrumentTable = new GridData(SWT.FILL, SWT.FILL, true, true, 3, 1);
-        gdInstrumentTable.heightHint = 300;
-        instrumentTable.setLayoutData(gdInstrumentTable);
-        instrumentTable.addSelectionChangedListener(new ISelectionChangedListener() {
-            @Override
-            public void selectionChanged(SelectionChangedEvent event) {
-                IStructuredSelection selection = (IStructuredSelection) event.getSelection();
-                if (selection.size() > 0) {
-                    InstrumentInfo instrument = (InstrumentInfo) selection.getFirstElement();
-                    txtInstrument.setText(instrument.name());
-                }
-            }
-        });
+        createInstrumentLabel(grpInstrument);
+        createInstrumentTextBox(grpInstrument);
+        createClearButton(grpInstrument);
+        createTable(grpInstrument);
     }
 
     public InstrumentInfo getSelected() {
@@ -116,72 +78,56 @@ public class InstrumentSelectionPanel extends Composite {
             
         return new InstrumentInfo(selectedInstrumentName);
     }
-}
 
-//import java.util.Collection;
-//
-//import org.eclipse.jface.viewers.ArrayContentProvider;
-//import org.eclipse.jface.viewers.ComboViewer;
-//import org.eclipse.jface.viewers.IStructuredSelection;
-//import org.eclipse.jface.viewers.LabelProvider;
-//import org.eclipse.swt.SWT;
-//import org.eclipse.swt.layout.GridData;
-//import org.eclipse.swt.layout.GridLayout;
-//import org.eclipse.swt.widgets.Combo;
-//import org.eclipse.swt.widgets.Composite;
-//import org.eclipse.swt.widgets.Label;
-//
-//import uk.ac.stfc.isis.ibex.instrument.InstrumentInfo;
-//
-//public class InstrumentSelectionPanel extends Composite {
-//	
-//	private final ComboViewer comboViewer;
-//	
-//	public InstrumentSelectionPanel(Composite parent, int style, Collection<InstrumentInfo> instruments) {
-//		super(parent, style);
-//		setLayout(new GridLayout(2, false));
-//		
-//		Label lblInstrument = new Label(this, SWT.NONE);
-//		lblInstrument.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-//		lblInstrument.setText("Instrument:");
-//		
-//        comboViewer = new ComboViewer(this, SWT.DROP_DOWN);
-//		Combo combo = comboViewer.getCombo();
-//		combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-//
-//		if (instruments != null) {
-//			bind(instruments);
-//		}
-//	}
-//
-//	private void bind(Collection<InstrumentInfo> instruments) {
-//		comboViewer.setContentProvider(ArrayContentProvider.getInstance());
-//		comboViewer.setLabelProvider(new LabelProvider() {
-//			@Override
-//			public String getText(Object element) {
-//				if (element instanceof InstrumentInfo) {
-//					InstrumentInfo info = (InstrumentInfo) element;
-//					return info.name();
-//				}
-//				
-//				return super.getText(element);
-//			}
-//		});
-//		
-//		comboViewer.setInput(instruments);
-//	}
-//
-//	public InstrumentInfo getSelected() {
-//		IStructuredSelection selection = (IStructuredSelection) comboViewer.getSelection();
-//		if (!selection.isEmpty()) {
-//			return (InstrumentInfo) selection.getFirstElement(); 				
-//		}
-//		
-//        String inputText = comboViewer.getCombo().getText();
-//        if (!inputText.isEmpty()) {
-//            return new InstrumentInfo(inputText);
-//        }
-//        
-//		return null;
-//	}
-//}
+    private void createInstrumentLabel(Composite parent) {
+        Label lblInstrument = new Label(parent, SWT.NONE);
+        lblInstrument.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+        lblInstrument.setText("Instrument:");
+    }
+
+    private void createInstrumentTextBox(Composite parent) {
+        txtInstrument = new Text(parent, SWT.BORDER);
+        GridData gdInstrument = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+        gdInstrument.widthHint = 130;
+        txtInstrument.setLayoutData(gdInstrument);
+        txtInstrument.addModifyListener(new ModifyListener() {
+            @Override
+            public void modifyText(ModifyEvent arg0) {
+                instrumentTable.setSearch(txtInstrument.getText());
+            }
+        });
+    }
+
+    private void createClearButton(Composite parent) {
+        final Button btnClear = new Button(parent, SWT.NONE);
+        btnClear.setText("Clear");
+        Listener clearListener = new Listener() {
+            @Override
+            public void handleEvent(Event event) {
+                if (event.widget == btnClear) {
+                    txtInstrument.setText("");
+                }
+            }
+        };
+
+        btnClear.addListener(SWT.Selection, clearListener);
+    }
+
+    private void createTable(Composite parent) {
+        instrumentTable = new InstrumentTable(parent, SWT.NONE, SWT.V_SCROLL | SWT.NO_SCROLL | SWT.FULL_SELECTION,
+                this.instruments);
+        GridData gdInstrumentTable = new GridData(SWT.FILL, SWT.FILL, true, true, 3, 1);
+        gdInstrumentTable.heightHint = 300;
+        instrumentTable.setLayoutData(gdInstrumentTable);
+        instrumentTable.addSelectionChangedListener(new ISelectionChangedListener() {
+            @Override
+            public void selectionChanged(SelectionChangedEvent event) {
+                IStructuredSelection selection = (IStructuredSelection) event.getSelection();
+                if (selection.size() > 0) {
+                    InstrumentInfo instrument = (InstrumentInfo) selection.getFirstElement();
+                    txtInstrument.setText(instrument.name());
+                }
+            }
+        });
+    }
+}
