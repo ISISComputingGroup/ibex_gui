136dca4c724c6a4bebad9b3e4bb732565833bdf1
IsabellaRey <isabella.rey@stfc.ac.uk>
2016-04-19 12:05:29 +0100

    Ticket 1032: minor renaming

--- base/uk.ac.stfc.isis.ibex.epics.tests/src/uk/ac/stfc/isis/ibex/epics/tests/writing/BaseWritableTest.java
+++ base/uk.ac.stfc.isis.ibex.epics.tests/src/uk/ac/stfc/isis/ibex/epics/tests/writing/BaseWritableTest.java
@@ -31,25 +31,25 @@ import uk.ac.stfc.isis.ibex.epics.writing.ConfigurableWriter;
 @SuppressWarnings({ "unchecked", "checkstyle:methodname" })
 public class BaseWritableTest {
 
-    private TestableBaseWritable<String> baseWritable;
+    private TestableBaseWritable<String> writable;
 
     @Test
     public void last_error_is_null_at_initialisation() {
         // Arrange
-        baseWritable = new TestableBaseWritable<String>();
+        writable = new TestableBaseWritable<String>();
 
         // Assert
-        assertNull(baseWritable.lastError());
+        assertNull(writable.lastError());
     }
 
     @Test
     public void subscribing_a_writer_updates_the_writer_canWrite() {
         // Arrange
-        baseWritable = new TestableBaseWritable<String>();
+        writable = new TestableBaseWritable<String>();
         ConfigurableWriter<String, String> mockWriter = mock(ConfigurableWriter.class);
 
         // Act
-        baseWritable.subscribe(mockWriter);
+        writable.subscribe(mockWriter);
 
         // Assert
         verify(mockWriter, times(1)).onCanWriteChanged(anyBoolean());
@@ -58,11 +58,11 @@ public class BaseWritableTest {
     @Test
     public void subscribing_a_writer_updates_the_writer_onError() {
         // Arrange
-        baseWritable = new TestableBaseWritable<String>();
+        writable = new TestableBaseWritable<String>();
         ConfigurableWriter<String, String> mockWriter = mock(ConfigurableWriter.class);
 
         // Act
-        baseWritable.subscribe(mockWriter);
+        writable.subscribe(mockWriter);
 
         // Assert
         verify(mockWriter, times(1)).onError(any(Exception.class));
@@ -71,30 +71,30 @@ public class BaseWritableTest {
     @Test
     public void last_error_is_updated_on_destination_error() {
         // Arrange
-        baseWritable = new TestableBaseWritable<String>();
+        writable = new TestableBaseWritable<String>();
         Exception exception = new Exception();
 
         // Act
-        baseWritable.error(exception);
+        writable.error(exception);
 
         // Assert
-        assertEquals(exception, baseWritable.lastError());
+        assertEquals(exception, writable.lastError());
     }
 
     @Test
     public void all_subscribed_writers_are_notified_in_case_of_error() {
         // Arrange
-        baseWritable = new TestableBaseWritable<String>();
+        writable = new TestableBaseWritable<String>();
 
         ConfigurableWriter<String, String> mockWriter1 = mock(ConfigurableWriter.class);
         ConfigurableWriter<String, String> mockWriter2 = mock(ConfigurableWriter.class);
-        baseWritable.subscribe(mockWriter1);
-        baseWritable.subscribe(mockWriter2);
+        writable.subscribe(mockWriter1);
+        writable.subscribe(mockWriter2);
 
         Exception exception = new Exception();
 
         // Act
-        baseWritable.error(exception);
+        writable.error(exception);
 
         // Assert
         verify(mockWriter1, times(1)).onError(exception);
@@ -104,43 +104,43 @@ public class BaseWritableTest {
     @Test
     public void can_write_is_updated_on_destination_can_write_change_true_case() {
         // Arrange
-        baseWritable = new TestableBaseWritable<String>();
+        writable = new TestableBaseWritable<String>();
         boolean expected = true;
 
         // Act
-        baseWritable.canWriteChanged(expected);
+        writable.canWriteChanged(expected);
 
         // Assert
-        assertEquals(expected, baseWritable.canWrite());
+        assertEquals(expected, writable.canWrite());
     }
 
     @Test
     public void can_write_is_updated_on_destination_can_write_change_false_case() {
         // Arrange
-        baseWritable = new TestableBaseWritable<String>();
+        writable = new TestableBaseWritable<String>();
         boolean expected = false;
 
         // Act
-        baseWritable.canWriteChanged(expected);
+        writable.canWriteChanged(expected);
 
         // Assert
-        assertEquals(expected, baseWritable.canWrite());
+        assertEquals(expected, writable.canWrite());
     }
 
     @Test
     public void all_subscribed_writers_are_notified_of_can_write_changed() {
         // Arrange
-        baseWritable = new TestableBaseWritable<String>();
+        writable = new TestableBaseWritable<String>();
 
         ConfigurableWriter<String, String> mockWriter1 = mock(ConfigurableWriter.class);
         ConfigurableWriter<String, String> mockWriter2 = mock(ConfigurableWriter.class);
-        baseWritable.subscribe(mockWriter1);
-        baseWritable.subscribe(mockWriter2);
+        writable.subscribe(mockWriter1);
+        writable.subscribe(mockWriter2);
 
         boolean expected = true;
 
         // Act
-        baseWritable.canWriteChanged(expected);
+        writable.canWriteChanged(expected);
 
         // Assert
         verify(mockWriter1, times(1)).onCanWriteChanged(expected);
@@ -150,10 +150,10 @@ public class BaseWritableTest {
     @Test
     public void subscribing_a_writer_returns_an_unsubscriber_for_the_writer() {
         // Arrange
-        baseWritable = new TestableBaseWritable<String>();
+        writable = new TestableBaseWritable<String>();
 
         ConfigurableWriter<String, String> mockWriter = mock(ConfigurableWriter.class);
-        Subscription returnedSubscription = baseWritable.subscribe(mockWriter);
+        Subscription returnedSubscription = writable.subscribe(mockWriter);
 
         Exception exception = new Exception();
 
@@ -161,24 +161,24 @@ public class BaseWritableTest {
         returnedSubscription.removeObserver();
 
         // Assert
-        baseWritable.error(exception);
+        writable.error(exception);
         verify(mockWriter, never()).onError(exception);
     }
 
     @Test
     public void a_writer_gets_subscribed_only_once() {
         // Arrange
-        baseWritable = new TestableBaseWritable<String>();
+        writable = new TestableBaseWritable<String>();
 
         ConfigurableWriter<String, String> mockWriter = mock(ConfigurableWriter.class);
 
         Exception exception = new Exception();
 
         // Act
-        baseWritable.subscribe(mockWriter);
-        baseWritable.subscribe(mockWriter);
+        writable.subscribe(mockWriter);
+        writable.subscribe(mockWriter);
 
-        baseWritable.error(exception);
+        writable.error(exception);
 
         // Assert
         verify(mockWriter, times(1)).onError(exception);
