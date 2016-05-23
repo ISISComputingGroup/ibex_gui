6bfffd8f7e1970cdffbac5997751c439533c48ce
IsabellaRey <isabella.rey@stfc.ac.uk>
2016-05-23 14:33:29 +0100

    Ticket 1189: Forgot to delete commented code in previous commit

--- base/uk.ac.stfc.isis.ibex.ui.synoptic/src/uk/ac/stfc/isis/ibex/ui/synoptic/component/icons/ComponentIcons.java
+++ base/uk.ac.stfc.isis.ibex.ui.synoptic/src/uk/ac/stfc/isis/ibex/ui/synoptic/component/icons/ComponentIcons.java
@@ -28,51 +28,6 @@ public final class ComponentIcons {
 	
 	private ComponentIcons() { }
 	
-//	public static Image iconForComponent(Component component) {
-//		switch (component.type()) {
-//			case JAWS:
-//				return icon("jaws.png");
-//			case CHOPPER:
-//				return icon("chopper.png");
-//			case MONITOR:
-//				return icon("monitor.png");
-//			case SAMPLESTACK:
-//				return icon("sample_stack.png");
-//			case DAE:
-//				return icon("dae.png");
-//            case DANFSYIK:
-//			case KEPCO:
-//            case TDK_LAMBDA_GENESYS:
-//			case CAEN:
-//				return icon("caen.png");
-//			case BEAMSTOP:
-//				return icon("beamstop.png");
-//			case MOVINGMONITOR:
-//			case MOVINGBEAMSTOP:
-//				return icon("moving_monitor.png");
-//			case SAMPLECHANGER:
-//				return icon("sample_changer.png");
-//			case ANALYSER:
-//				return icon("analyser.png");
-//			case POLARISER:
-//				return icon("polariser.png");
-//			case ROTATINGBENCH:
-//				return icon("rotating_bench.png");
-//            case JULABO:
-//				return icon("julabo.png");
-//			case EUROTHERM:
-//				return icon("euro.png");
-//			case GONIOMETER:
-//				return icon("ALF Gonio.png");
-//			case PINHOLESELECTOR:
-//				return icon("pinhole_selector.png");
-//			case SINGLESTAGE:
-//				return icon("single_stage.png");
-//			default:
-//				return icon("cog.png");
-//		}
-//	}
-
     public static Image iconForType(ComponentType componentType) {
         String iconName = componentType == ComponentType.GONIOMETER ? "ALF Gonio" : getIconNameForType(componentType);
         return icon(iconName + ".png");
