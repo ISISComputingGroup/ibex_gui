5b1e895d74a6ea9b5e09066b89ce4b37259e71b1
IsabellaRey <isabella.rey@stfc.ac.uk>
2016-05-25 11:59:31 +0100

    Ticket 1179: added test for delete initialisation

--- base/uk.ac.stfc.isis.ibex.synoptic.tests/src/uk/ac/stfc/isis/ibex/synoptic/tests/internal/VariablesTest.java
+++ base/uk.ac.stfc.isis.ibex.synoptic.tests/src/uk/ac/stfc/isis/ibex/synoptic/tests/internal/VariablesTest.java
@@ -23,6 +23,9 @@ import static org.junit.Assert.*;
 import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -47,6 +50,7 @@ public class VariablesTest {
     private final String synopticPV = "Synoptic PV";
     private static final String SYNOPTIC_ADDRESS = "CS:BLOCKSERVER:SYNOPTICS:";
     private static final String SET_DETAILS = "SET_DETAILS";
+    private static final String DELETE = "DELETE";
 
     private Variables variables;
     private WritableFactory closingWritableFactory;
@@ -106,6 +110,29 @@ public class VariablesTest {
         assertEquals(expectedResult, variables.synopticSetter);
         assertNotEquals(mockWritable, variables.synopticSetter);
 	}
+
+    @Test
+    public void
+            synopticsDeleter_is_initialised_as_writable_converting_string_collection_to_json_string_to_correct_pv() {
+        // Arrange
+        Writable expectedDestination = mock(Writable.class);
+        when(switchingWritableFactory.getSwitchableWritable(any(ChannelType.class),
+                eq(pvPrefix + SYNOPTIC_ADDRESS + DELETE))).thenReturn(expectedDestination);
+
+        Collection<String> inputValue = new ArrayList<String>();
+        inputValue.add("name 1");
+        inputValue.add("name 2");
+        String convertedValue = "[\"name 1\",\"name 2\"]";
+
+        // Act
+        variables = createVariables();
+
+        // Assert
+        Writable deleter = variables.synopticsDeleter;
+        verify(expectedDestination, never()).write(any());
+        deleter.write(inputValue);
+        verify(expectedDestination, times(1)).write(convertedValue);
+    }
 
 //	/**
 //	 * Test method for {@link uk.ac.stfc.isis.ibex.synoptic.internal.Variables#toSynopticInfo()}.
