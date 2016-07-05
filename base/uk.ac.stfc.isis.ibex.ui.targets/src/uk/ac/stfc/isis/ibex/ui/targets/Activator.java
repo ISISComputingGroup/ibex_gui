3e0a14f36c3ffe4b2bcabfc0a62bff964517841a
AdrianPotter <adrian.potter@tessella.com>
2016-07-05 14:41:53 +0100

    Update activators

--- base/uk.ac.stfc.isis.ibex.ui.targets/src/uk/ac/stfc/isis/ibex/ui/targets/Activator.java
+++ base/uk.ac.stfc.isis.ibex.ui.targets/src/uk/ac/stfc/isis/ibex/ui/targets/Activator.java
@@ -19,66 +19,51 @@
 
 package uk.ac.stfc.isis.ibex.ui.targets;
 
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 /**
- * The activator class controls the plug-in life cycle.
+ * The activator for the plug-in.
+ *
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator implements BundleActivator {
 
-    /**
-     * The plug-in ID.
-     */
-    public static final String PLUGIN_ID = "uk.ac.stfc.isis.ibex.ui.targets"; //$NON-NLS-1$
+    private static BundleContext context;
 
-    /**
-     * The shared instance
-     */
-    private static Activator plugin;
+    private static Activator instance;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.
-     * BundleContext)
-     */
-    @Override
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        plugin = this;
+    public Activator() {
+        instance = this;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.
-     * BundleContext)
-     */
-    @Override
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        super.stop(context);
-    }
-
-    /**
-     * Returns the shared instance.
-     *
-     * @return the shared instance
-     */
     public static Activator getDefault() {
-        return plugin;
+        return instance;
     }
 
-    /**
-     * Returns an image descriptor for the image file at the given plug-in
-     * relative path.
-     *
-     * @param path the path
-     * @return the image descriptor
-     */
-    public static ImageDescriptor getImageDescriptor(String path) {
-        return imageDescriptorFromPlugin(PLUGIN_ID, path);
+    static BundleContext getContext() {
+        return context;
     }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.
+     * BundleContext)
+     */
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        Activator.context = bundleContext;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        Activator.context = null;
+    }
+
 }
