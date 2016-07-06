0645d7e43a0946e74a3982da0d9c0ccc95a00cf2
IsabellaRey <isabella.rey@stfc.ac.uk>
2016-07-06 17:03:08 +0100

    Ticket 1122: DeviceScreens perspective not visible by default until ready to deploy

--- base/uk.ac.stfc.isis.ibex.ui.devicescreens/src/uk/ac/stfc/isis/ibex/ui/devicescreens/Perspective.java
+++ base/uk.ac.stfc.isis.ibex.ui.devicescreens/src/uk/ac/stfc/isis/ibex/ui/devicescreens/Perspective.java
@@ -52,4 +52,9 @@ public class Perspective extends BasePerspective {
 		// from Flaticon http://www.flaticon.com
 		// is licensed by Creative Commons BY 3.0 http://creativecommons.org/licenses/by/3.0/
 	}
+
+    @Override
+    public boolean isVisibleDefault() {
+        return false;
+    }
 }
