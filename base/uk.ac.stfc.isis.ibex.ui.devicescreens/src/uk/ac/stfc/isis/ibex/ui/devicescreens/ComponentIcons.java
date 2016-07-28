53eaa51b9af41900f2e64d52b6de7177cd0379cb
IsabellaRey <isabella.rey@stfc.ac.uk>
2016-07-28 15:02:06 +0100

    Ticket 1404: Added OPI for Lakeshore 336

--- base/uk.ac.stfc.isis.ibex.ui.devicescreens/src/uk/ac/stfc/isis/ibex/ui/devicescreens/ComponentIcons.java
+++ base/uk.ac.stfc.isis.ibex.ui.devicescreens/src/uk/ac/stfc/isis/ibex/ui/devicescreens/ComponentIcons.java
@@ -106,6 +106,8 @@ public final class ComponentIcons {
                 return "mercury";
             case MUON_FRONT_END:
                 return "muon_front_end";
+            case LAKESHORE:
+                return "lakeshore";
             default:
                 return "cog";
         }
