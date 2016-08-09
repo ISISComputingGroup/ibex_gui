b9f3372599cd1f0b0423ea38b9bba33d8b97e188
simonfernandes <simon.fernandes@stfc.ac.uk>
2016-08-09 10:32:15 +0100

    create blank perspective

---
+++ base/uk.ac.stfc.isis.ibex.ui.scriptgenerator/src/uk/ac/stfc/isis/ibex/ui/scriptgenerator/ScriptGeneratorView.java
@@ -0,0 +1,26 @@
+package uk.ac.stfc.isis.ibex.ui.scriptgenerator;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.SWT;
+
+public class ScriptGeneratorView extends ViewPart {
+	public static final String ID = "uk.ac.stfc.isis.ibex.ui.scriptgenerator.scriptgeneratorview";
+	 
+	public ScriptGeneratorView() {
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		
+		Browser browser = new Browser(parent, SWT.NONE);
+		
+	}
+
+	@Override
+	public void setFocus() {
+		
+	}
+
+}
