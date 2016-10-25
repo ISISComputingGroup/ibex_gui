c15bd4ca13e3cccd0f9a9334a7688c5343053ae7
John-Holt-Tessella <John.Holt@tessella.com>
2016-10-25 11:28:30 +0100

    Update tests

--- base/uk.ac.stfc.isis.ibex.ui.logplotter.tests/src/uk/ac/stfc/isis/ibex/ui/logplotter/tests/LogPlotterSettingsTest.java
+++ base/uk.ac.stfc.isis.ibex.ui.logplotter.tests/src/uk/ac/stfc/isis/ibex/ui/logplotter/tests/LogPlotterSettingsTest.java
@@ -75,7 +75,7 @@ public class LogPlotterSettingsTest {
      *         instrument.
      */
     private static String BuildArchiveSettings(String instrumentName) {
-        return "RDB|1|jdbc\\:mysql\\://" + instrumentName + "/archive*RDB|2|jdbc\\:mysql\\://130.246.39.152/archive";
+        return "RDB|1|jdbc:mysql://" + instrumentName + "/archive*RDB|2|jdbc:mysql://130.246.39.152/archive";
     }
 
     /**
@@ -83,7 +83,7 @@ public class LogPlotterSettingsTest {
      * @return The URLs settings string corresponding to the given instrument.
      */
     private static String BuildUrlsSettings(String instrumentName) {
-        return "jdbc\\:mysql\\://" + instrumentName + "/archive*jdbc\\:mysql\\://130.246.39.152/archive";
+        return "jdbc:mysql://" + instrumentName + "/archive*jdbc:mysql://130.246.39.152/archive";
     }
 
     // These settings represent the defaults, as set in
