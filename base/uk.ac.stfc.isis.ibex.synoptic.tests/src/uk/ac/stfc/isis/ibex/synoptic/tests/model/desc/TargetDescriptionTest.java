8c970027a3bb24262399a991819403ab0dd9146c
AdrianPotter <adrian.potter@tessella.com>
2016-12-06 16:32:47 +0000

    Fix various checkstyle warnings

--- base/uk.ac.stfc.isis.ibex.synoptic.tests/src/uk/ac/stfc/isis/ibex/synoptic/tests/model/desc/TargetDescriptionTest.java
+++ base/uk.ac.stfc.isis.ibex.synoptic.tests/src/uk/ac/stfc/isis/ibex/synoptic/tests/model/desc/TargetDescriptionTest.java
@@ -31,7 +31,7 @@ import uk.ac.stfc.isis.ibex.synoptic.model.desc.Property;
 import uk.ac.stfc.isis.ibex.synoptic.model.desc.TargetDescription;
 import uk.ac.stfc.isis.ibex.synoptic.model.desc.TargetType;
 
-@SuppressWarnings("checkstyle:methodname")
+@SuppressWarnings("checkstyle:methodname checkstyle:magicnumber")
 public class TargetDescriptionTest {
     
     private static final String NAME = "name";
@@ -198,7 +198,8 @@ public class TargetDescriptionTest {
         // Assert
         Property defaultProperty = new Property();
         assertSame(newProperty, source.getProperty(new_key, defaultProperty));
-        assertEquals("Number of properties in the list", 3, source.getProperties().size());
+        final int expected_number_of_properties = 3;
+        assertEquals("Number of properties in the list", expected_number_of_properties, source.getProperties().size());
     }
 
 }
--- base/uk.ac.stfc.isis.ibex.ui.widgets/src/uk/ac/stfc/isis/ibex/ui/widgets/states/RecordLabelState.java
+++ base/uk.ac.stfc.isis.ibex.ui.widgets/src/uk/ac/stfc/isis/ibex/ui/widgets/states/RecordLabelState.java
@@ -25,8 +25,8 @@ import org.eclipse.wb.swt.ResourceManager;
 import org.eclipse.wb.swt.SWTResourceManager;
 
 public enum RecordLabelState {
-	DISCONNECTED (255, 0, 0, ResourceManager.getPluginImage("uk.ac.stfc.isis.ibex.ui.widgets", "icons/warning_red.png")),	// Red
-	CONNECTED (0, 0, 0, null);																							// Black
+    DISCONNECTED(255, 0, 0, ResourceManager.getPluginImage("uk.ac.stfc.isis.ibex.ui.widgets", "icons/warning_red.png")), // Red
+    CONNECTED(0, 0, 0, null); // Black
 	
 	private final Color color;
 	private final Image image;
