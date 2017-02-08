ef96a986d0b9553c28491722f0fb0c5dda4cc00e
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-02-08 16:46:55 +0000

    add missing files

---
+++ base/uk.ac.stfc.isis.ibex.ui.devicescreens/src/uk/ac/stfc/isis/ibex/ui/devicescreens/models/ViewDeviceScreensDescriptionViewModel.java
@@ -0,0 +1,62 @@
+ /*
+ * This file is part of the ISIS IBEX application.
+ * Copyright (C) 2012-2016 Science & Technology Facilities Council.
+ * All rights reserved.
+ *
+ * This program is distributed in the hope that it will be useful.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution.
+ * EXCEPT AS EXPRESSLY SET FORTH IN THE ECLIPSE PUBLIC LICENSE V1.0, THE PROGRAM 
+ * AND ACCOMPANYING MATERIALS ARE PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES 
+ * OR CONDITIONS OF ANY KIND.  See the Eclipse Public License v1.0 for more details.
+ *
+ * You should have received a copy of the Eclipse Public License v1.0
+ * along with this program; if not, you can obtain a copy from
+ * https://www.eclipse.org/org/documents/epl-v10.php or 
+ * http://opensource.org/licenses/eclipse-1.0.php
+ */
+
+/**
+ * 
+ */
+package uk.ac.stfc.isis.ibex.ui.devicescreens.models;
+
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.beans.BeanProperties;
+
+import uk.ac.stfc.isis.ibex.devicescreens.DeviceScreensModel;
+import uk.ac.stfc.isis.ibex.devicescreens.desc.DeviceScreensDescription;
+import uk.ac.stfc.isis.ibex.model.ModelObject;
+
+/**
+ * The view model for the main device screens panel.
+ */
+public class ViewDeviceScreensDescriptionViewModel extends ModelObject {
+
+    private DeviceScreensModel deviceScreensModel;
+    private DeviceScreensDescription deviceScreensDescription;
+    private final DataBindingContext bindingContext = new DataBindingContext();
+
+    public ViewDeviceScreensDescriptionViewModel(DeviceScreensModel deviceScreensModel) {
+        this.deviceScreensModel = deviceScreensModel;
+        bindingContext.bindValue(BeanProperties.value("deviceScreensDescription").observe(this),
+                BeanProperties.value("deviceScreensDescription").observe(this.deviceScreensModel));
+    }
+
+    /**
+     * @return the deviceScreensModel
+     */
+    public DeviceScreensDescription getDeviceScreensDescription() {
+        return deviceScreensDescription;
+    }
+
+    /**
+     * @param deviceScreensModel the deviceScreensModel to set
+     */
+    public void setDeviceScreensDescription(DeviceScreensDescription deviceScreensDescription) {
+        firePropertyChange("deviceScreensDescription", this.deviceScreensDescription,
+                this.deviceScreensDescription = deviceScreensDescription);
+    }
+
+
+}
