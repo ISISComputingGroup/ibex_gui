dc5c722b90fea02e241d57afa456b1c2cc8f399b
AdrianPotter <adrian.potter@tessella.com>
2017-02-09 09:49:44 +0000

    Add button to help to reset all switchable PVs by switching instrument to self

---
+++ base/uk.ac.stfc.isis.ibex.ui.help/src/uk/ac/stfc/isis/ibex/ui/help/ResetHandler.java
@@ -0,0 +1,41 @@
+
+/*
+* This file is part of the ISIS IBEX application.
+* Copyright (C) 2012-2015 Science & Technology Facilities Council.
+* All rights reserved.
+*
+* This program is distributed in the hope that it will be useful.
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v1.0 which accompanies this distribution.
+* EXCEPT AS EXPRESSLY SET FORTH IN THE ECLIPSE PUBLIC LICENSE V1.0, THE PROGRAM 
+* AND ACCOMPANYING MATERIALS ARE PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES 
+* OR CONDITIONS OF ANY KIND.  See the Eclipse Public License v1.0 for more details.
+*
+* You should have received a copy of the Eclipse Public License v1.0
+* along with this program; if not, you can obtain a copy from
+* https://www.eclipse.org/org/documents/epl-v10.php or 
+* http://opensource.org/licenses/eclipse-1.0.php
+*/
+
+package uk.ac.stfc.isis.ibex.ui.help;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+import uk.ac.stfc.isis.ibex.instrument.Instrument;
+
+/**
+ * The handler for resetting all switching observable PVs in case they're not
+ * responding.
+ */
+public class ResetHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent e) throws ExecutionException {
+        final Instrument instrument = Instrument.getInstance();
+        instrument.setInstrumentForAllPlugins(instrument.currentInstrument());
+        return null;
+	}
+
+}
