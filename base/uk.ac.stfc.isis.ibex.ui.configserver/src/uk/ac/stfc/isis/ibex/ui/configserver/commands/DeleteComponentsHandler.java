ba6ffb0adaa219e2ddd718e1842861eb87ac5d2b
Thomas Lohnert <thomas.lohnert@stfc.ac.uk>
2017-02-13 14:20:42 +0000

    Deletion dialog has dependency information

--- base/uk.ac.stfc.isis.ibex.ui.configserver/src/uk/ac/stfc/isis/ibex/ui/configserver/commands/DeleteComponentsHandler.java
+++ base/uk.ac.stfc.isis.ibex.ui.configserver/src/uk/ac/stfc/isis/ibex/ui/configserver/commands/DeleteComponentsHandler.java
@@ -28,18 +28,22 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.window.Window;
 
 import uk.ac.stfc.isis.ibex.configserver.configuration.ConfigInfo;
+import uk.ac.stfc.isis.ibex.epics.observing.BaseObserver;
 import uk.ac.stfc.isis.ibex.ui.configserver.dialogs.MultipleConfigsSelectionDialog;
 
 /**
  * Handles the selection of the delete components menu item.
  */
 public class DeleteComponentsHandler extends DisablingConfigHandler<Collection<String>> {
+    
+    private Map<String, Collection<String>> dependencies = new HashMap<>();
 	
     /**
      * The constructor.
      */
 	public DeleteComponentsHandler() {
 		super(SERVER.deleteComponents());
+        createObservers(SERVER.componentsInfo().getValue());
 	}
 	
 	@Override
@@ -48,33 +52,91 @@ public class DeleteComponentsHandler extends DisablingConfigHandler<Collection<S
                 SERVER.componentsInfo().getValue(), true, false);
 		if (dialog.open() == Window.OK) {
             Collection<String> toDelete = dialog.selectedConfigs();
-            Map<String, Collection<String>> dependencies = getDependencies(toDelete);
-            if (dependencies.size() == 0) {
-                configService.write(toDelete);
+            Map<String, Collection<String>> selectedDependencies = filterUnselected(toDelete);
+            System.out.println(selectedDependencies);
+            if (checkNoDependencies(selectedDependencies)) {
+//              configService.write(toDelete);
+                System.out.println("deleting: " + selectedDependencies);
             } else {
-                // TODO create dialog showing conflicts
+                System.out.println("nope: " + selectedDependencies);
             }
 		}
 		
 		return null;
 	}
 
-    private Map<String, Collection<String>> getDependencies(Collection<String> names) {
-        Map<String, Collection<String>> result = new HashMap<String, Collection<String>>();
-        for (String name : names) {
+    /**
+     * Creates observers for each component's dependencies PV and stores values
+     * in a local hashmap.
+     * 
+     * @param components
+     *            The available components.
+     */
+    private void createObservers(Collection<ConfigInfo> components) {
+        Collection<String> names = ConfigInfo.names(components);
+        for (final String name : names) {
             String pv = getCompPV(name);
-            Collection<String> dependencies = SERVER.dependencies(pv).getValue();
-            result.put(name, dependencies);
+            if (pv == null) {
+                continue; // TODO throw exception
+            }
+            SERVER.dependencies(pv).addObserver(new BaseObserver<Collection<String>>() {
+                
+                @Override
+                public void onValue(java.util.Collection<String> value) {
+                    dependencies.put(name, value);
+                };
+            });
         }
-        return result;
     }
 
+    /**
+     * Finds the dynamic blockserver PV of a given component based on its name.
+     * 
+     * @param name
+     *            The name of the component.
+     * @return The PV associated to the component.
+     */
     private String getCompPV(String name) {
         for (ConfigInfo comp : SERVER.componentsInfo().getValue()) {
             if (comp.name().equals(name)) {
                 return comp.pv();
             }
         }
-        return "";
+        return null;
+    }
+
+    /**
+     * Filters dependencies for unselected components from the map of all
+     * dependencies.
+     * 
+     * @param selected
+     *            The list of names of selected components to delete.
+     * @return A map containing dependencies for selected components only.
+     */
+    private Map<String, Collection<String>> filterUnselected(Collection<String> selected) {
+        Map<String, Collection<String>> result = new HashMap<String, Collection<String>>();
+        for (String key : dependencies.keySet()) {
+            if (selected.contains(key)) {
+                result.put(key, dependencies.get(key));
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Checks whether any component in a given map has any dependencies.
+     * 
+     * @param dependencyMap
+     *            The map of component dependencies.
+     * @return True if no dependencies for any component in the map, false
+     *         otherwise.
+     */
+    private boolean checkNoDependencies(Map<String, Collection<String>> dependencyMap) {
+        for (Collection<String> dependencies : dependencyMap.values()) {
+            if (dependencies.size() > 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
