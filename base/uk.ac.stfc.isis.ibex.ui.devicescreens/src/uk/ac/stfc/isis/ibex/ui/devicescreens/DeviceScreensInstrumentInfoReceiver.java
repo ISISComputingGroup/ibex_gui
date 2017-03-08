e35d6f4fb237a4581e32db9f490ecdf2e3a2090b
AdrianPotter <adrian.potter@tessella.com>
2017-03-08 16:14:15 +0000

    Add instrument receiver to coordinate closing screens

---
+++ base/uk.ac.stfc.isis.ibex.ui.devicescreens/src/uk/ac/stfc/isis/ibex/ui/devicescreens/DeviceScreensInstrumentInfoReceiver.java
@@ -0,0 +1,49 @@
+
+/*
+* This file is part of the ISIS IBEX application.
+* Copyright (C) 2012-2015 Science & Technology Facilities Council.
+* All rights reserved.
+*
+* This program is distributed in the hope that it will be useful.
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v1.0 which accompanies this distribution.
+* EXCEPT AS EXPRESSLY SET FORTH IN THE ECLIPSE PUBLIC LICENSE V1.0, THE PROGRAM 
+* AND ACCOMPANYING MATERIALS ARE PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES 
+* OR CONDITIONS OF ANY KIND.  See the Eclipse Public License v1.0 for more details.
+*
+* You should have received a copy of the Eclipse Public License v1.0
+* along with this program; if not, you can obtain a copy from
+* https://www.eclipse.org/org/documents/epl-v10.php or 
+* http://opensource.org/licenses/eclipse-1.0.php
+*/
+
+package uk.ac.stfc.isis.ibex.ui.devicescreens;
+
+import uk.ac.stfc.isis.ibex.instrument.InstrumentInfo;
+import uk.ac.stfc.isis.ibex.instrument.InstrumentInfoReceiver;
+
+/**
+ * Performs actions of the device screens when the instrument is changed.
+ */
+public class DeviceScreensInstrumentInfoReceiver implements InstrumentInfoReceiver {
+
+    @Override
+    public void setInstrument(InstrumentInfo instrument) {
+        // nothing to do on set instrument
+    }
+
+    /**
+     * Close the open OPIs they will refer to the old instrument.
+     * 
+     * @param instrument the instrument being switched to
+     */
+    @Override
+    public void preSetInstrument(InstrumentInfo instrument) {
+        DevicesOpiTargetView.closeAllOPIs();
+    }
+
+    @Override
+    public void postSetInstrument(InstrumentInfo instrument) {
+        // nothing to do on post set instrument
+    }
+}
