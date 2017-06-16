a04b3458bb8fbf548258b5575d1858014445f4a3
David Keymer <david.keymer@stfc.ac.uk>
2017-06-16 11:20:29 +0100

    Update copyright year

--- base/uk.ac.stfc.isis.ibex.ui.perspectives/src/uk/ac/stfc/isis/ibex/ui/perspectives/switcher/FlashingButton.java
+++ base/uk.ac.stfc.isis.ibex.ui.perspectives/src/uk/ac/stfc/isis/ibex/ui/perspectives/switcher/FlashingButton.java
@@ -1,7 +1,7 @@
 
 /*
 * This file is part of the ISIS IBEX application.
-* Copyright (C) 2012-2017 Science & Technology Facilities Council.
+* Copyright (C) 2012-2015 Science & Technology Facilities Council.
 * All rights reserved.
 *
 * This program is distributed in the hope that it will be useful.
