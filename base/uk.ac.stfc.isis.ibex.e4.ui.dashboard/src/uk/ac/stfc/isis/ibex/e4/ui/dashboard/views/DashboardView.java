3bed194d7ff9e54d176f7f9200c80ebe4ddbed78
AdrianPotter <adrian.potter@tessella.com>
2017-07-06 11:18:42 +0100

    Make 4-part layout

--- base/uk.ac.stfc.isis.ibex.e4.ui.dashboard/src/uk/ac/stfc/isis/ibex/e4/ui/dashboard/views/DashboardView.java
+++ base/uk.ac.stfc.isis.ibex.e4.ui.dashboard/src/uk/ac/stfc/isis/ibex/e4/ui/dashboard/views/DashboardView.java
@@ -1,32 +1,23 @@
-
-/*
-* This file is part of the ISIS IBEX application.
-* Copyright (C) 2012-2015 Science & Technology Facilities Council.
-* All rights reserved.
-*
-* This program is distributed in the hope that it will be useful.
-* This program and the accompanying materials are made available under the
-* terms of the Eclipse Public License v1.0 which accompanies this distribution.
-* EXCEPT AS EXPRESSLY SET FORTH IN THE ECLIPSE PUBLIC LICENSE V1.0, THE PROGRAM 
-* AND ACCOMPANYING MATERIALS ARE PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES 
-* OR CONDITIONS OF ANY KIND.  See the Eclipse Public License v1.0 for more details.
-*
-* You should have received a copy of the Eclipse Public License v1.0
-* along with this program; if not, you can obtain a copy from
-* https://www.eclipse.org/org/documents/epl-v10.php or 
-* http://opensource.org/licenses/eclipse-1.0.php
-*/
-
+ 
 package uk.ac.stfc.isis.ibex.e4.ui.dashboard.views;
 
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
 
-/**
- * The dashboard view.
- */
-public class DashboardView  {
-	public static final String ID = "uk.ac.stfc.isis.ibex.ui.e4.dashboard.views.DashboardView"; //$NON-NLS-1$
-
+public class DashboardView {
+	@Inject
 	public DashboardView() {
-		System.out.println("Creating dashboard view");
+		System.out.println("Initialising dashboard view");
+		
 	}
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		System.out.println("Creating dashboard view");		
+	}
+	
+	
+	
+	
 }
