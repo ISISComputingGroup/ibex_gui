855ae745dd98efd30b88dd64c8e4a6989f7320e8
AdrianPotter <adrian.potter@tessella.com>
2017-07-12 12:40:22 +0100

    Add perspective switcher icons

--- base/uk.ac.stfc.isis.ibex.e4.ui.perspectiveswitcher/src/uk/ac/stfc/isis/ibex/e4/ui/perspectiveswitcher/views/PerspectiveSwitcherView.java
+++ base/uk.ac.stfc.isis.ibex.e4.ui.perspectiveswitcher/src/uk/ac/stfc/isis/ibex/e4/ui/perspectiveswitcher/views/PerspectiveSwitcherView.java
@@ -4,34 +4,23 @@ import javax.inject.Inject;
 
 import javax.annotation.PostConstruct;
 
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IExtensionRegistry;
-import org.eclipse.e4.core.contexts.IEclipseContext;
-import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.model.application.ui.MElementContainer;
-import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
-import org.eclipse.e4.ui.model.application.ui.basic.MPart;
-import org.eclipse.e4.ui.workbench.IWorkbench;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
+import org.eclipse.wb.swt.ResourceManager;
 
-import uk.ac.stfc.isis.ibex.e4.perspectiveswitcher.PerspectiveSwitcher;
-
-@SuppressWarnings("unused")
 public class PerspectiveSwitcherView {
 	
 	private ToolBar toolBar;
-	private PerspectiveSwitcher perspectiveSwitcher = new PerspectiveSwitcher();
 
 	@Inject
 	public PerspectiveSwitcherView() {
@@ -45,15 +34,17 @@ public class PerspectiveSwitcherView {
 		
 		toolBar = new ToolBar(composite, SWT.VERTICAL);
 		
-		addPerspectiveShortcut("Alarms", "uk.ac.stfc.isis.ibex.client.e4.product.perspective.alarms", app, partService, modelService);
-		addPerspectiveShortcut("Beam status", "uk.ac.stfc.isis.ibex.client.e4.product.perspective.beamstatus", app, partService, modelService);
-		addPerspectiveShortcut("DAE", "uk.ac.stfc.isis.ibex.client.e4.product.perspective.dae", app, partService, modelService);
+		addPerspectiveShortcut("Alarms", "uk.ac.stfc.isis.ibex.client.e4.product.perspective.alarms", "icons/AlarmIcon.png", app, partService, modelService);
+		addPerspectiveShortcut("Beam status", "uk.ac.stfc.isis.ibex.client.e4.product.perspective.beamstatus", "icons/BeamStatusIcon.png", app, partService, modelService);
+		addPerspectiveShortcut("DAE", "uk.ac.stfc.isis.ibex.client.e4.product.perspective.dae", "icons/DAEIcon.png", app, partService, modelService);
 	}
 
-	public void addPerspectiveShortcut(String name, String partId, MApplication app, EPartService partService, EModelService modelService) {
+	public void addPerspectiveShortcut(String name, String partId, String iconPath, MApplication app, EPartService partService, EModelService modelService) {
 		ToolItem shortcut = new ToolItem(toolBar, SWT.RADIO);
 		shortcut.setText(name);
 		shortcut.setToolTipText(name);
+		Image img = ResourceManager.getPluginImage("uk.ac.stfc.isis.ibex.e4.ui", iconPath);
+		shortcut.setImage(img);
 		shortcut.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent event) {
@@ -62,4 +53,4 @@ public class PerspectiveSwitcherView {
 			}	
 		});
 	}
-};
+}
