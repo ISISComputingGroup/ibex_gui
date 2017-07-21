f08c47f3ee12af13def619ece366cadf2f94b766
AdrianPotter <adrian.potter@tessella.com>
2017-07-21 14:24:39 +0100

    Move handler next to view

--- base/uk.ac.stfc.isis.ibex.e4.ui.configeditor/src/uk/ac/stfc/isis/ibex/e4/ui/configeditor/EditCurrentConfigHandler.java
+++ base/uk.ac.stfc.isis.ibex.e4.ui.configeditor/src/uk/ac/stfc/isis/ibex/e4/ui/configeditor/EditCurrentConfigHandler.java
@@ -1,10 +1,8 @@
-package uk.ac.stfc.isis.ibex.e4.ui.handlers;
+package uk.ac.stfc.isis.ibex.e4.ui.configeditor;
 
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.jface.window.IShellProvider;
-
-import uk.ac.stfc.isis.ibex.e4.ui.configeditor.ConfigEditorDialog;
 
 public class EditCurrentConfigHandler {
 	
