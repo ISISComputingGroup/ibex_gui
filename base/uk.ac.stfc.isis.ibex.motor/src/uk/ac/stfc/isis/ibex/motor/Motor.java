f335a7ec10499a3e0e8381f994cd7c85acf72501
Matt Clarke <matt.clarke@stfc.ac.uk>
2017-10-17 15:58:20 +0100

    Checkstyle fixes

--- base/uk.ac.stfc.isis.ibex.motor/src/uk/ac/stfc/isis/ibex/motor/Motor.java
+++ base/uk.ac.stfc.isis.ibex.motor/src/uk/ac/stfc/isis/ibex/motor/Motor.java
@@ -21,31 +21,99 @@ package uk.ac.stfc.isis.ibex.motor;
 
 import uk.ac.stfc.isis.ibex.model.ModelObject;
 
+/**
+ * The abstract motor class.
+ */
 public abstract class Motor extends ModelObject {
 
+    /**
+     * Gets the motor name.
+     * 
+     * @return the name
+     */
 	public abstract String name();
 	
+    /**
+     * Gets the motor address
+     * 
+     * @return the address
+     */
 	public abstract String motorAddress();
 
+    /**
+     * Gets the description.
+     * 
+     * @return the description
+     */
 	public abstract String getDescription();
 	
+    /**
+     * Gets the current setpoint.
+     * 
+     * @return the setpoint
+     */
 	public abstract MotorSetpoint getSetpoint();
 	
+    /**
+     * Gets whether the motor is enabled.
+     * 
+     * @return enabled or not
+     */
 	public abstract MotorEnable getEnabled();
 	
+    /**
+     * Gets the lower limit.
+     * 
+     * @return the lower limit
+     */
 	public abstract Double getLowerLimit();
 		
+    /**
+     * Gets the upper limit.
+     * 
+     * @return the upper limit
+     */
 	public abstract Double getUpperLimit();
 
+    /**
+     * Gets the motor direction.
+     * 
+     * @return the direction
+     */
 	public abstract MotorDirection getDirection();
 	
+    /**
+     * Gets whether the motor is moving.
+     * 
+     * @return whether it is moving
+     */
 	public abstract Boolean getMoving();
 	
+    /**
+     * Gets whether the motor is at the home position.
+     * 
+     * @return whether it is at home
+     */
 	public abstract Boolean getAtHome();
 
+    /**
+     * Gets whether the motor is at the lower limit switch.
+     * 
+     * @return whether it is at the limit switch
+     */
 	public abstract Boolean getAtLowerLimitSwtich();
 
+    /**
+     * Gets whether the motor is at the upper limit switch.
+     * 
+     * @return whether it is at the limit switch
+     */
 	public abstract Boolean getAtUpperLimitSwitch();
 	
+    /**
+     * Gets the status of the motor.
+     * 
+     * @return the status
+     */
 	public abstract String getStatus();
 }
