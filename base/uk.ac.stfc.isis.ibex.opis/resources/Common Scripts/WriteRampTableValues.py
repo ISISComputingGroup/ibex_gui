f3bec2924269fc2e50fbfcd1b2d9706d812104ff
esouthren <eilidhsouthren@gmail.com>
2017-10-19 16:03:40 +0100

    read/write file working

--- base/uk.ac.stfc.isis.ibex.opis/resources/Common Scripts/WriteRampTableValues.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/Common Scripts/WriteRampTableValues.py
@@ -1,7 +1,6 @@
 from org.csstudio.opibuilder.scriptUtil import PVUtil
 from org.csstudio.opibuilder.scriptUtil import ColorFontUtil
 
-
 table = display.getWidget("ramp_table").getTable()
 
 rates = display.getWidget("ramp_table_rates").getPV()
@@ -11,11 +10,12 @@ row = 1
 col = 1
 index = 0
 
+
+
 rates = PVUtil.getDoubleArray(rates)
 targets = PVUtil.getDoubleArray(targets)
 
 color = ColorFontUtil.getColorFromRGB(240,245,245)
-
 
 while targets[index] > 0:
 	table.setCellText(index, 0, "up to (Gauss)")	
@@ -29,19 +29,6 @@ while targets[index] > 0:
 		
 	index += 1
 
-
-
-def remove_zero_rows():
-	rows = table.getRowCount()
-	for x in range(0, rows):
-		
-		if float(table.getCellText(x,1)) == 0:
-			table.deleteRow(x)
-			if x < rows:
-				remove_zero_rows()
-			
-remove_zero_rows()
-			
 
 	
 	
