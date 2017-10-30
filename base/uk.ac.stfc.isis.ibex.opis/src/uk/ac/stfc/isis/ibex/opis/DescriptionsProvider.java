9f3f1ab753a4217730d5c0c36ba4a57f9da31ce3
Dominic Oram <dominic.oram@stfc.ac.uk>
2017-10-30 19:41:23 +0000

    Removed unnecessary conversion to string

--- base/uk.ac.stfc.isis.ibex.opis/src/uk/ac/stfc/isis/ibex/opis/DescriptionsProvider.java
+++ base/uk.ac.stfc.isis.ibex.opis/src/uk/ac/stfc/isis/ibex/opis/DescriptionsProvider.java
@@ -19,7 +19,6 @@
 
 package uk.ac.stfc.isis.ibex.opis;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -91,24 +90,10 @@ public class DescriptionsProvider extends Provider {
 	}
 	
 	private Descriptions importDescriptions() {
-		Path path = pathToFileResource("/resources/" + FILENAME);
-		StringBuilder sb = new StringBuilder();
-		
-        try (BufferedReader br = new BufferedReader(new FileReader(path.toOSString()))) {
-			String sCurrentLine;
- 
-			while ((sCurrentLine = br.readLine()) != null) {
-				sb.append(sCurrentLine);
-			}
- 
-		} catch (IOException e) {
-			e.printStackTrace();
-			return new Descriptions();
-		} 
-		
+        Path path = pathToFileResource("/resources/" + FILENAME);
 		try {
-            return XMLUtil.fromXml(sb.toString(), Descriptions.class);
-        } catch (JAXBException e) {
+            return XMLUtil.fromXml(new FileReader(path.toOSString()), Descriptions.class);
+        } catch (JAXBException | IOException e) {
 			e.printStackTrace();
 			return new Descriptions();
 		}
