547e6b38b97016606a4b6134af689f0588dc432b
Adrian Potter <Adrian.Potter@tessella.com>
2017-11-14 14:33:55 +0000

    Set motor macros with script

--- base/uk.ac.stfc.isis.ibex.opis/resources/sample_stack/setMotorMacros.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/sample_stack/setMotorMacros.py
@@ -3,13 +3,24 @@ from org.csstudio.opibuilder.scriptUtil import ConsoleUtil
 
 def is_unexpanded_macro(value):
     return len(value)>=3 and value[:2]=="$(" and value[-1]==")"
+    
+def display_available_attrs(o):
+    ConsoleUtil.writeInfo("\n".join(dir(o)))
+    
+def add_motor_macro(motor_index, target):
+    motor_widget = display.getWidget("Motor_" + str(motor_index))
+    motor_macros = motor_widget.getPropertyValue("macros")
+    motor_macros.put("MOTOR", target)
+    motor_widget.setPropertyValue("macros", motor_macros)
+    motor_widget.refresh()
 
 motor_names = [PVUtil.getString(pv) for pv in pvs[1:]]
+motor_macros = widget.getPropertyValue("macros")
+
+
 
 for i, name in enumerate(motor_names):
     if not is_unexpanded_macro(name):
-        ConsoleUtil.writeInfo(name)
         # Use enumerate for the odd case where named motors are not contiguous
-        naxes = i+1
-ConsoleUtil.writeInfo(str(naxes))
-pvs[0].setValue(naxes)
+        pvs[0].setValue(i+1)
+        add_motor_macro(i+1, name)
