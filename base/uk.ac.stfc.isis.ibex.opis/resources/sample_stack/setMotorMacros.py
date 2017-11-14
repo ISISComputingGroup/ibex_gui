17b92d58ede7a81a1c3dcce21027fc8653e640e6
Adrian Potter <Adrian.Potter@tessella.com>
2017-11-14 15:30:27 +0000

    Reload by unsetting/setting target OPI

--- base/uk.ac.stfc.isis.ibex.opis/resources/sample_stack/setMotorMacros.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/sample_stack/setMotorMacros.py
@@ -5,17 +5,11 @@ from time import sleep
 def is_unexpanded_macro(value):
     return len(value)>=3 and value[:2]=="$(" and value[-1]==")"
     
-def display_available_attrs(o):
-    ConsoleUtil.writeInfo("\n".join(dir(o)))
-    
-def print_info(o):
-    ConsoleUtil.writeInfo(str(o))
-    
 def get_pv_value(pv_name, widget, add_prefix=True, max_wait=1.0):
     if add_prefix:
         pv_name = display.getMacroValue("P") + pv_name
     pv = PVUtil.createPV(pv_name, widget)
-    steps = 10000
+    steps = 100
     for i in range(steps):
         try:
             sleep(max_wait/steps)
@@ -24,18 +18,29 @@ def get_pv_value(pv_name, widget, add_prefix=True, max_wait=1.0):
             pass
     return ""
     
-def add_motor_macro(motor_index, target):
+def reload_widget(widget):
+    # Reloading is achieved by unsetting and resetting the opi
+    widget.setPropertyValue("opi_file","null.opi");
+    widget.setPropertyValue("opi_file","motor_control.opi");
+    
+def add_motor_macro(motor_index, name):
     motor_widget = display.getWidget("Motor_" + str(motor_index))
+    
+    def get_motor_target():
+        return name + ":MOTOR"
+    
     motor_macros = motor_widget.getPropertyValue("macros")
-    motor_macros.put("MOTOR", get_pv_value(target + ":MOTOR", motor_widget))
+    motor_macros.put("MOTOR", get_pv_value(get_motor_target(), motor_widget))
     motor_widget.setPropertyValue("macros", motor_macros)
-    motor_widget.refresh()        
+    
+    reload_widget(motor_widget)
 
-motor_names = [PVUtil.getString(pv) for pv in pvs[1:]]
-motor_macros = widget.getPropertyValue("macros")
-
-for i, name in enumerate(motor_names):
-    if not is_unexpanded_macro(name):
-        # Use enumerate for the odd case where named motors are not contiguous
-        pvs[0].setValue(i+1)
-        add_motor_macro(i+1, name)
+def main():
+    motor_names = [PVUtil.getString(pv) for pv in pvs[1:]]
+    for i, name in enumerate(motor_names):
+        if not is_unexpanded_macro(name):
+            add_motor_macro(i+1, name)
+            naxes = i+1
+    pvs[0].setValue(naxes)
+            
+main()
