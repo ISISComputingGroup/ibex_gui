322c47c69bf8ae99740f50a3529552468889350c
Adrian Potter <Adrian.Potter@tessella.com>
2017-11-14 15:44:13 +0000

    Create PVs first to reduce wait time

--- base/uk.ac.stfc.isis.ibex.opis/resources/sample_stack/setMotorMacros.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/sample_stack/setMotorMacros.py
@@ -5,41 +5,45 @@ from time import sleep
 def is_unexpanded_macro(value):
     return len(value)>=3 and value[:2]=="$(" and value[-1]==")"
     
-def get_pv_value(pv_name, widget, add_prefix=True, max_wait=1.0):
-    if add_prefix:
-        pv_name = display.getMacroValue("P") + pv_name
-    pv = PVUtil.createPV(pv_name, widget)
+def get_pv_value(pv, max_wait=1.0):
     steps = 100
     for i in range(steps):
         try:
-            sleep(max_wait/steps)
             return PVUtil.getString(pv)
         except:
-            pass
-    return ""
+            sleep(max_wait/steps)
+    return None
     
 def reload_widget(widget):
     # Reloading is achieved by unsetting and resetting the opi
     widget.setPropertyValue("opi_file","null.opi");
     widget.setPropertyValue("opi_file","motor_control.opi");
     
-def add_motor_macro(motor_index, name):
-    motor_widget = display.getWidget("Motor_" + str(motor_index))
-    
-    def get_motor_target():
-        return name + ":MOTOR"
+def add_motor_macro(motor_index, pv):
+    motor_widget = display.getWidget("Motor_" + str(motor_index))    
     
     motor_macros = motor_widget.getPropertyValue("macros")
-    motor_macros.put("MOTOR", get_pv_value(get_motor_target(), motor_widget))
+    motor_macros.put("MOTOR", get_pv_value(pv))
     motor_widget.setPropertyValue("macros", motor_macros)
     
     reload_widget(motor_widget)
+    
+def get_motor_pvs():
+    motor_pvs = []
+    for name in [PVUtil.getString(pv) for pv in pvs[1:]]:
+        if is_unexpanded_macro(name):
+            motor_pvs.append(None)
+        else:
+            def get_motor_target():
+                return display.getMacroValue("P") + name + ":MOTOR"
+            motor_pvs.append(PVUtil.createPV(get_motor_target(), display))
+    return motor_pvs
 
 def main():
-    motor_names = [PVUtil.getString(pv) for pv in pvs[1:]]
-    for i, name in enumerate(motor_names):
-        if not is_unexpanded_macro(name):
-            add_motor_macro(i+1, name)
+    naxes = 0
+    for i, pv in enumerate(get_motor_pvs()):
+        if pv is not None:
+            add_motor_macro(i+1, pv)
             naxes = i+1
     pvs[0].setValue(naxes)
             
