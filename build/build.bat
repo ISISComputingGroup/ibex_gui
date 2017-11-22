6030745d03c4275bcd713b8f1aea217433616414
Matt Clarke <matt.clarke@stfc.ac.uk>
2017-11-22 12:09:21 +0000

    Update build.bat

--- build/build.bat
+++ build/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call c:\Python27\python.exe .\check_build.py ..\base\
+REM call c:\Python27\python.exe .\check_build.py ..\base\
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 call mvn --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.ibex.client.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% clean verify
