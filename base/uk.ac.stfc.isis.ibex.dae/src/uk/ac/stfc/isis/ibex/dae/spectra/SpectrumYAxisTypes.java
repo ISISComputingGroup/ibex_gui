5eef55b5820962187f2b60177989e5f960b13fe8
Tom Willemsen <tomwillemsen1995@gmail.com>
2017-11-28 11:53:36 +0000

    Rename axis

--- base/uk.ac.stfc.isis.ibex.dae/src/uk/ac/stfc/isis/ibex/dae/spectra/SpectrumYAxisTypes.java
+++ base/uk.ac.stfc.isis.ibex.dae/src/uk/ac/stfc/isis/ibex/dae/spectra/SpectrumYAxisTypes.java
@@ -7,7 +7,7 @@ import uk.ac.stfc.isis.ibex.ui.Utils;
  */
 public enum SpectrumYAxisTypes {
 	/* Enum constant indicating the Y axis is a count rate, in counts per microsecond. */
-	COUNT_RATE("Counts (/" + Utils.MU + "s)"),
+	COUNT_RATE("Counts/" + Utils.MU + "s"),
 	/* Enum constant indicating the Y axis is showing cumulative counts. */
 	ABSOLUTE_COUNTS("Counts");
 	
