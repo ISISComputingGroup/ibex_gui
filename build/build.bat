478d507c0f513df29b04b5cdc1c786002638ca1e
Dominic Oram <dominic.oram@stfc.ac.uk>
2017-12-07 16:55:00 +0000

    Fixed path in build.bat

--- build/build.bat
+++ build/build.bat
@@ -3,5 +3,5 @@
 call mvn --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.ibex.client.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% clean verify
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-@echo Client built in %~dp0..\base\uk.ac.stfc.isis.ibex.client.product\target\products\ibex.product\win32\win32\x86_64
+@echo Client built in %~dp0..\base\uk.ac.stfc.isis.ibex.e4.client.product\target\products\ibex.product\win32\win32\x86_64
 pause
