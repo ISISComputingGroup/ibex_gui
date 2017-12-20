f7652fa267d26c8d484e9084c41bd4d77fbf384f
Adrian Potter <Adrian.Potter@tessella.com>
2017-12-20 16:20:02 +0000

    Add extra logic to make more complex labels if necessary

--- base/uk.ac.stfc.isis.ibex.opis/resources/sample_stack/setMotorMacros.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/sample_stack/setMotorMacros.py
@@ -48,7 +48,7 @@ def _add_macro(motor_index, name, value):
     motor_widget.setPropertyValue("macros", motor_macros)
     
 def _get_axis_pv(index):
-	return display.getMacroValue(MACRO_ROOT + str(index))
+    return display.getMacroValue(MACRO_ROOT + str(index))
     
 def add_motor_macro(motor_index, pv):
     """
@@ -59,16 +59,16 @@ def add_motor_macro(motor_index, pv):
     motor_address = get_pv_value(pv)
     
     if motor_address is not None:
-    	if not motor_address.startswith("MOT:"):
-        	motor_address = "MOT:" + motor_address
+        if not motor_address.startswith("MOT:"):
+            motor_address = "MOT:" + motor_address
         _add_macro(motor_index, "MOTOR", motor_address)
     
-def add_label_macro(motor_index):
+def add_label_macro(motor_index, label):
     """
     Add the $(LABEL) macro to a motor widget. Widgets are identified as "Motor_n"
     for n in [1,8] in the parent OPI
     """
-    _add_macro(motor_index, "LABEL", _get_axis_pv(motor_index).split(":")[-1])
+    _add_macro(motor_index, "LABEL", label)
 
 def add_name_macro(motor_index):
     """
@@ -92,14 +92,43 @@ def get_motor_pvs():
             motor_pvs.append(PVUtil.createPV(display.getMacroValue("P") + name + ":MOTOR", display))
             
     return motor_pvs
+    
+def number_of_overlapping_final_elements(pv1, pv2):
+    if pv1==None or pv2==None or pv1==pv2:
+        return 0       
+
+    list1 = pv1.split(":")
+    list2 = pv2.split(":")
+    overlap = 0
+    for i in range(min(len(list1), len(list2))):
+         if list1[-1-i]==list2[-1-i]:
+             overlap += 1
+         else:
+             break
+    return overlap
+        
+def generate_labels():
+    labels = []
+    pvs = [_get_axis_pv(i+1) for i in range(8)]
+    for this_pv in pvs:
+        max_overlap = 0
+        if this_pv is not None:
+            for other_pv in pvs:
+                max_overlap = max(max_overlap, number_of_overlapping_final_elements(this_pv, other_pv))
+            label = " ".join(this_pv.split(":")[-1-max_overlap:])
+        else:
+            label = None
+        labels.append(label)
+    return labels
 
 def main():
     naxes = 0
+    labels = generate_labels()
     # enumerate( , 1) not supported in CSS Python
     for i, pv in enumerate(get_motor_pvs()): 
         if pv is not None:
             add_motor_macro(i+1, pv)
-            add_label_macro(i+1)
+            add_label_macro(i+1, labels[i])
             add_name_macro(i+1)
             naxes = i+1
     pvs[0].setValue(naxes)
