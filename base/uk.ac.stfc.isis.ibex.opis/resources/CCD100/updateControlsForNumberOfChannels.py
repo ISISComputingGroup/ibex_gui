1704e9a2363fb40f1b8451b2bd6a20625f05f93e
Adrian Potter <Adrian.Potter@tessella.com>
2017-12-20 12:01:03 +0000

    Make number of flows variable

---
+++ base/uk.ac.stfc.isis.ibex.opis/resources/CCD100/updateControlsForNumberOfChannels.py
@@ -0,0 +1,33 @@
+from org.csstudio.opibuilder.scriptUtil import PVUtil
+from org.csstudio.opibuilder.scriptUtil import ConsoleUtil
+from time import sleep
+
+def is_active_macro(value):
+    if value is None or len(value)==0:
+        return False  # Empty macro
+    elif len(value)>=3 and value[:2]=="$(" and value[-1]==")":
+        return False  # Unexpanded macro
+    else:
+        return True
+    
+def get_count():
+    count = 0
+    macros = display.getPropertyValue("macros").getMacrosMap()
+    for i in range(1,5):
+        if is_active_macro(macros.get("CCD_"+str(i))):
+            count = i
+    return count
+    
+def set_number_of_graph_traces(count):
+    graph = display.getWidget("GasFlowGraph")
+    graph.setPropertyValue("trace_count", count)
+    for index in range(count):
+        graph.setPropertyValue("trace_"+str(index)+"_y_pv", "$(P)$(CCD_"+str(index+1)+"):READING")
+        graph.setPropertyValue("trace_"+str(index)+"_name", "Gas "+str(index))
+
+def main():
+    count = get_count()
+    pvs[0].setValue(count)
+    set_number_of_graph_traces(count)
+            
+main()
