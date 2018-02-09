eae0151c31189a2525e82a2871a90e5773b075ac
Adrian Potter <Adrian.Potter@tessella.com>
2018-02-09 17:08:26 +0000

    Process status

--- base/uk.ac.stfc.isis.ibex.opis/resources/Common Scripts/binarySwitch.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/Common Scripts/binarySwitch.py
@@ -1,10 +1,15 @@
 from org.csstudio.opibuilder.scriptUtil import PVUtil
+from org.csstudio.opibuilder.scriptUtil import ConsoleUtil
+from time import sleep
 
 remote_pv = widget.getPV();
-setpoint_pv = PVUtil.createPV(widget.getProperty("pv_name")+":SP", widget)
+setpoint_pv = PVUtil.createPV(widget.getPropertyValue("pv_name")+":SP", widget)
 current_value = int(PVUtil.getDouble(remote_pv))
-
-if current_value == 0:
-	setpoint_pv.setValue(1)
+for _ in range(10):
+    if remote_pv.isConnected() and setpoint_pv.isConnected():
+        setpoint_pv.setValue(1 if current_value==0 else 0)
+        break
+    else:
+        sleep(0.1)
 else:
-	setpoint_pv.setValue(1)
+    ConsoleUtil.writeError("Error: Unable to connect to rate PVs")
