0492c8de03578c98138bbf0118c71ecd2308548d
esouthren <eilidhsouthren@gmail.com>
2018-02-20 16:44:03 +0000

    added graph and first line visibility toggle rule

--- base/uk.ac.stfc.isis.ibex.opis/resources/Keithley2700/display_table_values.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/Keithley2700/display_table_values.py
@@ -1,16 +1,26 @@
-from org.csstudio.opibuilder.scriptUtil import PVUtil
+from org.csstudio.opibuilder.scriptUtil import PVUtil, ColorFontUtil
 
 table = widget.getTable()
-i = 1
+row = 1
+color = ColorFontUtil.getColorFromRGB(240,245,245)
 
 for pv in pvs:
     value = PVUtil.getString(pv)
-    table.setCellText(i, 3, value)
-	
+    table.setCellText(row, 3, value)
+    
+    if row%2 != 0:
+		for col in range(0,4):
+			table.setCellBackground(row, col, color)
+    
     if not pv.isConnected():
-        table.setCellText(i, 1, "Disconnected")
+        table.setCellText(row, 1, "Disconnected")
 	
     i+=1
 
 
-table.setCellBorder(2)
+
+
+
+
+	
+	
