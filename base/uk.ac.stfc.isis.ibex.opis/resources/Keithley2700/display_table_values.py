92bfe46589a45ca921b0279d27132531ecbb5b31
esouthren <eilidhsouthren@gmail.com>
2018-02-21 15:02:31 +0000

    Sorted formatting of values table for drift/temp

--- base/uk.ac.stfc.isis.ibex.opis/resources/Keithley2700/display_table_values.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/Keithley2700/display_table_values.py
@@ -1,21 +1,35 @@
 from org.csstudio.opibuilder.scriptUtil import PVUtil, ColorFontUtil
 
+##############################################
+#
+# pvs[0-19] = CHNL:XXX:READING
+# pvs[20-39] = CHNL:XXX:DRIFT
+# pvs[40-59] = CHNL:XXX:TEMP
+#
+##############################################
+
+
 table = widget.getTable()
 row = 1
 color = ColorFontUtil.getColorFromRGB(240,245,245)
-
-for pv in pvs:
-    value = PVUtil.getString(pv)
-    table.setCellText(row, 3, value)
+i = 0
+for i in range(20):
+    reading = PVUtil.getString(pvs[i])
+    temp = PVUtil.getString(pvs[i+40])
+    drift = PVUtil.getString(pvs[i+20])
+    
+    table.setCellText(row, 3, reading)
+    table.setCellText(row, 2, temp)
+    table.setCellText(row, 1, drift)
     
     if row%2 != 0:
 		for col in range(0,4):
 			table.setCellBackground(row, col, color)
     
-    if not pv.isConnected():
+    if not pvs[i].isConnected():
         table.setCellText(row, 1, "Disconnected")
 	
-    i+=1
+    row+=1
 
 
 
