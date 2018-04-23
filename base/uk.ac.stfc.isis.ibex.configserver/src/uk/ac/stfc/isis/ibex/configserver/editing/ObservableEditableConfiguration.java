a3e5b9ac10c90fd30ade1b12d076f33ee73835b3
Adrian Potter <Adrian.Potter@tessella.com>
2018-04-23 11:04:34 +0100

    Set source after initialisation to avoid nulls

--- base/uk.ac.stfc.isis.ibex.configserver/src/uk/ac/stfc/isis/ibex/configserver/editing/ObservableEditableConfiguration.java
+++ base/uk.ac.stfc.isis.ibex.configserver/src/uk/ac/stfc/isis/ibex/configserver/editing/ObservableEditableConfiguration.java
@@ -49,8 +49,8 @@ public class ObservableEditableConfiguration
 	public ObservableEditableConfiguration(
 			ClosableObservable<Configuration> config,
 			ConfigServer configServer) {
-		setSource(config);
 		this.configServer = configServer;
+		setSource(config);
 	}
 	
 	@Override
