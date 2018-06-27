22ba34293cffb69735e53f7a02451689bfd2d76a
RichardTysonIsis <40176733+RichardTysonIsis@users.noreply.github.com>
2018-06-27 08:59:07 +0100

    Update ComponentIcons.java

--- base/uk.ac.stfc.isis.ibex.ui.devicescreens/src/uk/ac/stfc/isis/ibex/ui/devicescreens/ComponentIcons.java
+++ base/uk.ac.stfc.isis.ibex.ui.devicescreens/src/uk/ac/stfc/isis/ibex/ui/devicescreens/ComponentIcons.java
@@ -60,8 +60,8 @@ public final class ComponentIcons {
 
     private static String getIconNameForType(ComponentType componentType) {
         switch (componentType) {
-        	case IN_OUT_MOTION_SET_POINTS:
-        		return "in_out_motion_set_points";
+            case MOTION_SET_POINTS_FEW:
+                return "in_out_motion_set_points";
             case JAWS:
                 return "jaws";
             case CHOPPER:
