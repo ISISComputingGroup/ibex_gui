392d59e12d2e76a7e5ea4b79483f1497b1683a87
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-07-13 13:26:33 +0100

    Display graph next to scripts

--- base/uk.ac.stfc.isis.ibex.ui.scripting/src/uk/ac/stfc/isis/ibex/ui/scripting/Commands.java
+++ base/uk.ac.stfc.isis.ibex.ui.scripting/src/uk/ac/stfc/isis/ibex/ui/scripting/Commands.java
@@ -34,10 +34,13 @@ public final class Commands {
 	 */
 	public static final String GENIE_INITIALISATION = 
 			"# Configuring GENIE PYTHON, please wait\n" 
-			+ "import sys;sys.executable=''\n" 
+			+ "import sys;sys.executable='' \n" 
+			+ "import matplotlib \n"
+			+ "matplotlib.use('module://genie_python.matplotlib.backends.ibex_web_backend') \n"
+			+ "matplotlib.interactive(False) \n"
+			+ "import matplotlib.pyplot as plt \n"
 			+ "from genie_python.genie_startup import * \n" 
-			+ "load_script(None, globals()) \n"
-			+ "%matplotlib qt4 \n";
+			+ "load_script(None, globals()) \n";
 
     private Commands() {
     }
