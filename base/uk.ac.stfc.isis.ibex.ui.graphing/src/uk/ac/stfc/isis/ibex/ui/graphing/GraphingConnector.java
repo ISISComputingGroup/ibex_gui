f6d3a3ecb8ebed62d94ac6d835c9e95424deda6a
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-07-17 23:48:25 +0100

    Fix maven build.

--- base/uk.ac.stfc.isis.ibex.ui.graphing/src/uk/ac/stfc/isis/ibex/ui/graphing/GraphingConnector.java
+++ base/uk.ac.stfc.isis.ibex.ui.graphing/src/uk/ac/stfc/isis/ibex/ui/graphing/GraphingConnector.java
@@ -1,9 +1,6 @@
 package uk.ac.stfc.isis.ibex.ui.graphing;
 
-import java.io.IOException;
-
 import py4j.GatewayServer;
-import py4j.Py4JNetworkException;
 import uk.ac.stfc.isis.ibex.logger.IsisLog;
 import org.apache.logging.log4j.Logger;
 
@@ -17,12 +14,12 @@ public class GraphingConnector {
 	/**
 	 * Starts listening for incoming py4j calls.
 	 */
-    public static void startListening() throws IOException {
+    public static void startListening() {
     	ConnectionHandler handler = new ConnectionHandler();
     	try {
 	    	GatewayServer server = new GatewayServer(handler);
 		    server.start();
-    	} catch (Py4JNetworkException e) {
+    	} catch (RuntimeException e) {
     		LOG.error("Could not start Py4J connection listener because: " + e.getMessage());
     	}
     	LOG.info("Started Py4J connection listener.");
