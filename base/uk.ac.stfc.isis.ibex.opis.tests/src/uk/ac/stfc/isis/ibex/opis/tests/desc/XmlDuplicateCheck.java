97cc910115b27712dbb5c295f6b5380d6cee3157
aaronlong <aaronlong@NDLT1095.isis.cclrc.ac.uk>
2018-09-13 16:53:20 +0100

    Refactored

--- base/uk.ac.stfc.isis.ibex.opis.tests/src/uk/ac/stfc/isis/ibex/opis/tests/desc/XmlDuplicateCheck.java
+++ base/uk.ac.stfc.isis.ibex.opis.tests/src/uk/ac/stfc/isis/ibex/opis/tests/desc/XmlDuplicateCheck.java
@@ -1,11 +1,8 @@
 package uk.ac.stfc.isis.ibex.opis.tests.desc;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -19,20 +16,21 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.junit.Test;
 
 import uk.ac.stfc.isis.ibex.epics.conversion.XMLUtil;
-import uk.ac.stfc.isis.ibex.opis.OpiProvider;
-import uk.ac.stfc.isis.ibex.opis.desc.OpiDescription;
 
-public class XmlDupeCheck {
+/**
+ * Here we don't use the default Xml parser class is we need to check the keys in the OPI list. 
+ * 
+ *
+ */
+public class XmlDuplicateCheck {
 	
 	@XmlAccessorType(XmlAccessType.FIELD)
 	private static class OpiEntry {
 		public String key;
-		public OpiDescription value;
 	}
 	
 	@XmlRootElement(name = "descriptions")
@@ -42,33 +40,25 @@ public class XmlDupeCheck {
 		@XmlElement(name = "entry", type = OpiEntry.class)
 		public List<OpiEntry> keys;
 		
-		public Set<String> getDuplicateKeys() {
+		public Stream<String> getDuplicateKeys() {
 			Set<String> uniques = new HashSet<>();
 			return keys.stream()
 					.map(entry -> entry.key)
-					.filter(key -> !uniques.add(key))
-					.collect(Collectors.toSet());
-		}
-		
+					.filter(key -> !uniques.add(key));
+		}	
 	}
 	
 	@Test
-	public void test_that_fails() throws IOException, JAXBException {
-	   	
+	public void test_that_opi_info_file_does_not_contain_any_duplicate_keys() throws IOException, JAXBException {
 		Path path = new Path("../uk.ac.stfc.isis.ibex.opis/resources/opi_info.xml");
-		assertNotNull("Path not found", path);
 	    KeyList keyList = XMLUtil.fromXml(new FileReader(path.toOSString()), KeyList.class);
 	    
-	    Set<String> duplicates = keyList.getDuplicateKeys();
+	    Set<String> duplicateKeys = keyList.getDuplicateKeys().collect(Collectors.toSet());
 	    
-	    StringBuilder builder = new StringBuilder();
-	    builder.append("The following keys are duplicated in opi_info: ");
-
-	    for (String a : duplicates) {
-	    	builder.append(String.format("%s, ", a));
-	    }
+		String error = String.format("The following keys are duplicated in opi_info: %s", 
+				String.join(", ", duplicateKeys));
 	    
-	    assertEquals(builder.toString(), 0, duplicates.size());
+	    assertEquals(error, 0, duplicateKeys.size());
 	}
 
 }
