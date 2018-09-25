ba3c2ab2a72eb38c0ab3239e634366591ff61413
John-Holt-Tessella <John.Holt@tessella.com>
2018-09-25 16:34:07 +0100

    Add comments and use iter

--- base/uk.ac.stfc.isis.ibex.opis/resources/reflectometry/setPositionMacros.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/reflectometry/setPositionMacros.py
@@ -5,7 +5,9 @@ import zlib
 
 def reload_widgets(count):
     """
-    Reload all the position widgets. We have 'count' of them
+    Reload all the position widgets. We have 'count' of them.
+    Args:
+        Count: widget to reload
     """
     for index in range(1, count + 1):
         reload_widget(display.getWidget("pos_" + str(index)))
@@ -15,9 +17,10 @@ def reload_widget(widget):
     """
     We have to reload the widget to update the macros. Done by unsetting and resetting the
     target OPI.
+    Args:
+        widget: wdidget to reload
     """
     widget.setPropertyValue("opi_file", "null.opi")
-    # widget.setPropertyValue("opi_file", "component_pos.opi")
     widget.setPropertyValue("opi_file", "component_pos.opi")
 
 
@@ -25,6 +28,10 @@ def _add_macros(widget_index, name, value):
     """
     Add the macro with name to a motor widget with value. 
     Widgets are identified as "Motor_n" for n in [1,8] in the parent OPI
+    Args:
+        widget_index: index of the widget
+        name: macro name
+        value: macro value
     """
     motor_widget = display.getWidget("pos_" + str(widget_index))
     motor_macros = motor_widget.getPropertyValue("macros")
@@ -38,9 +45,9 @@ value = zlib.decompress(value.decode("hex"))
 params = json.loads(value)
 
 pos = 1
-for pv, name in params.iteritems():
+for pv, name in params.items():
     _add_macros(pos, "PARAM_PV", pv)
     _add_macros(pos, "PARAM_NAME", name)
     pos += 1
-reload_widgets(pos)
 
+reload_widgets(pos)
