16fa00fadb6930e82e710ea5f9461b22450c746c
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-11-29 19:47:22 +0000

    Add module names to poms

--- build/build.bat
+++ build/build.bat
@@ -3,7 +3,9 @@
 call python .\check_build.py ..\base\
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-call mvn --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.ibex.client.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% clean verify
+if "%BUILD_NUMBER%" == "" SET BUILD_NUMBER=DEV
+
+call mvn --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.ibex.client.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% -Dmaven.compiler.fork=true -Dmaven.compiler.executable=javac clean verify
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 @echo Client built in %~dp0..\base\uk.ac.stfc.isis.ibex.e4.client.product\target\products\ibex.product\win32\win32\x86_64
