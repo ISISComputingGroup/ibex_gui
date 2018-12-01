99923bece3266f069054c8c58a3489850510439b
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-12-01 02:38:20 +0000

    Functional maven build.

--- build/build.bat
+++ build/build.bat
@@ -3,9 +3,9 @@
 call python .\check_build.py ..\base\
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-if "%BUILD_NUMBER%" == "" SET BUILD_NUMBER=DEV
+if "%BUILD_NUMBER%" == "" SET BUILD_NUMBER=0.0.0-SNAPSHOT
 
-call mvn --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.ibex.client.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% -Dmaven.compiler.fork=true -Dmaven.compiler.executable=javac clean verify
+call mvn --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.ibex.client.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% clean verify
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 @echo Client built in %~dp0..\base\uk.ac.stfc.isis.ibex.e4.client.product\target\products\ibex.product\win32\win32\x86_64
