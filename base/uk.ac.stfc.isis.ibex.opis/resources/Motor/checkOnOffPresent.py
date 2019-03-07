2050d9c530f5142ee22fde9c68e5a1cc28521eec
ThomasLohnert <thomas.lohnert@stfc.ac.uk>
2019-03-07 11:46:09 +0000

    Relabel widgets

--- base/uk.ac.stfc.isis.ibex.opis/resources/Motor/checkOnOffPresent.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/Motor/checkOnOffPresent.py
@@ -1,7 +1,7 @@
 from org.csstudio.opibuilder.scriptUtil import PVUtil
 from org.csstudio.opibuilder.scriptUtil import ConsoleUtil
 
-onoff_group = display.getWidget("OnOffGroup")
+onoff_group = display.getWidget("Power")
 if pvArray[0].isConnected():
     onoff_group.setPropertyValue('visible', True)
 else:
