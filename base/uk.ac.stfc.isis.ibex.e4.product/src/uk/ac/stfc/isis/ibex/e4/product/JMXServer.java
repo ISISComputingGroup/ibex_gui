90e2859c947f85397ecc4cdb96697492271d6e5b
aaronlong <aaron.long@stfc.ac.uk>
2019-03-11 12:11:41 +0000

    Changed println to log for handling output

--- base/uk.ac.stfc.isis.ibex.e4.product/src/uk/ac/stfc/isis/ibex/e4/product/JMXServer.java
+++ base/uk.ac.stfc.isis.ibex.e4.product/src/uk/ac/stfc/isis/ibex/e4/product/JMXServer.java
@@ -7,11 +7,15 @@ import javax.management.remote.JMXConnectorServer;
 import javax.management.remote.JMXConnectorServerFactory;
 import javax.management.remote.JMXServiceURL;
 
+import org.apache.logging.log4j.Logger;
+import uk.ac.stfc.isis.ibex.logger.IsisLog;
+
 /**
  * This starts a JMX server so we can remotely monitor the GUI.
  * We do it here rather than with the JMX command line arguments so that we can dynamically allocate ports
  */
 public class JMXServer {
+	private static final Logger LOG = IsisLog.getLogger(JMXServer.class);
 	/**
 	 * Starts the server.
 	 */
@@ -21,9 +25,9 @@ public class JMXServer {
 			JMXServiceURL url = new JMXServiceURL("rmi", null, 0);
 			JMXConnectorServer cs = JMXConnectorServerFactory.newJMXConnectorServer(url, null, mbs);
 			cs.start();
-		    System.out.println("JMX url: " + cs.getAddress().toString());
+		    LOG.info("JMX url: " + cs.getAddress().toString());
 		} catch (Exception e) {
-			System.out.println("Failed to start JMX server: " + e.getMessage());
+			LOG.error("Failed to start JMX server: " + e.getMessage());
 		}
 	}
 }
