3329b0297a328effec13defb52f9ddf62306ae0c
Alistair McGann <alistair.mcgann@tessella.com>
2019-06-24 16:51:45 +0100

    Remove extra UI elements from client. Add build file

---
+++ build/build_script_generator.bat
@@ -0,0 +1,14 @@
+@echo off
+ 
+call python .\check_build.py ..\base\
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+if "%BUILD_NUMBER%" == "" (
+    set BUILD_NUMBER=SNAPSHOT
+)
+
+call mvn --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.scriptgenerator.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% clean verify
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+@echo Client built in %~dp0..\ibex_gui_4\base\uk.ac.stfc.isis.scriptgenerator.client.product\target\products\scriptgenerator.product\win32\win32\x86_64
+pause
