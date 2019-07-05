8073a183baafc2268fe9e2312a0b466ef498843e
Alistair McGann <alistair.mcgann@tessella.com>
2019-07-05 15:39:28 +0100

    Correct file path

--- build/build_script_generator.bat
+++ build/build_script_generator.bat
@@ -10,5 +10,5 @@ if "%BUILD_NUMBER%" == "" (
 call mvn --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.scriptgenerator.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% clean verify
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-@echo Client built in %~dp0..\ibex_gui_4\base\uk.ac.stfc.isis.scriptgenerator.client.product\target\products\scriptgenerator.product\win32\win32\x86_64
+@echo Client built in %~dp0..\base\uk.ac.stfc.isis.scriptgenerator.client.product\target\products\scriptgenerator.product\win32\win32\x86_64
 pause
