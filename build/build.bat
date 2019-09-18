118376e0e9f8cd5408c5e3e6d8b9829ebd3f36ae
Tom Willemsen <tomwillemsen1995@gmail.com>
2019-09-18 11:46:43 +0100

    Refactor java getter

--- build/build.bat
+++ build/build.bat
@@ -1,9 +1,14 @@
-python -m pip install six
+robocopy "\\isis\inst$\Kits$\CompGroup\ICP\JDK_11" "%~dp0\jdk" /E /PURGE /R:2 /LOG:"%~dp0\jdk\install.log" /MT /XF "install.log" /NFL /NDL /NP
 
-call python get_java.py
-if %errorlevel% neq 0 exit /b %errorlevel%
+set errcode=%ERRORLEVEL%
+if %errcode% GEQ 4 (
+    @echo *** Exit Code %errcode% ERROR see %INSTALLDIR%install.log ***
+	@echo ************** Exit Code %errcode% ERROR **************** >>%INSTALLDIR%install.log
+    if not "%1" == "NOLOG" start %INSTALLDIR%install.log
+	exit /b %errcode%
+)
 
-SET "JAVA_HOME=%~dp0jdk\jdk-11.0.1"
+for /D %%I in ( "%~dp0\jdk" ) do SET "JAVA_HOME=%%I"
  
 call python .\check_build.py ..\base\
 if %errorlevel% neq 0 exit /b %errorlevel%
