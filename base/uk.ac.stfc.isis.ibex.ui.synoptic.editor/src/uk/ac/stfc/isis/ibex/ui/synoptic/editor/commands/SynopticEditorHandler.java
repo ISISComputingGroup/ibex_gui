1f03643f8eda1e3c99bc1448d7d1f287b22638e8
RaiBishal <bishal.rai@ndlt1205.isis.cclrc.ac.uk>
2019-09-18 08:55:32 +0100

    - Added changes to display which synoptic is being edited by the user currently

--- base/uk.ac.stfc.isis.ibex.ui.synoptic.editor/src/uk/ac/stfc/isis/ibex/ui/synoptic/editor/commands/SynopticEditorHandler.java
+++ base/uk.ac.stfc.isis.ibex.ui.synoptic.editor/src/uk/ac/stfc/isis/ibex/ui/synoptic/editor/commands/SynopticEditorHandler.java
@@ -90,8 +90,9 @@ public abstract class SynopticEditorHandler {
      */
 	protected void openDialog(Shell shell, SynopticDescription synoptic, String title, boolean isBlank) {
         SynopticViewModel viewModel = new SynopticViewModel(synoptic);
+        String subtitle = "Editing the " + viewModel.getSynoptic().name() + " synoptic";
         EditSynopticDialog editDialog =
-                new EditSynopticDialog(shell, title, isBlank, viewModel);
+                new EditSynopticDialog(shell, title, subtitle, isBlank, viewModel);
 		if (editDialog.open() == Window.OK) {
 		    try {
 		        writer.write(viewModel.getSynoptic());
