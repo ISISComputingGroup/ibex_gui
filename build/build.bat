65fc2471503a22af58a28f9cb25497903da10a08
Tom Willemsen <tomwillemsen1995@gmail.com>
2019-09-18 17:55:58 +0100

    Make script generator work with J11?

--- build/build.bat
+++ build/build.bat
@@ -15,7 +15,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 if "%BUILD_NUMBER%" == "" SET BUILD_NUMBER=SNAPSHOT
 
-call mvn --fail-at-end --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.ibex.client.tycho.parent\pom.xml clean verify
+call mvn --fail-at-end --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.ibex.client.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% clean verify
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 @echo Client built in %~dp0..\base\uk.ac.stfc.isis.ibex.e4.client.product\target\products\ibex.product\win32\win32\x86_64
--- build/build_script_generator.bat
+++ build/build_script_generator.bat
@@ -1,4 +1,14 @@
-@echo off
+robocopy "\\isis\inst$\Kits$\CompGroup\ICP\JDK_11" "%~dp0\jdk" /E /PURGE /R:2 /MT /XF "install.log" /NFL /NDL /NP
+
+set errcode=%ERRORLEVEL%
+if %errcode% GEQ 4 (
+    @echo *** Exit Code %errcode% ERROR see %INSTALLDIR%install.log ***
+	@echo ************** Exit Code %errcode% ERROR **************** >>%INSTALLDIR%install.log
+    if not "%1" == "NOLOG" start %INSTALLDIR%install.log
+	exit /b %errcode%
+)
+
+for /D %%I in ( %~dp0jdk\* ) do SET "JAVA_HOME=%%I"
  
 call python .\check_build.py ..\base\
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -7,7 +17,7 @@ if "%BUILD_NUMBER%" == "" (
     set BUILD_NUMBER=SNAPSHOT
 )
 
-call mvn --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.scriptgenerator.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% clean verify
+call mvn --fail-at-end --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.scriptgenerator.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% clean verify
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 @echo Client built in %~dp0..\base\uk.ac.stfc.isis.scriptgenerator.client.product\target\products\scriptgenerator.product\win32\win32\x86_64
