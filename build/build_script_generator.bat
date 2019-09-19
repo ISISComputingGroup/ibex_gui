9f4563908a23221bb7690a94cd31a5b004848591
Tom Willemsen <tomwillemsen1995@gmail.com>
2019-09-19 11:03:37 +0100

    Make all JDK references modular

--- build/build_script_generator.bat
+++ build/build_script_generator.bat
@@ -17,7 +17,7 @@ if "%BUILD_NUMBER%" == "" (
     set BUILD_NUMBER=SNAPSHOT
 )
 
-call mvn --fail-at-end --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.scriptgenerator.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% clean verify
+call mvn --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.scriptgenerator.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% clean verify
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 @echo Client built in %~dp0..\base\uk.ac.stfc.isis.scriptgenerator.client.product\target\products\scriptgenerator.product\win32\win32\x86_64
