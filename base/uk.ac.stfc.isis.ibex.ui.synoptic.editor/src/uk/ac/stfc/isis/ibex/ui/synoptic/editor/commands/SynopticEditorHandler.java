70040f9215a4cfa2f80affff87927c4b2015cc67
RaiBishal <bishal.rai@ndlt1205.isis.cclrc.ac.uk>
2019-10-10 08:31:54 +0100

    Correct title display when editing/creating new synoptic

--- base/uk.ac.stfc.isis.ibex.ui.synoptic.editor/src/uk/ac/stfc/isis/ibex/ui/synoptic/editor/commands/SynopticEditorHandler.java
+++ base/uk.ac.stfc.isis.ibex.ui.synoptic.editor/src/uk/ac/stfc/isis/ibex/ui/synoptic/editor/commands/SynopticEditorHandler.java
@@ -91,8 +91,8 @@ public abstract class SynopticEditorHandler {
 	protected void openDialog(Shell shell, SynopticDescription synoptic, String title, boolean isBlank) {
         SynopticViewModel viewModel = new SynopticViewModel(synoptic);
         String SynopticName = (viewModel.getSynoptic().name() == null) ?  
-        		"current" : viewModel.getSynoptic().name();
-        String subtitle = "Editing the " + SynopticName + " synoptic";
+        		"a new" : viewModel.getSynoptic().name();
+        String subtitle = "Editing " + SynopticName + " synoptic";
         EditSynopticDialog editDialog =
                 new EditSynopticDialog(shell, title, subtitle, isBlank, viewModel);
 		if (editDialog.open() == Window.OK) {
