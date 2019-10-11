3e7f388a326252811d4d73caa0ecee380d992b3a
Alexandru Dascalu <965337@swansea.ac.uk>
2019-10-11 10:42:23 +0100

    Revert "changed anonymous inner class to lamba expression"
    This reverts commit 38ef953335acae1b4a5c05b7feae74e46c090067.

--- base/uk.ac.stfc.isis.ibex.ui.experimentdetails/src/uk/ac/stfc/isis/ibex/ui/experimentdetails/rblookup/RBLookupDialog.java
+++ base/uk.ac.stfc.isis.ibex.ui.experimentdetails/src/uk/ac/stfc/isis/ibex/ui/experimentdetails/rblookup/RBLookupDialog.java
@@ -6,6 +6,8 @@ import org.eclipse.jface.databinding.swt.WidgetProperties;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.TraverseEvent;
+import org.eclipse.swt.events.TraverseListener;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
@@ -37,9 +39,12 @@ public class RBLookupDialog extends Dialog {
 	@Override
 	protected Control createDialogArea(Composite parent) {	
 		//Prevent the return key from closing the dialog		
-		getShell().addTraverseListener(e -> {
-			if (e.detail == SWT.TRAVERSE_RETURN) {
-				e.doit = false;
+		getShell().addTraverseListener(new TraverseListener() {
+			@Override
+			public void keyTraversed(TraverseEvent e) {
+				if (e.detail == SWT.TRAVERSE_RETURN) {
+					e.doit = false;
+				}
 			}
 		});
 		
