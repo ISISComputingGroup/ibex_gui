7cf928e4b661920bee8e51c49b5f2e0e1b794367
Alistair McGann <alistair.mcgann@tessella.com>
2019-10-22 12:15:59 +0100

    Remove unused code

--- base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/ScriptGeneratorSingleton.java
+++ base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/ScriptGeneratorSingleton.java
@@ -76,41 +76,6 @@ public class ScriptGeneratorSingleton extends ModelObject {
 	public void moveActionDown(int index) {
 		scriptGeneratorTable.moveAction(index, index+1);
 	}
-	
-	/**
-	 * Adds one to the toy model's value.
-	 * Updates string representation
-	 * 
-	 * @return Integer of the updated model value
-	 */
-	public int iterateNumber() {
-
-		iteratedNumber++;
-		this.scriptGeneratorTable.getActions().get(0).setActionParameterValue("code", iteratedNumber.toString());
-		setIteratedNumber(iteratedNumber.toString());
-		return iteratedNumber;
-	}
-	
-	/**
-	 * Returns the model's value as a string.
-	 * 
-	 * @return String representation of the model's value
-	 */
-	public String getIteratedNumber() {
-
-		return iteratedNumberString;
-	}
-	
-	/**
-	 * Updates the string representation of the model's value.
-	 * 
-	 * @param newValue
-	 *         String representation of the toy model's current value
-	 */
-	protected synchronized void setIteratedNumber(String newValue) {
-
-		firePropertyChange("iteratedNumber", this.iteratedNumberString, this.iteratedNumberString = newValue);
-	}
 
 
 }
