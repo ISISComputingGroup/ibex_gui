3c839ca19d54cc8c44f84aff435ec220eef2fd4e
JamesKingWork <JamesKingWork@ndlt1208.isis.cclrc.ac.uk>
2019-11-19 09:29:12 +0000

    Added python project to serve python executable

---
+++ base/uk.ac.stfc.isis.python/src/uk/ac/stfc/isis/python/Python.java
@@ -0,0 +1,43 @@
+package uk.ac.stfc.isis.python;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+
+public class Python {
+
+	private static final String pythonRelativePath = "/Python3/python.exe";
+	private static final String devPythonPath = "C:/Instrument/Apps/Python3/python.exe";
+
+	/**
+	 * Gets the python that's been bundled with the gui.
+	 * @return The string path to the python executable.
+	 * @throws IOException if python could not be found.
+	 */
+	public String getPythonPath() {
+		try {
+			return relativePathToFull(pythonRelativePath);
+		} catch(IOException e) {
+			return Path.forWindows(devPythonPath).toOSString();
+		}
+	}
+
+	/**
+	 * Gets the full path to a file given the path relative to this plugin.
+	 * @param relativePath The path of the file relative to this plugin.
+	 * @return The full path.
+	 * @throws IOException if the file could not be found.
+	 */
+	private static String relativePathToFull(String relativePath) throws IOException {
+		try {
+			URL resourcePath = Python.class.getResource(relativePath);
+			String fullPath = FileLocator.resolve(resourcePath).getPath();
+			return Path.forWindows(fullPath).toOSString();
+		} catch(NullPointerException e) {
+			throw new IOException("Cannot find python on relative path: " + relativePath);
+		}
+	}
+
+}
