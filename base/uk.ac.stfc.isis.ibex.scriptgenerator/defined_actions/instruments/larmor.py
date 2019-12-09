1edf17a8e8c993a1c70ef4ae63860a0235f694bc
JamesKingWork <JamesKingWork@ndlt1208.isis.cclrc.ac.uk>
2019-12-09 08:52:17 +0000

    Working out casting of types

--- base/uk.ac.stfc.isis.ibex.scriptgenerator/defined_actions/instruments/larmor.py
+++ base/uk.ac.stfc.isis.ibex.scriptgenerator/defined_actions/instruments/larmor.py
@@ -2,17 +2,27 @@ from action_interface import ActionDefinition
 
 
 class DoRun(ActionDefinition):
-    def run(self, temperature: float=0.0, field: float=0.0, uamps: float=0.0):
+    def run(self, temperature: str="0.0", field: str="0.0", uamps: str="0.0"):
         g.cset("temperature", temperature)
         g.cset("field", field)
         g.begin()
         g.waitfor_uamps(uamps)
         g.end()
 
-    def parameters_valid(self, temperature: float=0.0, field: float=0.0, uamps: float=0.0):
-        if not 0.1 <= temperature <= 300:
+    def parameters_valid(self, temperature: str="0.0", field: str="0.0", uamps: str="0.0"):
+        # Cast the types
+        try:
+            temperature = float(temperature)
+        except ValueError:
+            return "Temperature not passed as float"
+        try:
+            field = float(field)
+        except ValueError:
+            return "Field not passed as float"
+        # Check the parameters are valid
+        if not 0.1 <= float(temperature) <= 300:
             return "Temperature outside range"
-        if not -5 <= field < 5:
+        if not -5 <= int(field) < 5:
             return "Field outside range"
         return None
 
