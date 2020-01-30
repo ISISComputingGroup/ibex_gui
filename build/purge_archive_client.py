d84493fcca03248638d8ba16ded3305e4108862b
John-Holt-Tessella <John.Holt@tessella.com>
2020-01-30 13:57:09 +0000

    Dont install genie python this is in the script generator. Delete old builds correctly

--- build/purge_archive_client.py
+++ build/purge_archive_client.py
@@ -1,9 +1,16 @@
 import os, time, sys, datetime, glob, shutil
 
-path = r"\\isis\inst$\kits$\CompGroup\ICP\Client\BUILD*"
+base_path = r"\\isis\inst$\kits$\CompGroup\ICP"
+paths = [
+    r"{}\script_generator\BUILD*".format(base_path),
+    r"{}\Client_E4\BUILD*".format(base_path),
+    r"{}\script_generator\BUILD*".format(base_path)]
+    
 
 dir_list = []
-for d in glob.glob(path):
+for path in paths:
+    print(path)
+    for d in glob.glob(path):
             if os.path.isdir(d):
                 file_modified = datetime.datetime.fromtimestamp(os.path.getmtime(d))
                 if datetime.datetime.now() - file_modified > datetime.timedelta(hours=24*60):
