63d53168c68676cfd18c83e90115cf6b3003e0a9
JamesKingWork <54139864+JamesKingWork@users.noreply.github.com>
2020-02-13 16:03:17 +0000

    Update base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/pythoninterface/PythonNotReadyException.java
    Co-Authored-By: John Holt <John.Holt@tessella.com>

--- base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/pythoninterface/PythonNotReadyException.java
+++ base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/pythoninterface/PythonNotReadyException.java
@@ -3,7 +3,6 @@ package uk.ac.stfc.isis.ibex.scriptgenerator.pythoninterface;
 /**
  * An exception for when python is not ready to be called in to with a custom message.
  * 
- * @author James King
  */
 @SuppressWarnings("serial")
 public class PythonNotReadyException extends Exception {
