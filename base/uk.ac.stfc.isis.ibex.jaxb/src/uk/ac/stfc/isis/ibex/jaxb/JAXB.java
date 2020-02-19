ad0cf0a5ab74769924ce32bf29e54c7c25268c8d
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-02-19 14:55:05 +0000

    Fix package signature verification error

--- base/uk.ac.stfc.isis.ibex.jaxb/src/uk/ac/stfc/isis/ibex/jaxb/JAXB.java
+++ base/uk.ac.stfc.isis.ibex.jaxb/src/uk/ac/stfc/isis/ibex/jaxb/JAXB.java
@@ -1,89 +1,50 @@
 package uk.ac.stfc.isis.ibex.jaxb;
 
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
-import org.apache.logging.log4j.Logger;
 import org.eclipse.persistence.jaxb.JAXBContextFactory;
 
-import uk.ac.stfc.isis.ibex.logger.IsisLog;
-import uk.ac.stfc.isis.ibex.logger.LoggerUtils;
-
+/**
+ * Utility methods to easily acquire JAXB marshallers and unmarshallers.
+ */
 public class JAXB {
 	
-	private static final Logger LOG = IsisLog.getLogger(JAXB.class);
+	static {
+		System.setProperty("javax.xml.bind.context.factory", JAXBContextFactory.class.getName());
+	}
 	
-    public static <T> JAXBContext getJaxbContext(Class<T> clazz) throws JAXBException {
-    	
-//    	ClassLoader cl = JAXB.class.getClassLoader().loadClass("")
-//    	
-//    	return JAXBContextFactory.createContext(new Class[] { clazz }, Collections.emptyMap(), JAXB.class.getClassLoader());
-//    	
-    	// Nuclear approach - depending on where this gets called from
-    	// we need different classloaders to be used. Try them all until
-    	// one doesn't error. This is hacky but it's the best way I've found
-    	// to get around JAXB's limitations so far.
-    	List<ClassLoader> classloaders = List.of(
-			clazz.getClassLoader(),
-			JAXB.class.getClassLoader(),
-			JAXBContextFactory.class.getClassLoader(),
-			Thread.currentThread().getContextClassLoader(),
-			ClassLoader.getPlatformClassLoader(),
-			ClassLoader.getSystemClassLoader()
-    	);
-    	
-    	Map<String, Throwable> errors = new HashMap<>();
-    	
-    	for (ClassLoader classloader : classloaders) {
-    		LOG.info(String.format("Classloader %s loads JAXBContext from %s", classloader, classloader.getResource("javax/xml/bind/JAXBContext.class")));
-    		try {
-        		return JAXBContextFactory.createContext(new Class[] {clazz}, Collections.emptyMap(), classloader);
-        	} catch (Exception | NoClassDefFoundError e) {
-        		errors.put(String.format("used classloader %s", classloader), e);
-        	}
-    		
-    		try {
-        		return JAXBContextFactory.createContext(new Class[] {clazz}, null);
-        	} catch (Exception | NoClassDefFoundError e) {
-        		errors.put(String.format("used raw class moxy %s", clazz), e);
-        	}
-    		
-    		try {
-        		return JAXBContext.newInstance(clazz);
-        	} catch (Exception | NoClassDefFoundError e) {
-        		errors.put(String.format("used raw class %s", clazz), e);
-        	}
-    		
-    		try {
-        		return JAXBContext.newInstance(clazz.getPackageName(), classloader);
-        	} catch (Exception | NoClassDefFoundError e) {
-        		errors.put(String.format("used package name %s with classloader %s", clazz.getPackageName(), classloader), e);
-        	}
+    private static <T> JAXBContext getJaxbContext(Class<T> clazz) throws JAXBException {
+    	try {
+    		// For running the full GUI
+    	    return JAXBContextFactory.createContext(new Class [] { clazz }, Collections.emptyMap(), JAXBContextFactory.class.getClassLoader());
+    	} catch (Exception | LinkageError e) {
+    		// For unit tests
+    		return JAXBContextFactory.createContext(new Class [] { clazz }, Collections.emptyMap(), Thread.currentThread().getContextClassLoader());
     	}
-    	
-    	for (var entry : errors.entrySet()) {
-    		LoggerUtils.logErrorWithStackTrace(LOG, 
-    				String.format("%s, got exception: %s", entry.getKey(), entry.getValue().getMessage()), entry.getValue());
-    		
-    		if (entry.getValue().getCause() != null) {
-    			LoggerUtils.logErrorWithStackTrace(LOG, "Caused by: " + entry.getValue().getCause().getMessage(), entry.getValue().getCause());
-    		}
-    	}
-    	
-    	throw new JAXBException("Failed to get a JAXBContext using any classloader");
     }
     
+    /**
+     * Get an XML unmarshaller.
+     * @param <T> - the type to get an unmarshaller for
+     * @param clazz - the class to get an unmarshaller for
+     * @return the unmarshaller
+     * @throws JAXBException if an error occured
+     */
     public static <T> Unmarshaller getUnmarshaller(Class<T> clazz) throws JAXBException {
     	return getJaxbContext(clazz).createUnmarshaller();
     }
     
+    /**
+     * Get an XML marshaller.
+     * @param <T> - the type to get a marshaller for
+     * @param clazz - the class to get a marshaller for
+     * @return the marshaller
+     * @throws JAXBException if an error occured
+     */
     public static <T> Marshaller getMarshaller(Class<T> clazz) throws JAXBException {
     	return getJaxbContext(clazz).createMarshaller();
     }
--- build/build.bat
+++ build/build.bat
@@ -15,7 +15,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 if "%BUILD_NUMBER%" == "" SET BUILD_NUMBER=SNAPSHOT
 
-call mvn --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.ibex.client.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% clean verify || set mvnErr=1
+call mvn -T16 --settings=%~dp0..\mvn_user_settings.xml -f %~dp0..\base\uk.ac.stfc.isis.ibex.client.tycho.parent\pom.xml -DforceContextQualifier=%BUILD_NUMBER% clean verify || set mvnErr=1
 if defined mvnErr exit /b 1
 
 REM Copy built client into a sensible directory to run it
