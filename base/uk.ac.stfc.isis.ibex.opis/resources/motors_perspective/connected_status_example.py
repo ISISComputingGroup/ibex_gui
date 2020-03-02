abd0d77e68afea6068cbbe1c9ffb8fae526613e3
aaronlong <aaron.long@stfc.ac.uk>
2020-03-02 11:23:09 +0000

    connected test script and updated icons & positions

---
+++ base/uk.ac.stfc.isis.ibex.opis/resources/motors_perspective/connected_status_example.py
@@ -0,0 +1,10 @@
+from org.csstudio.opibuilder.scriptUtil import PVUtil, ConsoleUtil
+
+# If connected write to internal PV
+
+if ( PVUtil.getSeverityString(pvs[0]) == -1 ):  # Disconnected
+	PVUtil.writePV("loc://motor_0101_connected", 0)
+	ConsoleUtil.writeInfo("Main: Set local pv to 0")
+else:
+	PVUtil.writePV("loc://motor_0101_connected", 1)
+	ConsoleUtil.writeInfo("Main: Set local pv to 1")from org.csstudio.opibuilder.scriptUtil import PVUtil
