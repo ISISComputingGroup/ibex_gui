d7c9350f32601a929199828f7f8e04c32724cee2
RaiBishal <bishal.rai@ndlt1205.isis.cclrc.ac.uk>
2020-03-18 13:05:14 +0000

    removed Author.

--- base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/ScriptDefinitionNotMatched.java
+++ base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/ScriptDefinitionNotMatched.java
@@ -3,7 +3,6 @@ package uk.ac.stfc.isis.ibex.scriptgenerator;
 /**
  * An exception for when we are trying to load a data file using different script definition file than it was used to create it.
  * 
- * @author Bishal Rai
  *
  */
 @SuppressWarnings("serial")
