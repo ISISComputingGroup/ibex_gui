ba304d31daa56a7062b95a699e38de0a0f23e390
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-03-19 14:30:32 +0000

    Delete unrequired scripts

--- base/uk.ac.stfc.isis.ibex.opis/resources/motors_perspective/check_connected.py
+++
@@ -1,8 +0,0 @@
-from org.csstudio.opibuilder.scriptUtil import PVUtil, ConsoleUtil
-
-if (PVUtil.getLong(pvs[0]) == 1):
-	widget.setPropertyValue("opi_file", "single_motor_standard_view.opi", True)
-	ConsoleUtil.writeInfo("Sub: Set opi to standard view")
-else:
-	widget.setPropertyValue("opi_file", "single_motor_view_disconnected.opi", True)
-	ConsoleUtil.writeInfo("Sub: Set opi to disconnected view")
