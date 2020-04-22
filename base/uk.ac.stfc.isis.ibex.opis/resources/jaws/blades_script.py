018a51e5db7e97cf02c3d36a4887779d5ca4772c
Adam Laverack <adalator@googlemail.com>
2020-04-22 14:32:15 +0100

    Jaws OPI/script now accounts for asymmetric jaw limits

--- base/uk.ac.stfc.isis.ibex.opis/resources/jaws/blades_script.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/jaws/blades_script.py
@@ -42,46 +42,59 @@ class UpdateJaws(Runnable):
         self.east = PVUtil.getDouble(pvs[2])
         self.west = PVUtil.getDouble(pvs[3])
 
-        self.max_y = PVUtil.getDouble(pvs[4])
-        self.max_x = PVUtil.getDouble(pvs[5])
+        self.max_n = PVUtil.getDouble(pvs[4])
+        self.max_e = PVUtil.getDouble(pvs[5])
+        self.max_s = abs(PVUtil.getDouble(pvs[6]))
+        self.max_w = abs(PVUtil.getDouble(pvs[7]))
+
         
     def calc_bound_values(self):
         """
         Calculate x and y positions for the limit of travel indicators (bounds)
         """
-        north_bound_val = (background_height/2.0)*(1 - self.scaling_factor)
-        south_bound_val = background_height - ((background_height/2.0)*(1 - self.scaling_factor))                    
-        west_bound_val = (background_width/2.0)*(1 - self.scaling_factor)
-        east_bound_val = background_width - ((background_width/2.0)*(1 - self.scaling_factor))
+        north_bound_val = (background_height/2.0)*(1 - self.scaling_factor_n)
+        south_bound_val = background_height - ((background_height/2.0)*(1 - self.scaling_factor_s))                    
+        west_bound_val = (background_width/2.0)*(1 - self.scaling_factor_w)
+        east_bound_val = background_width - ((background_width/2.0)*(1 - self.scaling_factor_e))
         return north_bound_val, south_bound_val, west_bound_val, east_bound_val
 
-    def calc_pair_height(self, pv_vals, max_val, gui_max):
+    def calc_gui_heights(self, gui_max_x, gui_max_y):
         """
-        Calculates the height or width of a pair of blades
-        :param pv_vals: the pv values in order (N, S) or (E, W)
-        :param max_val: the max of the PVs (assumes symmetry)
-        :param gui_max: the maximum width/height allowed for the jaws on the GUI
-        :return: the heights in order (N, S) or (E, W)
+        Calculates the height or width of blade gui elements
+        :param gui_max_x: the maximum width allowed for the jaws on the GUI
+        :param gui_max_y: the maximum height allowed for the jaws on the GUI
+        :return: the heights/widths in order north, east, south, west
         """
-        nominal = gui_max / 2
-        first = nominal - nominal * (pv_vals[0] / max_val)
-        second = nominal + nominal * (pv_vals[1] / max_val)
-
-        return bound_value(first, gui_max), bound_value(second, gui_max)
+        nominal_x = gui_max_x / 2
+        nominal_y = gui_max_y / 2
         
-    def calc_scaling_factor(self):
+        north_height = nominal_y - nominal_y * (self.north / self.max_n)
+        east_width = nominal_x - nominal_x * (self.east / self.max_e)
+        south_height = nominal_y + nominal_y * (self.south / self.max_s)
+        west_width = nominal_x + nominal_x * (self.west / self.max_w)
+        
+        north_height = bound_value(north_height, gui_max_y)
+        east_width = bound_value(east_width, gui_max_x)
+        south_height = bound_value(south_height, gui_max_y)
+        west_width = bound_value(west_width, gui_max_x)
+
+        return north_height, east_width, south_height, west_width
+        
+    def calc_scaling_factor(self, highest_jaw_limit):
         """
-        Calculate a scaling factor that will be used to shorten the GUI blade elements of
-        the shorter axis (i.e. the axis with the smaller limit of travel) such that when the
-        vertical and horizontal gap are set equal, the gap between blades will appear square
-        and not rectangular.
+        Calculate scaling factors that will be used to scale the blade GUI elements
+        down relative to the jaw with the largest maximum height. This ensures that
+        e.g. if a square gap is set in the PVs then the gap appears square instead
+        of rectangular.
         """
-        if self.max_x > self.max_y:
-            self.scaling_factor = self.max_y/self.max_x
-        elif self.max_y > self.max_x:
-            self.scaling_factor = self.max_x/self.max_y
-        else:
-            self.scaling_factor = 1.0
+        self.scaling_factor_n = self.max_n/highest_jaw_limit
+        self.scaling_factor_e = self.max_e/highest_jaw_limit
+        self.scaling_factor_s = self.max_s/highest_jaw_limit
+        self.scaling_factor_w = self.max_w/highest_jaw_limit
+    
+    def get_highest_limit(self):
+        return max([self.max_n, self.max_e, self.max_w, self.max_s])
+        
             
     def run(self):
         while True:
@@ -90,32 +103,32 @@ class UpdateJaws(Runnable):
 
             self.get_pvs()
             
-            self.calc_scaling_factor()
-            if self.max_x > self.max_y:
-                self.north *= self.scaling_factor
-                self.south *= self.scaling_factor
-            elif self.max_y > self.max_x:
-                self.east *= self.scaling_factor
-                self.west *= self.scaling_factor
+            highest_jaw_limit = self.get_highest_limit()
+            self.calc_scaling_factor(highest_jaw_limit)
+            self.north *= self.scaling_factor_n
+            self.east *= self.scaling_factor_e
+            self.south *= self.scaling_factor_s
+            self.west *= self.scaling_factor_w
 
             north_bound_val, south_bound_val, west_bound_val, east_bound_val = self.calc_bound_values()
                 
-            north_height, south_height = self.calc_pair_height((self.north, self.south), self.max_y, background_height)
-            east_width, west_width = self.calc_pair_height((self.east, self.west), self.max_x, background_width)
+            north_height, east_width, south_height, west_width = self.calc_gui_heights(background_width, background_height)
             
             south_y = background_y + background_height - south_height
             east_x = background_x + background_width - east_width
             
             #Make GUI changes on the GUI thread only
-            max_x = self.max_x
-            max_y = self.max_y
+            max_n = self.max_n
+            max_e = self.max_e
+            max_s = self.max_s
+            max_w = self.max_w                       
             class UITask(Runnable):
                 def run(self):
                     #Set limit of travel indicators to be visible only for the shorter axis
-                    north_bound.setPropertyValue("visible", max_x > max_y)
-                    south_bound.setPropertyValue("visible", max_x > max_y) 
-                    east_bound.setPropertyValue("visible", max_y > max_x)
-                    west_bound.setPropertyValue("visible", max_y > max_x)                       
+                    north_bound.setPropertyValue("visible", max_n/highest_jaw_limit < 1.0)
+                    south_bound.setPropertyValue("visible", max_s/highest_jaw_limit < 1.0) 
+                    east_bound.setPropertyValue("visible", max_e/highest_jaw_limit < 1.0)
+                    west_bound.setPropertyValue("visible", max_w/highest_jaw_limit < 1.0)                       
                     north_bound.setPropertyValue("y", north_bound_val)
                     south_bound.setPropertyValue("y", south_bound_val)
                     east_bound.setPropertyValue("x", east_bound_val)
