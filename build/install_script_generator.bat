4aa836b67d5f049e53fb06c9cb12d88dac44f636
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2020-05-19 12:17:23 +0100

    Use robocopy rather than xcopy

--- build/install_script_generator.bat
+++ build/install_script_generator.bat
@@ -2,7 +2,7 @@ setlocal enabledelayedexpansion
 REM When run will install all the script generator stuff (gui, genie_python, epics_utils)
 
 REM %~dp0 expands to directory where this file lives
-set BASEDIR=%~dp0
+set "BASEDIR=%~dp0"
 
 if not exist "%BASEDIR%COPY_COMPLETE.txt" (
     @echo ERROR script generator copy in %BASEDIR% is not complete
@@ -12,8 +12,8 @@ REM Copy the script generator files across
 set APPSDIR=C:\Instrument\Apps
 set SCRIPTGENDIR=%APPSDIR%\script_generator
 mkdir %SCRIPTGENDIR%
-xcopy /q /s /e /h %BASEDIR%script_generator %SCRIPTGENDIR%
-if %errorlevel% neq 0 (
+robocopy "%BASEDIR%script_generator" "%SCRIPTGENDIR%" /E /R:2 /MT /NFL /NDL /NP /NC /NS /LOG:NUL
+if %errorlevel% geq 4 (
     @echo ERROR copying SCRIPT GENERATOR
 )
 
