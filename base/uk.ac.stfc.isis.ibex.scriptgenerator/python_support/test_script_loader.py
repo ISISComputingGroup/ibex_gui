114bbb384b65228034d709488f8cb24204cf042f
Alistair McGann <alistair.mcgann@tessella.com>
2020-06-08 16:34:26 +0100

    New test for changing remote url

--- base/uk.ac.stfc.isis.ibex.scriptgenerator/python_support/test_script_loader.py
+++ base/uk.ac.stfc.isis.ibex.scriptgenerator/python_support/test_script_loader.py
@@ -5,10 +5,6 @@ from mock import Mock
 
 
 class DefinitionsRepoTests(unittest.TestCase):
-
-    def mock_unbundle(_, repo_path):
-        pass
-
     @patch("script_definition_loader.Repo")
     @patch("script_definition_loader.Git")
     def test_GIVEN_uninitialised_directory_WHEN_repo_cloned_THEN_git_called_with_correct_arguments(self, git_clone, mock_repo):
@@ -19,8 +15,15 @@ class DefinitionsRepoTests(unittest.TestCase):
         unbundle_script_definitions_repository(bundle_path, repo_path, "")
         git_instance_clone.clone.assert_called_with(bundle_path, repo_path)
 
+    @patch("script_definition_loader.Repo")
+    @patch("script_definition_loader.Git")
+    def test_GIVEN_uninitialised_directory_WHEN_repo_cloned_THEN_new_repo_has_correct_remote_URL(self, git_clone, mock_repo):
+        repo_instance_clone = Mock()
+        mock_repo.return_value = repo_instance_clone
+        bundle_path = "path/to/bundle"
+        repo_path = "path/to/repo"
+        upstream_url = "upstream_url.com"
+        unbundle_script_definitions_repository(bundle_path, repo_path, upstream_url)
+        repo_instance_clone.delete_remote.assert_called_with("origin")
+        repo_instance_clone.create_remote.assert_called_with("origin", upstream_url)
 
-
-
-    # def test_GIVEN_uninitialised_directory_WHEN_repo_cloned_THEN_directory_gets_initialised(self):
-    #     @patch.object(Git, 'clone', new=)
