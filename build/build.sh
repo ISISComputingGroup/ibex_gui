a5aa6f601e2eacfabbbb69809c8d66c5f1a6fd95
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-06-29 14:53:13 +0100

    adjust various dialog sizes to accomodate GTK widgets

--- build/build.sh
+++ build/build.sh
@@ -11,7 +11,7 @@ mvn --settings="$TOPPATH/mvn_user_settings.xml" \
     -DskipTests \
     clean verify
     
-cp -r "//isis/inst$/Kits$/CompGroup/ICP/ibex_client_jre_linux" "$TOPPATH/base/uk.ac.stfc.isis.ibex.client.product/target/products/ibex.product/linux/gtk/x86_64/jre"
+cp -r "\\isis\inst$\Kits$\CompGroup\ICP\ibex_client_jre_linux" "Client built in $TOPPATH/base/uk.ac.stfc.isis.ibex.client.product/target/products/ibex.product/linux/gtk/x86_64/jre"
 
 echo "Client built in $TOPPATH/base/uk.ac.stfc.isis.ibex.client.product/target/products/ibex.product/linux/gtk/x86_64"
 
