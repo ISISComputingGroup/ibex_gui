235fd2a74d1854d944b5dcec9af95dccb51024de
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-06-29 12:05:14 +0100

    Disable unit tests on linux for now.

--- build/build.sh
+++ build/build.sh
@@ -5,11 +5,10 @@ SCRIPT=$(readlink -f "${BASH_SOURCE[0]}")
 SCRIPTPATH="`dirname \"$SCRIPT\"`"
 TOPPATH="`dirname \"$SCRIPTPATH\"`"
 
-#sed -i -e 's%<os>win32</os>%<os>linux</os>%' -e 's%<ws>win32</ws>%<ws>gtk</ws>%'  $TOPPATH/base/uk.ac.stfc.isis.ibex.client.tycho.parent/pom.xml
-
 mvn --settings="$TOPPATH/mvn_user_settings.xml" \
     -f "$TOPPATH/base/uk.ac.stfc.isis.ibex.client.tycho.parent/pom.xml" \
     -DforceContextQualifier="$BUILD_NUMBER" \
+    -DskipTests \
     clean verify
 
 echo "Client built in $TOPPATH/base/uk.ac.stfc.isis.ibex.client.product/target/products/ibex.product/"
