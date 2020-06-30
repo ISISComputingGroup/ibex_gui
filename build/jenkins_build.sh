cdefb79bc0ce96933779f73dcea563071bb0181e
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2020-06-30 19:32:48 +0100

    Copy linux client to babylon

--- build/jenkins_build.sh
+++ build/jenkins_build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 sh build.sh
+mkdir -p /misc/babylon/Scratch/IBEX/
+rm -fr /misc/babylon/Scratch/IBEX/Client
+cp -r ./base/uk.ac.stfc.isis.ibex.e4.client.product/target/products/ibex.product/linux/gtk/x86_64  /misc/babylon/Scratch/IBEX/Client
