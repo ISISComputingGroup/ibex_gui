d01f7502a53dd1166e81d132bf4359cb381fcc14
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2020-06-30 19:44:24 +0100

    Correct path

--- build/jenkins_build.sh
+++ build/jenkins_build.sh
@@ -2,4 +2,4 @@
 sh build.sh
 mkdir -p /misc/babylon/Scratch/IBEX/
 rm -fr /misc/babylon/Scratch/IBEX/Client
-cp -r ./base/uk.ac.stfc.isis.ibex.e4.client.product/target/products/ibex.product/linux/gtk/x86_64  /misc/babylon/Scratch/IBEX/Client
+cp -r ../base/uk.ac.stfc.isis.ibex.e4.client.product/target/products/ibex.product/linux/gtk/x86_64  /misc/babylon/Scratch/IBEX/Client
