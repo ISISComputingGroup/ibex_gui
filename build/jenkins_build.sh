e234664eb155fd567bb8785158368a88b6337058
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2020-06-30 19:48:47 +0100

    Update jenkins_build.sh

--- build/jenkins_build.sh
+++ build/jenkins_build.sh
@@ -4,3 +4,4 @@ sh build.sh
 mkdir -p /misc/babylon/Scratch/IBEX/
 rm -fr /misc/babylon/Scratch/IBEX/Client
 cp -r ../base/uk.ac.stfc.isis.ibex.e4.client.product/target/products/ibex.product/linux/gtk/x86_64  /misc/babylon/Scratch/IBEX/Client
+echo Client deployed to  babylon/Scratch/IBEX/Client
