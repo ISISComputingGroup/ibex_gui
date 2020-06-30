408500004899aa7df729caf86192603046a4c9ce
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2020-06-30 19:53:59 +0100

    Deploy tgz

--- build/jenkins_build.sh
+++ build/jenkins_build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -o errexit
 sh build.sh
-mkdir -p /misc/babylon/Scratch/IBEX/
-rm -fr /misc/babylon/Scratch/IBEX/Client
-cp -r ../base/uk.ac.stfc.isis.ibex.e4.client.product/target/products/ibex.product/linux/gtk/x86_64  /misc/babylon/Scratch/IBEX/Client
-echo Client deployed to  babylon/Scratch/IBEX/Client
+mkdir -p /misc/babylon/Scratch/IBEX/Linux
+( cd ../base/uk.ac.stfc.isis.ibex.e4.client.product/target/products/ibex.product/linux/gtk/x86_64; tar czf /misc/babylon/Scratch/IBEX/Linux/ibex_client.tgz . )
+echo Client deployed to  babylon/Scratch/IBEX/Linux
