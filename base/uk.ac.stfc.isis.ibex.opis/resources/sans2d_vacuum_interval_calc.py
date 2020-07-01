5c047958ff044304f6bc3c0795e768b68ea76f90
JamesKingWork <james.c.king@stfc.ac.uk>
2020-07-01 09:32:21 +0100

    Tooltip for intervals

--- base/uk.ac.stfc.isis.ibex.opis/resources/sans2d_vacuum_interval_calc.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/sans2d_vacuum_interval_calc.py
@@ -1,6 +1,10 @@
 from org.csstudio.opibuilder.scriptUtil import PVUtil
 
-forward_component_z = PVUtil.getDouble(pvs[0])
-rear_component_z = PVUtil.getDouble(pvs[1])
+forward_component_pv = pvs[0]
+rear_component_pv = pvs[1]
+forward_component_z = PVUtil.getDouble(forward_component_pv)
+rear_component_z = PVUtil.getDouble(rear_component_pv)
+interval = rear_component_z - forward_component_z
 
-widget.setPropertyValue("text", str(rear_component_z - forward_component_z))
+widget.setPropertyValue("text", str(interval))
+widget.setPropertyValue("tooltip", str(forward_component_pv) + " - " + str(rear_component_pv) + "\n" + str(interval))
