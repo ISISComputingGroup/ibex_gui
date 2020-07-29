af34be55ffb1688a8c5f2f32336b6e03f2253eda
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2020-07-29 18:21:45 +0000

    Use jenkins build number for rpm release

--- build/build_rpm.sh
+++ build/build_rpm.sh
@@ -5,6 +5,9 @@ if test ! -d ~/rpmbuild; then
 fi
 VERSION=7.1
 BUILDVER=1
+if test ! -z "$1"; then
+    BUILDVER=$1
+fi
 rpmlint ibex-client.spec
 ## set QA_RPATHS as the embedded python shared libraries gives some RPATH errors
 env QA_RPATHS=2 rpmbuild -bb --define "ibexversion ${VERSION}" --define "ibexbuild ${BUILDVER}" ibex-client.spec
