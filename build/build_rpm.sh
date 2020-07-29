ce68b38d3734328b76dee4e62e9aa9af883925c3
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2020-07-29 18:02:56 +0000

    Ignore rpath errors

--- build/build_rpm.sh
+++ build/build_rpm.sh
@@ -6,5 +6,6 @@ fi
 VERSION=7.1
 BUILDVER=1
 rpmlint ibex-client.spec
-rpmbuild -bb --define "ibexversion ${VERSION}" --define "ibexbuild ${BUILDVER}" ibex-client.spec
+## set QA_RPATHS as the embedded python shared libraries gives some RPATH errors
+env QA_RPATHS=2 rpmbuild -bb --define "ibexversion ${VERSION}" --define "ibexbuild ${BUILDVER}" ibex-client.spec
 #rpmlint ~/rpmbuild/RPMS/x86_64/ibex-client-1-1.el7.x86_64.rpm
