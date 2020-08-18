903f18a7ce5f84d22384c6d242bf0641aa3c92ac
Mihai Stavila <mihai.stavila@stfc.ac.uk>
2020-08-18 20:01:49 +0100

    adjust _check_path_valid return type

--- base/uk.ac.stfc.isis.ibex.opis/check_opi_info.py
+++ base/uk.ac.stfc.isis.ibex.opis/check_opi_info.py
@@ -4,39 +4,46 @@ import xml.etree.ElementTree as ET
 import unittest
 
 DEFAULT_ROOT_DIR = os.path.join(os.path.dirname(__file__), "resources")
+OPI_INFO_PATH = os.path.join(DEFAULT_ROOT_DIR, "opi_info.xml")
 
 
 class TestOpiInfo(unittest.TestCase):
     def _check_path_valid(self, path_str):
+        """
+        Checks that the given opi_info.xml entry path is valid.
+        Args:
+            path_str (str): The path to verify (of a device entry in the opi_info.xml file)
+        Returns:
+            string: The error message, or None if there is no error
+        """
         full_path = os.path.join(DEFAULT_ROOT_DIR, path_str)
         # check path exists
         if not os.path.exists(full_path):
-            return ["opi_info.xml error: {} was not found".format(path_str)]
+            return "opi_info.xml error: {} was not found".format(path_str)
 
         # check path has no windows-style backslashes
         if '\\' in path_str:
-            return ['opi_info.xml error: "{}" contains windows-style backslash (\\)!'.format(path_str)]
+            return 'opi_info.xml error: "{}" contains windows-style backslash (\\)!'.format(path_str)
 
         # check file case sensitivity on xml and system
         filesystem_path = win32api.GetLongPathName(win32api.GetShortPathName(full_path)).split("resources\\")[1]
         if path_str != filesystem_path:
-            return ['opi_info.xml error: info path does not match filesystem path - "{}", should be "{}"'
-                    .format(path_str, filesystem_path)]
+            return 'opi_info.xml error: info path does not match filesystem path - "{}", should be "{}"' \
+                .format(path_str, filesystem_path)
 
-        return []
+        return None
 
     def test_opi_info(self):
-        file_path = os.path.join(DEFAULT_ROOT_DIR, "opi_info.xml")
-
         # if opi_info.xml was not found
-        if not os.path.exists(file_path):
-            self.fail("Could not find file path {}".format(file_path))
+        if not os.path.exists(OPI_INFO_PATH):
+            self.fail("Could not find file path {}".format(OPI_INFO_PATH))
 
-        tree = ET.parse(file_path)
+        tree = ET.parse(OPI_INFO_PATH)
         root = tree.getroot()
         errors = []
         for entry in root.iter('path'):
-            errors.extend(self.check_path_valid(entry.text))
+            if self._check_path_valid(entry.text):  # if an error message is returned
+                errors.extend([self._check_path_valid(entry.text)])
 
         if errors:
             self.fail("\n" + "\n".join(errors))
