71fd074f47bfa84a6aa9a7fbfb486a9afcaf3938
Mihai Stavila <mihai.stavila@stfc.ac.uk>
2020-09-14 17:01:38 +0100

    Added types to ComponentType, resized icons/thumbs, changed some OPI types
    Added DEBUG, (SANS2D) VACUUM_TANK and HE_LEVEL_GAUGE types to the ComponentType.
    
    Changed opi_info.xml GALIL_ENGINEERING and UNCONFIRMED to DEBUG.
    
    Resized icons and thumbnails to conform to conventions.
    
    Changed IconConventionsTest to IconConventionsCheck (as it is more of a system test than a unit test).
    Added IconConventionsCheckTest to unit test the conventions checks.
    
    Added more checks to see if a declared type has an icon, and if each icon has a corresponding thumbnail and vice versa.

---
+++ base/uk.ac.stfc.isis.ibex.ui.devicescreens.tests/src/uk/ac/stfc/isis/ibex/ui/devicescreens/tests/IconConventionsCheckTest.java
@@ -0,0 +1,59 @@
+//CHECKSTYLE:OFF
+
+/*
+ * This file is part of the ISIS IBEX application.
+ * Copyright (C) 2012-2020 Science & Technology Facilities Council.
+ * All rights reserved.
+ *
+ * This program is distributed in the hope that it will be useful.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution.
+ * EXCEPT AS EXPRESSLY SET FORTH IN THE ECLIPSE PUBLIC LICENSE V1.0, THE PROGRAM 
+ * AND ACCOMPANYING MATERIALS ARE PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES 
+ * OR CONDITIONS OF ANY KIND.  See the Eclipse Public License v1.0 for more details.
+ *
+ * You should have received a copy of the Eclipse Public License v1.0
+ * along with this program; if not, you can obtain a copy from
+ * https://www.eclipse.org/org/documents/epl-v10.php or 
+ * http://opensource.org/licenses/eclipse-1.0.php
+ */
+
+package uk.ac.stfc.isis.ibex.ui.devicescreens.tests;
+
+import static org.junit.Assert.*;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import uk.ac.stfc.isis.ibex.devicescreens.components.ComponentType;
+import uk.ac.stfc.isis.ibex.ui.devicescreens.ComponentIcons;
+import uk.ac.stfc.isis.ibex.ui.devicescreens.tests.IconConventionsCheck;
+
+/**
+ * Unit tests for the icon conventions checks
+ */
+public class IconConventionsCheckTest {
+	
+	
+}
+
+//CHECKSTYLE:ON
