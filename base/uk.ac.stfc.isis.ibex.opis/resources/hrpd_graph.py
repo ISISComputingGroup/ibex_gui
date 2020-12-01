1aa7b50426de99d1147c7899ced881a6c902752f
JamesKingWork <james.c.king@stfc.ac.uk>
2020-12-01 09:51:53 +0000

    Make script more efficient

--- base/uk.ac.stfc.isis.ibex.opis/resources/hrpd_graph.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/hrpd_graph.py
@@ -1,19 +1,23 @@
 from org.csstudio.opibuilder.scriptUtil import PVUtil
 import sys
 
+pv_prefix = display.getMacroValue("P")
 graph_source_val = pvs[0].getValue().getValue()
 graph_source_map = {
-    "Sensor 1": "TE:NDLT1208:CS:SB:DSC_CERNOX_1",
-    "Sensor 2": "TE:NDLT1208:CS:SB:DSC_CERNOX_2",
-    "Sensor 3": "TE:NDLT1208:CS:SB:DSC_CERNOX_3",
-    "Sensor 4": "TE:NDLT1208:CS:SB:DSC_CERNOX_4",
-    "Differential 1": "TE:NDLT1208:DSC:DIFFERENTIAL_1",
-    "Differential 2": "TE:NDLT1208:DSC:DIFFERENTIAL_2",
-    "Differential 3": "TE:NDLT1208:DSC:DIFFERENTIAL_3",
-    "Differential 4": "TE:NDLT1208:DSC:DIFFERENTIAL_4"
+    "Sensor 1": "{}CS:SB:DSC_CERNOX_1".format(pv_prefix),
+    "Sensor 2": "{}CS:SB:DSC_CERNOX_2".format(pv_prefix),
+    "Sensor 3": "{}CS:SB:DSC_CERNOX_3".format(pv_prefix),
+    "Sensor 4": "{}CS:SB:DSC_CERNOX_4".format(pv_prefix),
+    "Differential 1": "{}DSC:DIFFERENTIAL_1".format(pv_prefix),
+    "Differential 2": "{}DSC:DIFFERENTIAL_2".format(pv_prefix),
+    "Differential 3": "{}DSC:DIFFERENTIAL_3".format(pv_prefix),
+    "Differential 4": "{}DSC:DIFFERENTIAL_4".format(pv_prefix)
 }
+
+# Clear graph if selected graph source changes
 trace_current = widget.getPropertyValue("trace_0_name")
 if graph_source_val != trace_current:
     widget.clearGraph()
-widget.setPropertyValue("trace_0_y_pv", graph_source_map[graph_source_val])
-widget.setPropertyValue("trace_0_name", graph_source_val)
+    # Set pv and name correctly
+    widget.setPropertyValue("trace_0_y_pv", graph_source_map[graph_source_val])
+    widget.setPropertyValue("trace_0_name", graph_source_val)
