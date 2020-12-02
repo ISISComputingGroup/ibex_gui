965cd21afdd7ee78328f7d8fbaf606e607b41c35
JamesKingWork <james.c.king@stfc.ac.uk>
2020-12-02 16:18:04 +0000

    Graph working correctly with x axis title

--- base/uk.ac.stfc.isis.ibex.opis/resources/hrpd_graph.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/hrpd_graph.py
@@ -1,28 +1,57 @@
 from org.csstudio.opibuilder.scriptUtil import PVUtil
-from org.csstudio.opibuilder.util import BOYPVFactory
-import sys
+from java.lang import Thread, Runnable
+from org.csstudio.ui.util.thread import UIBundlingThread
+from org.csstudio.opibuilder.scriptUtil import ConsoleUtil
+from org.eclipse.swt.widgets import Display
 
-pv_prefix = display.getMacroValue("P")
-graph_source_val = pvs[0].getValue().getValue()
-graph_source_map = {
-    "Sensor 1": "{}CS:SB:DSC_CERNOX_1".format(pv_prefix),
-    "Sensor 2": "{}CS:SB:DSC_CERNOX_2".format(pv_prefix),
-    "Sensor 3": "{}CS:SB:DSC_CERNOX_3".format(pv_prefix),
-    "Sensor 4": "{}CS:SB:DSC_CERNOX_4".format(pv_prefix),
-    "Differential 1": "{}DSC:DIFFERENTIAL_1".format(pv_prefix),
-    "Differential 2": "{}DSC:DIFFERENTIAL_2".format(pv_prefix),
-    "Differential 3": "{}DSC:DIFFERENTIAL_3".format(pv_prefix),
-    "Differential 4": "{}DSC:DIFFERENTIAL_4".format(pv_prefix)
-}
 
-# Clear graph if selected graph source changes
-trace_current = widget.getPropertyValue("trace_0_name")
-trace_pv = graph_source_map[graph_source_val]
-y_axis_pv = BOYPVFactory.createPV(trace_pv + ".EGU")
-y_axis_title = PVUtil.getDouble(y_axis_pv)
-if graph_source_val != trace_current:
-    widget.clearGraph()
-    # Set pv and name correctly
-    widget.setPropertyValue("trace_0_y_pv", graph_source_map[graph_source_val])
-    widget.setPropertyValue("trace_0_name", graph_source_val)
-    widget.setPropertyValue("axis_1_axis_title", y_axis_title)
+class GraphListener(Runnable):
+
+    def __init__(self):
+
+        self.graph_source_to_pv_map = {
+            "Sensor 1": {"VAL": 1, "EGU": 2, "PV": "$(P)CS:SB:DSC_CERNOX_1"},
+            "Sensor 2": {"VAL": 3, "EGU": 4, "PV": "$(P)CS:SB:DSC_CERNOX_2"},
+            "Sensor 3": {"VAL": 5, "EGU": 6, "PV": "$(P)CS:SB:DSC_CERNOX_3"},
+            "Sensor 4": {"VAL": 7, "EGU": 8, "PV": "$(P)CS:SB:DSC_CERNOX_4"},
+            "Differential 1": {"VAL": 9, "EGU": 10, "PV": "$(P)DSC:DIFFERENTIAL_1"},
+            "Differential 2": {"VAL": 11, "EGU": 12, "PV": "$(P)DSC:DIFFERENTIAL_2"},
+            "Differential 3": {"VAL": 13, "EGU": 14, "PV": "$(P)DSC:DIFFERENTIAL_3"},
+            "Differential 4": {"VAL": 15, "EGU": 16, "PV": "$(P)DSC:DIFFERENTIAL_4"}
+        }
+
+        super(GraphListener, self).__init__()
+
+    def get_graph_source(self):
+        return pvs[0].getValue().getValue()
+    
+    def get_graph_source_pv_underlying_pv(self):
+        return pvs[self.graph_source_to_pv_map[self.get_graph_source()]["VAL"]].getName()
+    
+    def get_graph_source_underlying_pv_egu(self):
+        return pvs[self.graph_source_to_pv_map[self.get_graph_source()]["EGU"]].getValue().getValue()  
+    
+    def run(self):
+        while True:
+            if not display.isActive():
+                return
+            # Clear graph if selected graph source changes
+            trace_current = widget.getPropertyValue("trace_0_name")
+            trace_pv = self.get_graph_source_pv_underlying_pv()
+            trace_pv_egu = self.get_graph_source_underlying_pv_egu()
+            graph_source = self.get_graph_source()
+            if graph_source != trace_current:
+                class UITask(Runnable):
+                    def run(self):
+                        widget.clearGraph()
+                        # Set pv and name correctly
+                        widget.setPropertyValue("trace_0_name", graph_source)
+                        widget.setPropertyValue("axis_1_axis_title", trace_pv_egu)
+                        widget.setPropertyValue("trace_0_y_pv", trace_pv)
+                UIBundlingThread.getInstance().addRunnable(Display.getCurrent(), UITask())
+            
+            Thread.sleep(200)
+
+thread = Thread(GraphListener())
+thread.start()
+    
