866071324080d497fadc1bded670cac2af4294f7
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2021-03-15 13:47:05 +0000

    Remove pushd/popd

--- build/copy_python.bat
+++ build/copy_python.bat
@@ -1,8 +1,7 @@
 REM Get the latest genie python build with LATEST_PYTHON and LATEST_PYTHON_DIR as the vars
+setlocal
 
-pushd \\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP
-
-set "KITS_ICP_PATH=%cd%"
+set "KITS_ICP_PATH=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP"
 set "PYCOPYPATH=%1"
 
 REM Get latest build
@@ -13,7 +12,6 @@ if exist "%KITS_ICP_PATH%\genie_python_3\LATEST_BUILD.txt" (
 	)
 ) else (
 	@echo Could not access LATEST_BUILD.txt
-    popd
 	exit /b 1
 )
 
@@ -23,16 +21,15 @@ if %errcode% GEQ 4 (
 	@echo robocopy error
     @echo *** Exit Code %errcode% ERROR see %INSTALLDIR%install.log ***
 	@echo ************** Exit Code %errcode% ERROR **************** >>%INSTALLDIR%install.log
-	popd
 	exit /b %errcode%
 ) else (
 	set ERRORLEVEL=0
 )
-
-popd
 
 if not exist %PYCOPYPATH%\python.exe (
 	@echo Python not copied correctly>>%INSTALLDIR%install.log
 	@echo Python not copied correctly
 	exit /b 1
 )
+
+exit /b 0
