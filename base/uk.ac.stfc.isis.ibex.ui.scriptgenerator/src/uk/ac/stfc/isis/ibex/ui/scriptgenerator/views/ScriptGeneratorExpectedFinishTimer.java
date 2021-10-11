e48bd810664bdbab985d0711e13a2253f13d7688
Sam Jenkins <s.jenkins@stfc.ac.uk>
2021-10-11 09:39:23 +0100

    added beginning of work on timer

---
+++ base/uk.ac.stfc.isis.ibex.ui.scriptgenerator/src/uk/ac/stfc/isis/ibex/ui/scriptgenerator/views/ScriptGeneratorExpectedFinishTimer.java
@@ -0,0 +1,34 @@
+package uk.ac.stfc.isis.ibex.ui.scriptgenerator.views;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.eclipse.core.databinding.beans.typed.BeanProperties;
+import org.eclipse.jface.databinding.swt.typed.WidgetProperties;
+import org.eclipse.swt.widgets.Label;
+
+
+/***
+ * 
+ * Helper class that calculates expected finish time.
+ *
+ */
+public class ScriptGeneratorExpectedFinishTimer implements Runnable{
+	private Label expectedFinishTime;
+	private volatile long timeEstimateVal;
+	
+	public ScriptGeneratorExpectedFinishTimer(Label expectedFinishTime, long timeEstimateVal) {
+		this.expectedFinishTime = expectedFinishTime;
+		this.timeEstimateVal = timeEstimateVal;
+	}
+	
+	public void SetTimeEstimateVal(long timeEstimateVal) {
+		this.timeEstimateVal = timeEstimateVal;
+	}
+	public void run() {
+		LocalDateTime currentTime = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+		expectedFinishTime.setText(currentTime.format(formatter));
+		
+	}
+}
