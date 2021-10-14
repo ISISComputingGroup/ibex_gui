7e3d48b6f858b5f93293bdcc2844965d9143faa1
Dominic Oram <dominic.oram@stfc.ac.uk>
2021-10-14 12:01:01 +0100

    Avoid getting spectra plots stuck in endless loop

--- base/uk.ac.stfc.isis.ibex.opis/resources/spectra_plots_period_str.py
+++ base/uk.ac.stfc.isis.ibex.opis/resources/spectra_plots_period_str.py
@@ -3,22 +3,24 @@ from org.csstudio.opibuilder.scriptUtil import PVUtil
 periodPv = pvs[0]
 periodPv_str = pvs[1]
 
-def main():
+string_for_period_0 = "Current"
 
+def main():
     if periodPv_str.isConnected() and widget.getPropertyValue("visible"):
         period_str = PVUtil.getString(periodPv_str)
-        if period_str != "Current":
+        if period_str != string_for_period_0:
             try:
                 period = int(float(period_str))
                 PVUtil.writePV(periodPv.getName(), period)
             except ValueError as e:
-                PVUtil.writePV(periodPv_str.getName(), "Current")
+                PVUtil.writePV(periodPv_str.getName(), string_for_period_0)
 
     if periodPv.isConnected():
         period = PVUtil.getLong(periodPv)
         if period == 0:
             widget.setPropertyValue("visible", True)
-            PVUtil.writePV(periodPv_str.getName(), "Current")
+            if PVUtil.getString(periodPv_str) != string_for_period_0:
+                PVUtil.writePV(periodPv_str.getName(), string_for_period_0)
         else:
             widget.setPropertyValue("visible", False)
 
