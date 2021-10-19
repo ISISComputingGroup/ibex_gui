cf500efb7300c8834ea71360bf54a6de3a21133d
ThomasLohnert <thomas.lohnert@stfc.ac.uk>
2021-10-19 12:50:49 +0100

    checkstyle

--- base/uk.ac.stfc.isis.ibex.beamstatus/src/uk/ac/stfc/isis/ibex/beamstatus/FacilityPV.java
+++ base/uk.ac.stfc.isis.ibex.beamstatus/src/uk/ac/stfc/isis/ibex/beamstatus/FacilityPV.java
@@ -3,22 +3,29 @@ package uk.ac.stfc.isis.ibex.beamstatus;
 import uk.ac.stfc.isis.ibex.model.UpdatedValue;
 
 /**
- * A class to create facility PV objects
+ * A class to create facility PV objects.
  *
  */
 public class FacilityPV {
 
+	/**
+	 * The source PV address.
+	 */
 	public String pv;
+	
+	/**
+	 * The object holding the latest PV value.
+	 */
 	public UpdatedValue<String> updatedValue;
 
 	/**
-	 * Constructor for creating a facility PV
+	 * Constructor for creating a facility PV.
 	 * 
-	 * @param PV           Pv address
+	 * @param pv           Pv address
 	 * @param updatedValue Value of the pv
 	 */
-	public FacilityPV(String PV, UpdatedValue<String> updatedValue) {
-		this.pv = PV;
+	public FacilityPV(String pv, UpdatedValue<String> updatedValue) {
+		this.pv = pv;
 		this.updatedValue = updatedValue;
 
 	}
