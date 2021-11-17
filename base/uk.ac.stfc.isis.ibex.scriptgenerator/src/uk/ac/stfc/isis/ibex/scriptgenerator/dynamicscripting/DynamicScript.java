90fc17809bf8d7e86ae81b88a3a960cfc58e5126
JamesKingWork <james.c.king@stfc.ac.uk>
2021-11-17 16:45:04 +0000

    Script generator dynamic scripting: Add javadoc for dynamic script

--- base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/dynamicscripting/DynamicScript.java
+++ base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/dynamicscripting/DynamicScript.java
@@ -2,16 +2,31 @@ package uk.ac.stfc.isis.ibex.scriptgenerator.dynamicscripting;
 
 import java.util.Optional;
 
+/**
+ * A data transfer object for a dynamic script.
+ */
 public class DynamicScript {
 	
 	private Integer id;
 	private Optional<String> code;
 	
+	/**
+	 * Create the dynamic script with the id and no code.
+	 * 
+	 * @param id The id of the dynamic script.
+	 */
 	public DynamicScript(Integer id) {
 		this.id = id;
 		this.code = Optional.empty();
 	}
 	
+	/**
+	 * Given the name, obtain the ID of the script or an exception if it is not in the correct format.
+	 * 
+	 * @param name The name of the script.
+	 * @return The ID of the script.
+	 * @throws DynamicScriptNameFormatException Thrown if the name is not of the correct format to get the ID from.
+	 */
 	public static Integer getIdFromName(String name) throws DynamicScriptNameFormatException {
 		String prefix = "Script Generator: ";
 		if (name.startsWith(prefix)) {
@@ -27,18 +42,32 @@ public class DynamicScript {
 		}
 	}
 	
+	/**
+	 * @return The name of the script.
+	 */
 	public String getName() {
 		return String.format("Script Generator: %d", id);
 	}
 	
+	/**
+	 * @return The ID of the script.
+	 */
 	public Integer getId() {
 		return id;
 	}
 	
+	/**
+	 * Set the code for this script.
+	 * 
+	 * @param code The code to set
+	 */
 	public void setCode(String code) {
 		this.code = Optional.of(code);
 	}
 	
+	/**
+	 * @return An optional of the code with the runscript() call appended or an empty optional if not set.
+	 */
 	public Optional<String> getCode() {
 		if (code.isPresent()) {
 			return Optional.of(code.get() + "\nrunscript()");
