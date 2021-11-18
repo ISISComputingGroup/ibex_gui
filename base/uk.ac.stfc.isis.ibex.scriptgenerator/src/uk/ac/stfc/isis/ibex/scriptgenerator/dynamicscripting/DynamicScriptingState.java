470cfae5fe93c614cff37a15788fb1ca796cf880
JamesKingWork <james.c.king@stfc.ac.uk>
2021-11-18 13:34:22 +0000

    Script generator dynamic scripting: Javadoc for dynamic scripting state

--- base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/dynamicscripting/DynamicScriptingState.java
+++ base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/dynamicscripting/DynamicScriptingState.java
@@ -9,43 +9,92 @@ import uk.ac.stfc.isis.ibex.model.HasStatus;
 import uk.ac.stfc.isis.ibex.model.ModelObject;
 import uk.ac.stfc.isis.ibex.scriptgenerator.table.ScriptGeneratorAction;
 
+/**
+ * An abstract base class for the states of dynamic scripting to inherit from.
+ */
 public abstract class DynamicScriptingState extends ModelObject implements HasStatus<DynamicScriptingStatus>, PropertyChangeListener {
 	
+	/**
+	 * A collection of dynamic script ids and their associated actions.
+	 */
 	protected HashMap<Integer, ScriptGeneratorAction> dynamicScriptIdsToAction;
 	
+	/**
+	 * Create a dynamic scripting state with the given ids and their associated actions.
+	 *
+	 * @param dynamicScriptIdsToAction Script ids and their associated actions that are used in dynamic scripting.
+	 */
 	public DynamicScriptingState(HashMap<Integer, ScriptGeneratorAction> dynamicScriptIdsToAction) {
 		this.dynamicScriptIdsToAction = dynamicScriptIdsToAction;
 	}
 	
+	/**
+	 * Check if the given script ID is a dynamic script.
+	 * 
+	 * @param scriptId The ID of the script to check.
+	 */
 	public Boolean isScriptDynamic(Integer scriptId) {
 		return dynamicScriptIdsToAction.containsKey(scriptId);
 	}
 	
+	/**
+	 * Get the ids and associated actions of the dynamic scripts.
+	 */
 	public HashMap<Integer, ScriptGeneratorAction> getDynamicScriptIds() {
 		return dynamicScriptIdsToAction;
 	}
 	
+	/**
+	 * Default function for getting the currently executing action (An optional empty).
+	 *
+	 * @return An empty optional.
+	 */
 	public Optional<ScriptGeneratorAction> getCurrentlyExecutingAction() {
 		return Optional.empty();
 	}
 
+	/**
+	 * Default function for getting the next action to execute (An optional empty).
+	 *
+	 * @return An empty optional.
+	 */
 	public Optional<ScriptGeneratorAction> getNextExecutingAction() {
 		return Optional.empty();
 	}
 	
-	public void play() { /* Default function does nothing */ };
+	/**
+	 * Default function to play from this state (does nothing).
+	 */
+	public void play() { };
 	
-	public void stop() { /* Default function does nothing */ };
+	/**
+	 * Default function to play from this state (does nothing).
+	 */
+	public void stop() { };
 	
-	public void pause() { /* Default function does nothing */ };
+	/**
+	 * Default function to play from this state (does nothing).
+	 */
+	public void pause() { };
 	
-	public void start() { /* Default function does nothing */ };
+	/**
+	 * Default function to start this state (does nothing).
+	 */
+	public void start() { };
 	
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) { /* Default function does nothing */ };
 	
+	/**
+	 * Get the status of the dynamic scripting in this state.
+	 */
 	public abstract DynamicScriptingStatus getStatus();
 	
+	/**
+	 * Fire a property change to indicate we need to change from this state to a state with the new status.
+	 *
+	 * @param newStatus The status of the new state to indicate what state needs changing to.
+	 */
 	protected void changeState(DynamicScriptingStatus newStatus) {
 		firePropertyChange(DynamicScriptingProperties.STATE_CHANGE_PROPERTY, this, newStatus);
 	}
