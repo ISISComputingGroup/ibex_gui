6751f4f5d4815e0c9b9d6c5905b8aa8733f35e1c
JamesKingWork <james.c.king@stfc.ac.uk>
2021-11-18 13:07:39 +0000

    Script generator dynamic scripting: Javadoc for dynamic scripting status

--- base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/dynamicscripting/DynamicScriptingStatus.java
+++ base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/dynamicscripting/DynamicScriptingStatus.java
@@ -2,11 +2,26 @@ package uk.ac.stfc.isis.ibex.scriptgenerator.dynamicscripting;
 
 import uk.ac.stfc.isis.ibex.model.HasStatus;
 
+/**
+ * Status of the dynamic scripting.
+ */
 public enum DynamicScriptingStatus implements HasStatus<DynamicScriptingStatus> {
 	
+	/**
+	 * Dynamic scripting is currently playing
+	 */
 	PLAYING,
+	/**
+	 * Dynamic scripting is currently stopped.
+	 */
 	STOPPED,
+	/**
+	 * Dynamic scripting is in error.
+	 */
 	ERROR,
+	/**
+	 * Dynamic scripting is currently paused.
+	 */
 	PAUSED;
 
 	@Override
