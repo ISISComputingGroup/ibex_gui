6ea6043c853c969cced70951335ba578e3ec7f20
JamesKingWork <james.c.king@stfc.ac.uk>
2021-11-18 13:54:45 +0000

    Script generator dynamic scripting: Javadoc for dynamic scripting model adapter

--- base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/dynamicscripting/DynamicScriptingModelAdapter.java
+++ base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/dynamicscripting/DynamicScriptingModelAdapter.java
@@ -14,16 +14,31 @@ import uk.ac.stfc.isis.ibex.scriptgenerator.generation.InvalidParamsException;
 import uk.ac.stfc.isis.ibex.scriptgenerator.generation.UnsupportedLanguageException;
 import uk.ac.stfc.isis.ibex.scriptgenerator.table.ScriptGeneratorAction;
 
+/**
+ * An adapter for dynamic scripting to use when interacting with the script generator model.
+ */
 public class DynamicScriptingModelAdapter extends ModelObject implements PropertyChangeListener {
 	
 	private ScriptGeneratorSingleton scriptGeneratorModel;
 	private Optional<DynamicScript> dynamicScript = Optional.empty();
 	private HashMap<Integer, DynamicScriptingException> errors = new HashMap<>();
 	
+	/**
+	 * Create an adapter with the script generator model it depends on injected.
+	 *
+	 * @param scriptGeneratorModel The injected script generator model dependency.
+	 */
 	public DynamicScriptingModelAdapter(ScriptGeneratorSingleton scriptGeneratorModel) {
 		this.scriptGeneratorModel = scriptGeneratorModel;
 	}
 
+	/**
+	 * Refresh the generated script using the given action.
+	 *
+     * @param action The action to create the script from.
+	 * @return An optional of the script ID or empty if generation fails.
+	 * @throws DynamicScriptingException If generation fails.
+	 */
 	public Optional<Integer> refreshGeneratedScript(ScriptGeneratorAction action) throws DynamicScriptingException {
 		try {
 			var scriptId = scriptGeneratorModel.refreshGeneratedScript(action);
@@ -41,6 +56,12 @@ public class DynamicScriptingModelAdapter extends ModelObject implements Propert
 		}
 	}
 	
+	/**
+	 * Get the action in the table that is to be executed after the given action.
+	 *
+	 * @param action The action that comes just before the action to be returned.
+	 * @return The next action after the one given or an optional empty if there isn't one.
+	 */
 	public Optional<ScriptGeneratorAction> getActionAfter(ScriptGeneratorAction action) {
 		List<ScriptGeneratorAction> actions = scriptGeneratorModel.getActions();
 		Optional<ScriptGeneratorAction> nextAction = Optional.empty();
@@ -53,14 +74,24 @@ public class DynamicScriptingModelAdapter extends ModelObject implements Propert
 		return nextAction;
 	}
 	
+	/**
+	 * @return the first action to execute.
+	 */
 	public Optional<ScriptGeneratorAction> getFirstAction() {
 		return scriptGeneratorModel.getAction(0);
 	}
 	
+	/**
+	 * @return an optional of the dynamic script that is being/has just been generated, or empty if there isn't one.
+	 */
 	public Optional<DynamicScript> getDynamicScript() {
 		return dynamicScript;
 	}
 
+	/**
+	 * Handle property changes fired when generating scripts.
+	 * {@inheritdoc}
+	 */
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
 		if (evt.getPropertyName().equals(ScriptGeneratorProperties.SCRIPT_GENERATION_ERROR_PROPERTY)) {
