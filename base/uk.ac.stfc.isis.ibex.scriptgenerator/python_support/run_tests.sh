f59c1a7179536211368f2702d5304f24c6b1d53c
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2022-03-06 12:49:49 +0000

    Fix script generator linux tests

--- base/uk.ac.stfc.isis.ibex.scriptgenerator/python_support/run_tests.sh
+++ base/uk.ac.stfc.isis.ibex.scriptgenerator/python_support/run_tests.sh
@@ -1,1 +1,2 @@
-python -m unittest discover .
+#!/bin/sh
+/usr/local/python/bin/python3 -m unittest discover .
--- build/run_python_support_tests.sh
+++ build/run_python_support_tests.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 pushd `dirname $0`
 SCRIPTDIR=`pwd`
 popd
 pushd $SCRIPTDIR/../base/uk.ac.stfc.isis.ibex.scriptgenerator/python_support
-./run_tests.sh
+sh ./run_tests.sh
 popd
