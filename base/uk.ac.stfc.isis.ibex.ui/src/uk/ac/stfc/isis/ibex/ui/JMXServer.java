73754ba9e24b146753e9c3b31d8568755bd7b209
Tom Willemsen <tomwillemsen1995@gmail.com>
2022-07-01 17:48:37 +0100

    Add JMS connection details to a file

--- base/uk.ac.stfc.isis.ibex.ui/src/uk/ac/stfc/isis/ibex/ui/JMXServer.java
+++ base/uk.ac.stfc.isis.ibex.ui/src/uk/ac/stfc/isis/ibex/ui/JMXServer.java
@@ -1,6 +1,9 @@
 package uk.ac.stfc.isis.ibex.ui;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
+import java.nio.file.Paths;
 
 import javax.management.MBeanServer;
 import javax.management.remote.JMXConnectorServer;
@@ -9,6 +12,7 @@ import javax.management.remote.JMXServiceURL;
 
 import org.apache.logging.log4j.Logger;
 import uk.ac.stfc.isis.ibex.logger.IsisLog;
+import uk.ac.stfc.isis.ibex.logger.LoggerUtils;
 
 /**
  * This starts a JMX server so we can remotely monitor the GUI.
@@ -16,6 +20,8 @@ import uk.ac.stfc.isis.ibex.logger.IsisLog;
  */
 public final class JMXServer {
 	private static final Logger LOG = IsisLog.getLogger(JMXServer.class);
+	
+	private static final String JMX_URL_PATH = Paths.get(System.getProperty("user.dir"), "jmx_connection.txt").toString();
 	
 	/**
 	 * Private constructor for JMX Server utility class.
@@ -34,6 +40,13 @@ public final class JMXServer {
 			JMXConnectorServer cs = JMXConnectorServerFactory.newJMXConnectorServer(url, null, mbs);
 			cs.start();
 		    LOG.info("JMX url: " + cs.getAddress().toString());
+		    
+		    // Also write JMX details to a separate file.
+		    try (FileWriter writer = new FileWriter(JMX_URL_PATH)) {
+				writer.write(cs.getAddress().toString());
+			} catch (RuntimeException | IOException e) {
+				LoggerUtils.logErrorWithStackTrace(LOG, e.getMessage(), e);
+			}
 		} catch (Exception e) {
 			LOG.error("Failed to start JMX server: " + e.getMessage());
 		}
