2342a0d4686335c29d953edc091ceee8d0ba2040
Nikola Roev <nikola.roev@stfc.ac.uk>
2022-09-28 10:50:37 +0100

    Check for multiple instances using file lock.

--- base/uk.ac.stfc.isis.ibex.e4.product/src/uk/ac/stfc/isis/ibex/e4/product/ApplicationWorkbenchWindowAdvisor.java
+++ base/uk.ac.stfc.isis.ibex.e4.product/src/uk/ac/stfc/isis/ibex/e4/product/ApplicationWorkbenchWindowAdvisor.java
@@ -23,7 +23,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
@@ -31,8 +30,6 @@ import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -77,6 +74,9 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
      */
     protected boolean shutDown = false;
     
+    /**
+     * Used in detecting multiple client instances.
+     */
     private FileChannel file = null;
 	private FileLock lock = null;
 
@@ -120,6 +120,7 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
      */
     void performMultipleClientsCheck() {
         try {
+        	createTempFile();
         	file = FileChannel.open(Paths.get(TEMP_PATH), StandardOpenOption.WRITE);
         	lock = file.tryLock();
         	if (lock == null) {
@@ -132,65 +133,6 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		} catch (Exception e) {
 			LoggerUtils.logErrorWithStackTrace(LOG, MULTIPLE_INSTANCES_CHECKING_ERROR, e);
 		}
-    }
-    
-    /**
-     * This function compares contents of temporary file to the list of PIDs obtained from process list
-     * and overwrites the file only with those that match.
-     * @return New contents of temporary file as a string array.
-     * @throws IOException - if any of the operations result in IOException.
-     */
-    String[] clearTempFile() throws IOException {
-    	String[] fileContents = readTempFile();
-    	
-        ArrayList<String> processPIDs = new ArrayList<String>();
-        ProcessHandle.allProcesses()
-        .forEach(process -> processPIDs.add(Long.toString(process.pid())));
-        
-        ArrayList<String> newContent = new ArrayList<String>();
-        
-        for (String line : fileContents) {
-        	for (String pid : processPIDs) {
-        		if (line.equals(pid)) {
-        			newContent.add(line);
-        			break;
-        		}
-        	}
-        }
-        String[] newContentArr = newContent.toArray(new String[newContent.size()]);
-        writeTempFile(newContentArr, false);
-        
-        return newContentArr;
-    }
-    
-    /**
-     * Helper function for writing to temporary file.
-     * @param lines Lines of text to write.
-     * @param append When true the lines will be appended. When false, the file will be
-     * Overwritten.
-     * @throws IOException - if any of the operations result in IOException.
-     */
-    void writeTempFile(String[] lines, boolean append) throws IOException, NoSuchElementException {
-    	createTempFile();
-    	String content = "";
-    	for (String line : lines) {
-    		content += line + '\n';
-    	}
-    	FileWriter writer = new FileWriter(TEMP_PATH, append);
-        writer.write(content);
-        writer.close();
-    }
-    
-    /**
-     * Helper function for getting info from temporary file.
-     * @return Lines of text in the temporary file.
-     * @throws IOException - if any of the operations result in IOException.
-     */
-    String[] readTempFile() throws IOException {
-    	createTempFile();
-		String content = Files.readString(Paths.get(TEMP_PATH));
-		String[] lines = content.split("\n");
-		return lines;
     }
     
     /**
