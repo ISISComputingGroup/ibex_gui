fb7ababe6748d9cf33b7f8f299d1666b89eeb961
Jack Harper <jack.harper@stfc.ac.uk>
2023-01-11 11:41:49 +0000

    fix checkstyle - remove unused import of java.util.List

--- base/uk.ac.stfc.isis.ibex.ui.banner.tests/src/uk/ac/stfc/isis/ibex/ui/banner/tests/ServerStatusViewModelTest.java
+++ base/uk.ac.stfc.isis.ibex.ui.banner.tests/src/uk/ac/stfc/isis/ibex/ui/banner/tests/ServerStatusViewModelTest.java
@@ -3,9 +3,6 @@ package uk.ac.stfc.isis.ibex.ui.banner.tests;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 
