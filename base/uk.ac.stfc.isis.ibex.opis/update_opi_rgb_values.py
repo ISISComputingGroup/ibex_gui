ce53c384a65234f33595abc1b45b2ef8b88f086f
will2201 <will.simms@stfc.ac.uk>
2023-08-22 14:52:03 +0100

    confirmation before running and xml failed to parse error added

--- base/uk.ac.stfc.isis.ibex.opis/update_opi_rgb_values.py
+++ base/uk.ac.stfc.isis.ibex.opis/update_opi_rgb_values.py
@@ -1,5 +1,6 @@
 ﻿import argparse
 from lxml import etree
+from lxml.etree import LxmlError
 from check_OPI_format_utils.rgb_checker import check_rgb_definitions, populate_rgb_definitions
 from check_opi_format import file_iterator
 
@@ -16,8 +17,13 @@ def update_rgb_values(file_name):
             file_name (str):
     """
     rgb_definitions = populate_rgb_definitions()
-    tree = etree.parse(file_name)
-    root = tree.getroot()
+
+    try:
+        tree = etree.parse(file_name)
+        root = tree.getroot()
+    except LxmlError as e:
+        print("XML failed to parse {}".format(e))
+
     errors = check_rgb_definitions(root)
 
     for color in root.iter("color"):
@@ -32,16 +38,24 @@ def update_rgb_values(file_name):
             color.set("blue", correct_colours[2])
     # tree.write(file_name) # Commented out to avoid unwantedly updating files during testing
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='update_opi_rgb_values')
-    parser.add_argument("-file", type=str, default=None,
-                        help='A single file to update')
-    parser.add_argument("-directory", type=str, default=DEFAULT_ROOT_DIR,
-                        help='A directory to update files in')
-    args = parser.parse_args()
-    single_file = args.file
-    root_dir = args.directory
-    for file_name in file_iterator(root_dir, single_file):
-        print("Updating incorrect RGB values in '{}'".format(file_name))
-        update_rgb_values(file_name)
+def main():
+    confirmation = input("Update incorrect RGB definitions in .opi file(s)? [y/n]")
+    if confirmation not in ("y", "n"):
+        print("Invalid input. No files updated")
+    if confirmation == "y":
+        parser = argparse.ArgumentParser(prog='update_opi_rgb_values')
+        parser.add_argument("-file", type=str, default=None,
+                            help='A single file to update')
+        parser.add_argument("-directory", type=str, default=DEFAULT_ROOT_DIR,
+                            help='A directory to update files in')
+        args = parser.parse_args()
+        single_file = args.file
+        root_dir = args.directory
+        for file_name in file_iterator(root_dir, single_file):
+            print("Updating incorrect RGB values in '{}'".format(file_name))
+            update_rgb_values(file_name)
+    if confirmation == "n":
+        print("Confirmation denied. No files updated.")
 
+if __name__ == "__main__":
+    main()
