3625379146fadaad0b13001b337d592ac4721028
Zsolt Kebel <25386639+zsoltkebel@users.noreply.github.com>
2023-11-14 16:45:57 +0000

    Add java doc to new class

--- base/uk.ac.stfc.isis.ibex.ui.widgets/src/uk/ac/stfc/isis/ibex/ui/widgets/IBEXButtonFactory.java
+++ base/uk.ac.stfc.isis.ibex.ui.widgets/src/uk/ac/stfc/isis/ibex/ui/widgets/IBEXButtonFactory.java
@@ -7,6 +7,9 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Listener;
 
+/**
+ * A factory pattern for creating commonly used buttons.
+ */
 public class IBEXButtonFactory {
 	
 	/**
