160881bb9b10b94b4d1f19e97d95b87cf2025cfa
Zsolt Kebel <25386639+zsoltkebel@users.noreply.github.com>
2023-11-23 15:33:57 +0000

    Satisfy checkstyle

--- base/uk.ac.stfc.isis.ibex.ui.scriptgenerator/src/uk/ac/stfc/isis/ibex/ui/scriptgenerator/views/ScriptGeneratorViewModel.java
+++ base/uk.ac.stfc.isis.ibex.ui.scriptgenerator/src/uk/ac/stfc/isis/ibex/ui/scriptgenerator/views/ScriptGeneratorViewModel.java
@@ -822,7 +822,7 @@ public class ScriptGeneratorViewModel extends ModelObject {
 	 * the new script definition.
 	 * 
 	 * @param previousParameters list of previous parameters
-	 * @param previousActions list of previous actions
+	 * @param previousActions    list of previous actions
 	 * @return a list of parameters that were transferred
 	 */
 	private List<JavaActionParameter> transferPreviousParameters(List<JavaActionParameter> previousParameters,
@@ -1360,12 +1360,12 @@ public class ScriptGeneratorViewModel extends ModelObject {
 		dialog.setOverwrite(true);
 		if (action == SWT.SAVE) {
 			dialog.setText("Save as");
-			dialog.setFilterExtensions(new String[] { "*.sgp" });
+			dialog.setFilterExtensions(new String[] {"*.sgp"});
 
 		} else {
 			dialog.setText("Load");
 			// Keep JSON extension when loading (for older param. files)
-			dialog.setFilterExtensions(new String[] { "*.sgp", "*.json" });
+			dialog.setFilterExtensions(new String[] {"*.sgp", "*.json"});
 		}
 		return Optional.ofNullable(dialog.open());
 	}
@@ -1397,7 +1397,7 @@ public class ScriptGeneratorViewModel extends ModelObject {
 				dialogResponse = 0;
 			} else {
 				emptyModel = false;
-				String[] replaceOrAppend = new String[] { "Append", "Replace" };
+				String[] replaceOrAppend = new String[] {"Append", "Replace"};
 				MessageDialog dialog = new MessageDialog(Constants.DISPLAY.getActiveShell(), "Replace or Append", null,
 						"Would you like to replace the current parameters or append the new parameters?",
 						MessageDialog.QUESTION, replaceOrAppend, -1);
@@ -1473,7 +1473,7 @@ public class ScriptGeneratorViewModel extends ModelObject {
 	 * @param actions copied actions
 	 */
 	public void copyActions(String actions) {
-		clipboard.setContents(new Object[] { actions }, new Transfer[] { TextTransfer.getInstance() });
+		clipboard.setContents(new Object[] {actions}, new Transfer[] {TextTransfer.getInstance()});
 	}
 
 	/**
