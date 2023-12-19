bdcfe7b9a1ce452a7872dc14ec15106e9d346872
esmithExperimentControls <evan.smith@stfc.ac.uk>
2023-12-19 13:32:25 +0000

    Add changes to comply with Checkstyle

--- base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/ScriptGeneratorSettingsSingleton.java
+++ base/uk.ac.stfc.isis.ibex.scriptgenerator/src/uk/ac/stfc/isis/ibex/scriptgenerator/ScriptGeneratorSettingsSingleton.java
@@ -1,14 +1,24 @@
 package uk.ac.stfc.isis.ibex.scriptgenerator;
 
 // TODO import uk.ac.stfc.isis.ibex.ui.scriptgenerator.views.Constants;
-
-public class ScriptGeneratorSettingsSingleton {
+/***
+ * The singleton class for settings of the Script Generator, to hold
+ * values such as whether skipping or pausing on invalid actions is 
+ * selected.
+ * @author yyf77781
+ *
+ */
+public final class ScriptGeneratorSettingsSingleton {
 	private static ScriptGeneratorSettingsSingleton instance;
 	private boolean skipEnabled = false;
 	
 	
-	private ScriptGeneratorSettingsSingleton() {}
+	private ScriptGeneratorSettingsSingleton() { }
 	
+	/***
+	 * Method to get the instance of this class.
+	 * @return the instance of the class
+	 */
 	public static ScriptGeneratorSettingsSingleton getInstance() {
 		if (instance == null) {
 			instance = new ScriptGeneratorSettingsSingleton();
@@ -16,10 +26,19 @@ public class ScriptGeneratorSettingsSingleton {
 		return instance;
 	}
 	
+	/***
+	 * Get the value of skip enabled setting, where true is enabled and
+	 * false is not enabled.
+	 * @return true or false
+	 */
 	public boolean getSkipEnabled() {
 		return skipEnabled;
 	}
 	
+	/***
+	 * Set the value of skip enabled setting.
+	 * @param enabled
+	 */
 	public void setSkipEnabled(boolean enabled) {
 		skipEnabled = enabled;
 	}
