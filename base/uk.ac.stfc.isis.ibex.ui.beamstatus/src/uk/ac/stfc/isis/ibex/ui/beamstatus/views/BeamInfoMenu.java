0d36831210f496909798522d6e4741886843bb37
Zsolt Kebel <25386639+zsoltkebel@users.noreply.github.com>
2024-01-03 16:19:03 +0000

    Add context menu items on right click instead of at construction

--- base/uk.ac.stfc.isis.ibex.ui.beamstatus/src/uk/ac/stfc/isis/ibex/ui/beamstatus/views/BeamInfoMenu.java
+++ base/uk.ac.stfc.isis.ibex.ui.beamstatus/src/uk/ac/stfc/isis/ibex/ui/beamstatus/views/BeamInfoMenu.java
@@ -5,7 +5,8 @@ import java.util.concurrent.TimeoutException;
 
 import org.apache.logging.log4j.Logger;
 import org.eclipse.jface.action.Action;
-
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.MessageBox;
@@ -33,7 +34,6 @@ public class BeamInfoMenu extends MenuManager {
 	private static final String LOG_PLOTTER_PERSPECTIVE_ID = "uk.ac.stfc.isis.ibex.client.e4.product.perspective.logplotter";
 	private static final Logger LOG = IsisLog.getLogger(BeamInfoMenu.class);
 	static final ConfigServer SERVER = Configurations.getInstance().server();
-	
 
 	/**
 	 * The constructor, creates the menu for when the specific facility PV is
@@ -42,44 +42,52 @@ public class BeamInfoMenu extends MenuManager {
 	 * @param facilityPV the selected PV
 	 */
 	public BeamInfoMenu(FacilityPV facilityPV) {
-		
-		// Creating right-click menu
-		// Add the option to add a block to a config, if there are any configs we can write to.
-		add(new Action("Add block to config: " + facilityPV.pv) { // Opening configuration dialog window
+		this.addMenuListener(new IMenuListener() {
 			@Override
-			public void run() {
+			public void menuAboutToShow(IMenuManager manager) {
+				// clear previously added menu actions
+				removeAll();
 
-				try {
-					var handler = new NewBlockHandler();
-					handler.setLocal(false);
-					handler.createDialog(facilityPV.pv);
+				// Creating right-click menu
+				// Add the option to add a block to a config, if there are any configs we can
+				// write to.
+				add(new Action("Add block to config: " + facilityPV.pv) { // Opening configuration dialog window
+					@Override
+					public void run() {
 
-				} catch (TimeoutException e) {
-					LoggerUtils.logErrorWithStackTrace(LOG, e.getMessage(), e);
-				} catch (IOException e) {
-					LoggerUtils.logErrorWithStackTrace(LOG, e.getMessage(), e);
-				}
-				super.run();
-			}
-			
-			@Override
-			public boolean isEnabled() {
-				return super.isEnabled() && SERVER.writableConfigsExist();
-			}
-		});
+						try {
+							var handler = new NewBlockHandler();
+							handler.setLocal(false);
+							handler.createDialog(facilityPV.pv);
 
-		add(new Action("Open in Log Plotter: " + facilityPV.pv) { // Opening log plotter window
-			public void run() {
-				if (BlocksMenu.canAddPlot()) {
-					switchToLogPlotter();
-					Presenter.pvHistoryPresenter().newDisplay(facilityPV.pv, facilityPV.pv);
-				} else {
-					Shell shell = PlatformUI.getWorkbench().getDisplay().getActiveShell();
-					MessageBox messageBox = new MessageBox(shell, SWT.ICON_WARNING | SWT.OK);
-					messageBox.setText("Failed to open in Log Plotter");
-					messageBox.setMessage("Make the Log Plotter perspective visible.");
-					messageBox.open();
-				}
+						} catch (TimeoutException e) {
+							LoggerUtils.logErrorWithStackTrace(LOG, e.getMessage(), e);
+						} catch (IOException e) {
+							LoggerUtils.logErrorWithStackTrace(LOG, e.getMessage(), e);
+						}
+						super.run();
+					}
+
+					@Override
+					public boolean isEnabled() {
+						return super.isEnabled() && SERVER.writableConfigsExist();
+					}
+				});
+
+				add(new Action("Open in Log Plotter: " + facilityPV.pv) { // Opening log plotter window
+					public void run() {
+						if (BlocksMenu.canAddPlot()) {
+							switchToLogPlotter();
+							Presenter.pvHistoryPresenter().newDisplay(facilityPV.pv, facilityPV.pv);
+						} else {
+							Shell shell = PlatformUI.getWorkbench().getDisplay().getActiveShell();
+							MessageBox messageBox = new MessageBox(shell, SWT.ICON_WARNING | SWT.OK);
+							messageBox.setText("Failed to open in Log Plotter");
+							messageBox.setMessage("Make the Log Plotter perspective visible.");
+							messageBox.open();
+						}
+					}
+				});
 			}
 		});
 	}
