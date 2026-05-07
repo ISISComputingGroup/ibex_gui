feda85eab057ee395e14a17c1a9db144361c3bda
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2026-05-07 19:06:15 +0100

    Try split update; eclipse 2026-03, GEF 2024-12

--- base/uk.ac.stfc.isis.ibex.ui.scripting/src/uk/ac/stfc/isis/ibex/ui/scripting/GenieOpenConsoleAction.java
+++ base/uk.ac.stfc.isis.ibex.ui.scripting/src/uk/ac/stfc/isis/ibex/ui/scripting/GenieOpenConsoleAction.java
@@ -14,6 +14,7 @@ import org.eclipse.ui.console.ConsolePlugin;
 import org.eclipse.ui.console.IConsoleFactory;
 import org.eclipse.ui.internal.console.ConsoleFactoryExtension;
 import org.eclipse.ui.internal.console.ConsoleManager;
+import org.eclipse.ui.internal.console.ConsoleView;
 import org.eclipse.ui.internal.console.OpenConsoleAction;
 
 /**
@@ -34,8 +35,8 @@ public class GenieOpenConsoleAction extends OpenConsoleAction {
 	/**
 	 * Initialise factory extensions.
 	 */
-	public GenieOpenConsoleAction() {
-		super();
+	public GenieOpenConsoleAction(ConsoleView consoleView) {
+		super(consoleView);
 		fFactoryExtensions = getFactories();
 	}
 	
